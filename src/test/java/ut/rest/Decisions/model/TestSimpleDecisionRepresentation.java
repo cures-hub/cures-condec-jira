f8cb4f0a0ea5cb226b2b57669edd19bfa2fb2d1a
Tim <tim.kuchenbuch@gmx.net>
2017-12-21 16:11:42 +0100

    Adding test for Get/Set functions

---
+++ src/test/java/ut/rest/Decisions/model/TestSimpleDecisionRepresentation.java
@@ -0,0 +1,11 @@
+package ut.rest.Decisions.model;
+
+/**
+ * 
+ * @author Tim Kuchenbuch
+ * @description Test Class for Simple Geter and Setter Tests
+ *
+ */
+public class TestSimpleDecisionRepresentation {
+
+}
