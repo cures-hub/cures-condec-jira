7c963680f9e61f64283687a71abfe1289a5fd7e7
Tim <tim.kuchenbuch@gmx.net>
2018-03-03 11:46:01 +0100

    ConDec-154: Add logger to TreeViewerRest and move createData from Strategy to TreeViewerRest

--- src/main/java/de/uhd/ifi/se/decision/documentation/jira/view/treeviewer/TreeViewerRest.java
+++ src/main/java/de/uhd/ifi/se/decision/documentation/jira/view/treeviewer/TreeViewerRest.java
@@ -7,6 +7,7 @@ import com.google.common.collect.ImmutableMap;
 
 import de.uhd.ifi.se.decision.documentation.jira.decisionknowledge.DecisionKnowledgeElement;
 import de.uhd.ifi.se.decision.documentation.jira.persistence.IPersistenceStrategy;
+import de.uhd.ifi.se.decision.documentation.jira.persistence.IssueStrategy;
 import de.uhd.ifi.se.decision.documentation.jira.persistence.StrategyProvider;
 
 import javax.ws.rs.*;
@@ -17,6 +18,8 @@ import javax.ws.rs.core.Response.Status;
 import de.uhd.ifi.se.decision.documentation.jira.util.KeyValuePairList;
 import de.uhd.ifi.se.decision.documentation.jira.util.Pair;
 import org.ofbiz.core.entity.GenericEntityException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -28,20 +31,24 @@ import java.util.List;
  */
 @Path("/treeviewer")
 public class TreeViewerRest {
+	private static final Logger LOGGER = LoggerFactory.getLogger(TreeViewerRest.class);
+	private IPersistenceStrategy strategy;
 
 	@GET
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	public Response getMessage(@QueryParam("projectKey") final String projectKey) throws GenericEntityException {
 		if (projectKey != null) {
+			LOGGER.error("getMessage ProjectKey is NULL");
 			ProjectManager projectManager = ComponentAccessor.getProjectManager();
 			Project project = projectManager.getProjectObjByKey(projectKey);
 			if (project == null) {
+				LOGGER.error("getMessage no project with this ProjectKey found");
 				return Response.status(Status.INTERNAL_SERVER_ERROR).entity(
 						ImmutableMap.of("error", "Cannot find project for the given query parameter 'projectKey'"))
 						.build();
 			} else {
 				StrategyProvider strategyProvider = new StrategyProvider();
-				IPersistenceStrategy strategy = strategyProvider.getStrategy(projectKey);
+				strategy = strategyProvider.getStrategy(projectKey);
 				Core core = createCore(projectKey, strategy);
 				return Response.ok(core).build();
 			}
@@ -66,10 +73,39 @@ public class TreeViewerRest {
 			KeyValuePairList.keyValuePairList = new ArrayList<Pair<String, String>>();
 			Pair<String, String> kvp = new Pair<String, String>("root", decisions.get(index).getKey());
 			KeyValuePairList.keyValuePairList.add(kvp);
-			dataSet.add(strategy.createData(decisions.get(index)));
+			dataSet.add(createData(decisions.get(index)));
 
 		}
 		core.setData(dataSet);
 		return core;
+	}
+
+	private Data createData(DecisionKnowledgeElement decisionKnowledgeElement) {
+		if (decisionKnowledgeElement == null) {
+			LOGGER.error("createData decisionKnowledgeElement is NULL");
+			return new Data();
+		}
+		Data data = new Data();
+
+		data.setText(decisionKnowledgeElement.getType() + " / " + decisionKnowledgeElement.getName());
+		data.setId(String.valueOf(decisionKnowledgeElement.getId()));
+
+		NodeInfo nodeInfo = new NodeInfo();
+		nodeInfo.setId(Long.toString(decisionKnowledgeElement.getId()));
+		nodeInfo.setKey(decisionKnowledgeElement.getKey());
+		nodeInfo.setIssueType(decisionKnowledgeElement.getType());
+		nodeInfo.setDescription(decisionKnowledgeElement.getDescription());
+		nodeInfo.setSummary(decisionKnowledgeElement.getName());
+		data.setNodeInfo(nodeInfo);
+
+		List<DecisionKnowledgeElement> children = strategy.getChildren(decisionKnowledgeElement);
+
+		List<Data> childrenToData = new ArrayList<Data>();
+		for (DecisionKnowledgeElement child : children) {
+			childrenToData.add(createData(child));
+		}
+		data.setChildren(childrenToData);
+
+		return data;
 	}
 }
