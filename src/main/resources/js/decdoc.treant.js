5c685fc854687cd8a251cb17fe14f590c560385a
Tim <tim.kuchenbuch@gmx.net>
2018-04-20 10:48:27 +0200

    ConDec-154: Refactor getTreeView and getTreant URL

--- src/main/resources/js/decdoc.treant.js
+++ src/main/resources/js/decdoc.treant.js
@@ -1,32 +1,24 @@
 function buildTreant(projectKey, node) {
 	var depthOfTree = document.getElementById("depthOfTreeInput").value;
-	var treantUrl = getTreantUrl(projectKey, node.key, depthOfTree);
-	getJSON(
-			treantUrl,
-			function(error, treant) {
-				if (error != null) {
-					document.getElementById("treant-container").innerHTML = "Treant data could not be received. Error-Code: "
-							+ error;
-				} else {
-					document.getElementById("treant-container").innerHTML = "";
-					new Treant(treant);
-					var modal = document.getElementById('ContextMenuModal');
-					// add click-handler for elements in modal to close modal window
-					var elementsWithCloseFunction = document.getElementsByClassName("modal-close");
-					for (var counter = 0; counter < elementsWithCloseFunction.length; counter++) {
-						elementsWithCloseFunction[counter].onclick = function() {
-							closeModal();
-						}
-					}
-					// close modal window if user clicks anywhere outside of the modal
-					window.onclick = function(event) {
-						if (event.target == modal) {
-							closeModal();
-						}
-					};
-					createContextMenuForTreeNodes(projectKey);
-				}
-			});
+	getTreantView(projectKey, node.key, depthOfTree, function(treant){
+        document.getElementById("treant-container").innerHTML = "";
+        new Treant(treant);
+        var modal = document.getElementById('ContextMenuModal');
+        // add click-handler for elements in modal to close modal window
+        var elementsWithCloseFunction = document.getElementsByClassName("modal-close");
+        for (var counter = 0; counter < elementsWithCloseFunction.length; counter++) {
+            elementsWithCloseFunction[counter].onclick = function() {
+                closeModal();
+            }
+        }
+        // close modal window if user clicks anywhere outside of the modal
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                closeModal();
+            }
+        };
+        createContextMenuForTreeNodes(projectKey);
+	});
 }
 //TODO Bug appear point after selecting a Element in Treeview
 function createContextMenuForTreeNodes(projectKey) {
--- src/main/resources/js/decdoc.tree.viewer.js
+++ src/main/resources/js/decdoc.tree.viewer.js
@@ -1,47 +1,42 @@
 function buildTreeViewer(projectKey, nodeId) {
-	var treeJSONUrl = getTreeView(projectKey);
-	getJSON(treeJSONUrl, function(err, data) {
-		if (err !== null) {
-			displayGetJsonError(err);
-		} else {
-			if ($('#evts').jstree(true)) {
-				var tree = $('#evts').jstree(true);
-				tree.destroy();
-			}
-			$('#evts').on("select_node.jstree", function(e, data) {
-				setBack("");
-				var node = data.node.data;
-				fillAccordion(data, projectKey, node);
-				buildTreant(projectKey, node);
-			}).on('ready.jstree', function() {
-				if (nodeId) {
-					var tree = $('#evts').jstree(true);
-					if (tree) {
-						tree.select_node("" + nodeId)
-						console.log("select_node");
-						// no need in any scenario
-					} else {
-						console.log("set_back");
-						// setBack("No Element has been
-						// selected");
-					}
-				}
-			}).jstree({
-				'core' : data,
-				// TODO: add drag n drop
-				'plugins' : [ 'contextmenu', 'wholerow', 'sort', 'search', '' ],
-				'search' : {
-					'show_only_matches' : true
-				},
-				'contextmenu' : {
-					'items' : contextMenuActions
-				}
-			});
-			document.getElementById("Details").style.display = "block";
-			$(".search-input").keyup(function() {
-				var searchString = $(this).val();
-				$('#evts').jstree(true).search(searchString);
-			});
-		}
-	});
+	getTreeView(projectKey,function (data) {
+        if ($('#evts').jstree(true)) {
+            var tree = $('#evts').jstree(true);
+            tree.destroy();
+        }
+        $('#evts').on("select_node.jstree", function(e, data) {
+            setBack("");
+            var node = data.node.data;
+            fillAccordion(data, projectKey, node);
+            buildTreant(projectKey, node);
+        }).on('ready.jstree', function() {
+            if (nodeId) {
+                var tree = $('#evts').jstree(true);
+                if (tree) {
+                    tree.select_node("" + nodeId)
+                    console.log("select_node");
+                    // no need in any scenario
+                } else {
+                    console.log("set_back");
+                    // setBack("No Element has been
+                    // selected");
+                }
+            }
+        }).jstree({
+            'core' : data,
+            // TODO: add drag n drop
+            'plugins' : [ 'contextmenu', 'wholerow', 'sort', 'search', '' ],
+            'search' : {
+                'show_only_matches' : true
+            },
+            'contextmenu' : {
+                'items' : contextMenuActions
+            }
+        });
+        document.getElementById("Details").style.display = "block";
+        $(".search-input").keyup(function() {
+            var searchString = $(this).val();
+            $('#evts').jstree(true).search(searchString);
+        });
+    });
 }
