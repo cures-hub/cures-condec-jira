85c4872e897a4f72cdf0ae6c23fbe8617923b645
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2018-04-23 15:36:08 +0200

    ConDec-132: Refactor strategy provider

--- src/main/java/de/uhd/ifi/se/decision/documentation/jira/persistence/StrategyProvider.java
+++ src/main/java/de/uhd/ifi/se/decision/documentation/jira/persistence/StrategyProvider.java
@@ -8,33 +8,34 @@ import com.atlassian.sal.api.transaction.TransactionTemplate;
 import de.uhd.ifi.se.decision.documentation.jira.util.ComponentGetter;
 
 /**
- * @description Provides the right Strategy for every Project
+ * @description Provides the persistence strategy for a project
  */
 public class StrategyProvider {
 
-	public PersistenceStrategy getStrategy(final String projectKey) {
-		if(projectKey == null){
-			return  null;
+	public PersistenceStrategy getStrategy(String projectKey) {
+		if (projectKey == null) {
+			throw new IllegalArgumentException("The project key cannot be null.");
 		}
+		boolean isIssueStrategy = isIssueStrategy(projectKey);
+		if (isIssueStrategy) {
+			return new IssueStrategy();
+		}
+		return new ActiveObjectStrategy();
+	}
+
+	public boolean isIssueStrategy(String projectKey) {
 		TransactionTemplate transactionTemplate = ComponentGetter.getTransactionTemplate();
-		final PluginSettingsFactory pluginSettingsFactory = ComponentGetter.getPluginSettingsFactory();
-		final String pluginStorageKey = ComponentGetter.getPluginStorageKey();
-		Object ob = transactionTemplate.execute(new TransactionCallback<Object>() {
+		PluginSettingsFactory pluginSettingsFactory = ComponentGetter.getPluginSettingsFactory();
+		String pluginStorageKey = ComponentGetter.getPluginStorageKey();
+		Object isIssueStrategy = transactionTemplate.execute(new TransactionCallback<Object>() {
 			public Object doInTransaction() {
 				PluginSettings settings = pluginSettingsFactory.createSettingsForKey(projectKey);
-				Object o = settings.get(pluginStorageKey + ".isIssueStrategy");
-				return o;
+				return settings.get(pluginStorageKey + ".isIssueStrategy");
 			}
 		});
-		if (ob instanceof String) {
-			String strategyType = (String) ob;
-			if (strategyType.equalsIgnoreCase("true")) {
-				return new IssueStrategy();
-			} else {
-				return new ActiveObjectStrategy();
-			}
-		} else {
-			return new ActiveObjectStrategy();
+		if (isIssueStrategy instanceof String && isIssueStrategy.equals("true")) {
+			return Boolean.valueOf((String) isIssueStrategy);
 		}
+		return false;
 	}
 }
