fc594b38f1a15a8ff874de4a94d883c13e3a7133
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2018-04-30 10:53:34 +0200

    Refactor Config REST API class

--- src/main/java/de/uhd/ifi/se/decision/documentation/jira/rest/ConfigRest.java
+++ src/main/java/de/uhd/ifi/se/decision/documentation/jira/rest/ConfigRest.java
@@ -20,7 +20,7 @@ import com.google.common.collect.ImmutableMap;
 import de.uhd.ifi.se.decision.documentation.jira.persistence.ConfigPersistence;
 
 /**
- * @description Rest resource for plugin configuration
+ * @description Rest resource for plug-in configuration
  */
 @Path("/config")
 @Scanned
@@ -39,20 +39,19 @@ public class ConfigRest {
 	@POST
 	public Response setActivated(@Context HttpServletRequest request, @QueryParam("projectKey") String projectKey,
 			@QueryParam("isActivated") String isActivated) {
-		Response datacheck = checkData(request, projectKey);
-		if (datacheck != null) {
-			return datacheck;
-		} else {
-			if (isActivated == null) {
-				return Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "isActivated = null")).build();
-			}
-			try {
-				ConfigPersistence.setActivated(projectKey, Boolean.valueOf(isActivated));
-				return Response.ok(Status.ACCEPTED).build();
-			} catch (Exception e) {
-				LOGGER.error(e.getMessage());
-				return Response.status(Status.CONFLICT).build();
-			}
+		Response isValidDataResponse = checkIfDataIsValid(request, projectKey);
+		if (isValidDataResponse.getStatus() != Status.OK.getStatusCode()) {
+			return isValidDataResponse;
+		}
+		if (isActivated == null) {
+			return Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "isActivated = null")).build();
+		}
+		try {
+			ConfigPersistence.setActivated(projectKey, Boolean.valueOf(isActivated));
+			return Response.ok(Status.ACCEPTED).build();
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage());
+			return Response.status(Status.CONFLICT).build();
 		}
 	}
 
@@ -60,36 +59,54 @@ public class ConfigRest {
 	@POST
 	public Response setIssueStrategy(@Context HttpServletRequest request, @QueryParam("projectKey") String projectKey,
 			@QueryParam("isIssueStrategy") String isIssueStrategy) {
-		Response datacheck = checkData(request, projectKey);
-		if (datacheck != null) {
-			return datacheck;
-		} else {
-			if (isIssueStrategy == null) {
-				return Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "isIssueStrategy = null")).build();
-			}
-			try {
-				ConfigPersistence.setIssueStrategy(projectKey, Boolean.valueOf(isIssueStrategy));
-				return Response.ok(Status.ACCEPTED).build();
-			} catch (Exception e) {
-				LOGGER.error(e.getMessage());
-				return Response.status(Status.CONFLICT).build();
-			}
+		Response isValidDataResponse = checkIfDataIsValid(request, projectKey);
+		if (isValidDataResponse.getStatus() != Status.OK.getStatusCode()) {
+			return isValidDataResponse;
+		}
+		if (isIssueStrategy == null) {
+			return Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "isIssueStrategy = null")).build();
+		}
+		try {
+			ConfigPersistence.setIssueStrategy(projectKey, Boolean.valueOf(isIssueStrategy));
+			return Response.ok(Status.ACCEPTED).build();
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage());
+			return Response.status(Status.CONFLICT).build();
 		}
 	}
 
-	private Response checkData(HttpServletRequest request, String projectKey) {
+	private Response checkIfDataIsValid(HttpServletRequest request, String projectKey) {
 		if (request == null) {
 			return Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "request = null")).build();
 		}
+
+		Response userResponse = checkIfUserIsAuthorized(request);
+		if (userResponse.getStatus() != Status.OK.getStatusCode()) {
+			return userResponse;
+		}
+
+		Response projectResponse = checkIfProjectKeyIsValid(projectKey);
+		if (projectResponse.getStatus() != Status.OK.getStatusCode()) {
+			return projectResponse;
+		}
+
+		return Response.status(Status.OK).build();
+	}
+
+	private Response checkIfUserIsAuthorized(HttpServletRequest request) {
 		String username = userManager.getRemoteUsername(request);
 		if (username == null || !userManager.isSystemAdmin(username)) {
-			LOGGER.warn("Unauthorized user by name:{} tried to change Configuration", username);
+			LOGGER.warn("Unauthorized user (name:{}) tried to change configuration.", username);
 			return Response.status(Status.UNAUTHORIZED).build();
 		}
+		return Response.status(Status.OK).build();
+	}
+
+	private Response checkIfProjectKeyIsValid(String projectKey) {
 		if (projectKey == null || projectKey.equals("")) {
-			LOGGER.error("ProjectKey in ConfigRest setResponseForGet");
-			return Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "projectKey = null")).build();
+			LOGGER.error("Project configuration could not be changed since the project key is invalid.");
+			return Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "Project key is invalid.")).build();
 		}
-		return null;
+		return Response.status(Status.OK).build();
 	}
 }
--- src/test/java/de/uhd/ifi/se/decision/documentation/jira/config/TestConfigRest.java
+++ src/test/java/de/uhd/ifi/se/decision/documentation/jira/config/TestConfigRest.java
@@ -26,200 +26,237 @@ import net.java.ao.test.junit.ActiveObjectsJUnitRunner;
 @RunWith(ActiveObjectsJUnitRunner.class)
 public class TestConfigRest extends TestSetUp {
 	private EntityManager entityManager;
-	private HttpServletRequest req;
+	private HttpServletRequest request;
 	private ConfigRest confRest;
-	
+
+	private static final String INVALID_PROJECTKEY = "Project key is invalid.";
+	private static final String INVALID_REQUEST = "request = null";
+	private static final String INVALID_STRATEGY = "isIssueStrategy = null";
+	private static final String INVALID_ACTIVATION = "isActivated = null";
+
 	@Before
 	public void setUp() {
 		UserManager userManager = new MockDefaultUserManager();
 		confRest = new ConfigRest(userManager);
-		
-		initialization();
-		new ComponentGetter().init(new TestActiveObjects(entityManager), new MockTransactionTemplate(), new MockDefaultUserManager());
-		
-		req = new MockHttpServletRequest();
-		req.setAttribute("WithFails", false);
-		req.setAttribute("NoFails", true);
+		new ComponentGetter().init(new TestActiveObjects(entityManager), new MockTransactionTemplate(),
+				new MockDefaultUserManager());
+
+		request = new MockHttpServletRequest();
+		request.setAttribute("WithFails", false);
+		request.setAttribute("NoFails", true);
 	}
+
+	private Response getBadRequestResponse(String errorMessage) {
+		return Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", errorMessage)).build();
+	}
+
 	// Testing setActivated
-	
 	@Test
-	public void testdoPostrequestNullKeyNullIsActivatedNull() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "request = null")).build().getEntity(),confRest.setActivated(null, null, null).getEntity());
+	public void testSetActivatedRequestNullProjectKeyNullIsActivatedNull() {
+		assertEquals(getBadRequestResponse(INVALID_REQUEST).getEntity(),
+				confRest.setActivated(null, null, null).getEntity());
 	}
-	
+
 	@Test
-	public void testdoPostrequestNullKeyNullIsActivatedTrue() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "request = null")).build().getEntity(),confRest.setActivated(null, null, "true").getEntity());
+	public void testSetActivatedRequestNullProjectKeyNullIsActivatedTrue() {
+		assertEquals(getBadRequestResponse(INVALID_REQUEST).getEntity(),
+				confRest.setActivated(null, null, "true").getEntity());
 	}
-	
+
 	@Test
-	public void testdoPostrequestNullKeyNullIsActivatedFalse() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "request = null")).build().getEntity(),confRest.setActivated(null, null, "false").getEntity());
+	public void testSetActivatedRequestNullProjectKeyNullIsActivatedFalse() {
+		assertEquals(getBadRequestResponse(INVALID_REQUEST).getEntity(),
+				confRest.setActivated(null, null, "false").getEntity());
 	}
-	
+
 	@Test
-	public void testdoPostrequestNullKeyExistsIsActivatedNull() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "request = null")).build().getEntity(),confRest.setActivated(null, "TEST", null).getEntity());
+	public void testSetActivatedRequestNullProjectKeyExistsIsActivatedNull() {
+		assertEquals(getBadRequestResponse(INVALID_REQUEST).getEntity(),
+				confRest.setActivated(null, "TEST", null).getEntity());
 	}
-	
+
 	@Test
-	public void testdoPostrequestNullKeyExistsIsActivatedTrue() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "request = null")).build().getEntity(),confRest.setActivated(null, "TEST", "true").getEntity());
+	public void testSetActivatedRequestNullProjectKeyExistsIsActivatedTrue() {
+		assertEquals(getBadRequestResponse(INVALID_REQUEST).getEntity(),
+				confRest.setActivated(null, "TEST", "true").getEntity());
 	}
-	
+
 	@Test
-	public void testdoPostrequestNullKeyExistsIsActivatedFalse() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "request = null")).build().getEntity(),confRest.setActivated(null, "TEST", "false").getEntity());
+	public void testSetActivatedRequestNullProjectKeyExistsIsActivatedFalse() {
+		assertEquals(getBadRequestResponse(INVALID_REQUEST).getEntity(),
+				confRest.setActivated(null, "TEST", "false").getEntity());
 	}
-	
+
 	@Test
-	public void testdoPostrequestNullKeyDontExistIsActivatedNull() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "request = null")).build().getEntity(),confRest.setActivated(null, "NotTEST", null).getEntity());
+	public void testSetActivatedRequestNullProjectKeyDoesNotExistIsActivatedNull() {
+		assertEquals(getBadRequestResponse(INVALID_REQUEST).getEntity(),
+				confRest.setActivated(null, "NotTEST", null).getEntity());
 	}
-	
+
 	@Test
-	public void testdoPostrequestNullKeyDontExistIsActivatedTrue() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "request = null")).build().getEntity(),confRest.setActivated(null, "NotTEST", "true").getEntity());
+	public void testSetActivatedRequestNullProjectKeyDoesNotExistIsActivatedTrue() {
+		assertEquals(getBadRequestResponse(INVALID_REQUEST).getEntity(),
+				confRest.setActivated(null, "NotTEST", "true").getEntity());
 	}
-	
+
 	@Test
-	public void testdoPostrequestNullKeyDontExistIsActivatedFalse() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "request = null")).build().getEntity(),confRest.setActivated(null, "NotTEST", "false").getEntity());
+	public void testSetActivatedRequestNullProjectKeyDoesNotExistIsActivatedFalse() {
+		assertEquals(getBadRequestResponse(INVALID_REQUEST).getEntity(),
+				confRest.setActivated(null, "NotTEST", "false").getEntity());
 	}
-	
+
 	@Test
-	public void testdoPostrequestExKeyNullIsActivatedNull() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "projectKey = null")).build().getEntity(),confRest.setActivated(req, null, null).getEntity());
+	public void testSetActivatedRequestExistsProjectKeyNullIsActivatedNull() {
+		assertEquals(getBadRequestResponse(INVALID_PROJECTKEY).getEntity(),
+				confRest.setActivated(request, null, null).getEntity());
 	}
-	
+
 	@Test
-	public void testdoPostrequestExKeyNullIsActivatedTrue() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "projectKey = null")).build().getEntity(),confRest.setActivated(req, null, "true").getEntity());
+	public void testSetActivatedRequestExistsProjectKeyNullIsActivatedTrue() {
+		assertEquals(getBadRequestResponse(INVALID_PROJECTKEY).getEntity(),
+				confRest.setActivated(request, null, "true").getEntity());
 	}
-	
+
 	@Test
-	public void testdoPostrequestExKeyNullIsActivatedFalse() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "projectKey = null")).build().getEntity(),confRest.setActivated(req, null, "false").getEntity());
+	public void testSetActivatedRequestExistsProjectKeyNullIsActivatedFalse() {
+		assertEquals(getBadRequestResponse(INVALID_PROJECTKEY).getEntity(),
+				confRest.setActivated(request, null, "false").getEntity());
 	}
-	
+
 	@Test
-	public void testdoPostrequestExKeyExistsIsActivatedNull() {
-		assertEquals( Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "isActivated = null")).build().getEntity(),confRest.setActivated(req, "TEST", null).getEntity());
+	public void testSetActivatedRequestExistsProjectKeyExistsIsActivatedNull() {
+		assertEquals(getBadRequestResponse(INVALID_ACTIVATION).getEntity(),
+				confRest.setActivated(request, "TEST", null).getEntity());
 	}
-	
+
 	@Test
-	public void testdoPostrequestExKeyExistsIsActivatedTrue() {
-		assertEquals( Response.ok().build().getClass(),confRest.setActivated(req, "TEST", "true").getClass());
+	public void testSetActivatedRequestExistsProjectKeyExistsIsActivatedTrue() {
+		assertEquals(Response.ok().build().getClass(), confRest.setActivated(request, "TEST", "true").getClass());
 	}
-	
+
 	@Test
-	public void testdoPostrequestExKeyExistsIsActivatedFalse() {
-		assertEquals( Response.ok().build().getClass(),confRest.setActivated(req, "TEST", "false").getClass());
+	public void testSetActivatedRequestExistsProjectKeyExistsIsActivatedFalse() {
+		assertEquals(Response.ok().build().getClass(), confRest.setActivated(request, "TEST", "false").getClass());
 	}
-	
+
 	@Test
-	public void testdoPostUserUnauthorized() {
-		req.setAttribute("WithFails", true);
-		req.setAttribute("NoFails", false);
-		assertEquals(Status.UNAUTHORIZED.getStatusCode(),confRest.setActivated(req, "NotTEST", "false").getStatus());
+	public void testSetActivatedUserUnauthorized() {
+		request.setAttribute("WithFails", true);
+		request.setAttribute("NoFails", false);
+		assertEquals(Status.UNAUTHORIZED.getStatusCode(),
+				confRest.setActivated(request, "NotTEST", "false").getStatus());
 	}
-	
+
 	@Test
-	public void testdoPostUserNull() {
-		req.setAttribute("WithFails", false);
-		req.setAttribute("NoFails", false);
-		assertEquals(Status.UNAUTHORIZED.getStatusCode(),confRest.setActivated(req, "NotTEST", "false").getStatus());
+	public void testSetActivatedUserNull() {
+		request.setAttribute("WithFails", false);
+		request.setAttribute("NoFails", false);
+		assertEquals(Status.UNAUTHORIZED.getStatusCode(),
+				confRest.setActivated(request, "NotTEST", "false").getStatus());
 	}
-	
-	//Testing setIssueStrategy
-	
+
+	// Testing setIssueStrategy
 	@Test
 	public void testdoPutrequestNullKeyNullIsIssueStategydNull() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "request = null")).build().getEntity(),confRest.setIssueStrategy(null, null, null).getEntity());
+		assertEquals(getBadRequestResponse(INVALID_REQUEST).getEntity(),
+				confRest.setIssueStrategy(null, null, null).getEntity());
 	}
-	
+
 	@Test
 	public void testdoPutrequestNullKeyNullIsIssueStategyTrue() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "request = null")).build().getEntity(),confRest.setIssueStrategy(null, null, "true").getEntity());
+		assertEquals(getBadRequestResponse(INVALID_REQUEST).getEntity(),
+				confRest.setIssueStrategy(null, null, "true").getEntity());
 	}
-	
+
 	@Test
 	public void testdoPutrequestNullKeyNullIsIssueStategyFalse() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "request = null")).build().getEntity(),confRest.setIssueStrategy(null, null, "false").getEntity());
+		assertEquals(getBadRequestResponse(INVALID_REQUEST).getEntity(),
+				confRest.setIssueStrategy(null, null, "false").getEntity());
 	}
-	
+
 	@Test
 	public void testdoPutrequestNullKeyExistsIsIssueStategyNull() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "request = null")).build().getEntity(),confRest.setIssueStrategy(null, "TEST", null).getEntity());
+		assertEquals(getBadRequestResponse(INVALID_REQUEST).getEntity(),
+				confRest.setIssueStrategy(null, "TEST", null).getEntity());
 	}
-	
+
 	@Test
 	public void testdoPutrequestNullKeyExistsIsIssueStategyTrue() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "request = null")).build().getEntity(),confRest.setIssueStrategy(null, "TEST", "true").getEntity());
+		assertEquals(getBadRequestResponse(INVALID_REQUEST).getEntity(),
+				confRest.setIssueStrategy(null, "TEST", "true").getEntity());
 	}
-	
+
 	@Test
 	public void testdoPutrequestNullKeyExistsIsIssueStategyFalse() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "request = null")).build().getEntity(),confRest.setIssueStrategy(null, "TEST", "false").getEntity());
+		assertEquals(getBadRequestResponse(INVALID_REQUEST).getEntity(),
+				confRest.setIssueStrategy(null, "TEST", "false").getEntity());
 	}
-	
+
 	@Test
 	public void testdoPutrequestNullKeyDontExistIsIssueStategyNull() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "request = null")).build().getEntity(),confRest.setIssueStrategy(null, "NotTEST", null).getEntity());
+		assertEquals(getBadRequestResponse(INVALID_REQUEST).getEntity(),
+				confRest.setIssueStrategy(null, "NotTEST", null).getEntity());
 	}
-	
+
 	@Test
 	public void testdoPutrequestNullKeyDontExistIsIssueStategyTrue() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "request = null")).build().getEntity(),confRest.setIssueStrategy(null, "NotTEST", "true").getEntity());
+		assertEquals(getBadRequestResponse(INVALID_REQUEST).getEntity(),
+				confRest.setIssueStrategy(null, "NotTEST", "true").getEntity());
 	}
-	
+
 	@Test
 	public void testdoPutrequestNullKeyDontExistIsIssueStategyFalse() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "request = null")).build().getEntity(),confRest.setIssueStrategy(null, "NotTEST", "false").getEntity());
+		assertEquals(getBadRequestResponse(INVALID_REQUEST).getEntity(),
+				confRest.setIssueStrategy(null, "NotTEST", "false").getEntity());
 	}
-	
+
 	@Test
 	public void testdoPutrequestExKeyNullIsIssueStategyNull() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "projectKey = null")).build().getEntity(),confRest.setIssueStrategy(req, null, null).getEntity());
+		assertEquals(getBadRequestResponse(INVALID_PROJECTKEY).getEntity(),
+				confRest.setIssueStrategy(request, null, null).getEntity());
 	}
-	
+
 	@Test
 	public void testdoPutrequestExKeyNullIsIssueStategyTrue() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "projectKey = null")).build().getEntity(),confRest.setIssueStrategy(req, null, "true").getEntity());
+		assertEquals(getBadRequestResponse(INVALID_PROJECTKEY).getEntity(),
+				confRest.setIssueStrategy(request, null, "true").getEntity());
 	}
-	
+
 	@Test
 	public void testdoPutrequestExKeyNullIsIssueStategyFalse() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "projectKey = null")).build().getEntity(),confRest.setIssueStrategy(req, null, "false").getEntity());
+		assertEquals(getBadRequestResponse(INVALID_PROJECTKEY).getEntity(),
+				confRest.setIssueStrategy(request, null, "false").getEntity());
 	}
-	
+
 	@Test
 	public void testdoPutrequestExKeyExistsIsIssueStategyNull() {
-		assertEquals(Response.status(Status.BAD_REQUEST).entity(ImmutableMap.of("error", "isIssueStrategy = null")).build().getEntity(),confRest.setIssueStrategy(req, "TEST", null).getEntity());
+		assertEquals(getBadRequestResponse(INVALID_STRATEGY).getEntity(),
+				confRest.setIssueStrategy(request, "TEST", null).getEntity());
 	}
-	
+
 	@Test
 	public void testdoPutrequestExKeyExistsIsIssueStategyTrue() {
-		assertEquals( Response.ok().build().getClass(),confRest.setIssueStrategy(req, "TEST", "true").getClass());
+		assertEquals(Response.ok().build().getClass(), confRest.setIssueStrategy(request, "TEST", "true").getClass());
 	}
-	
+
 	@Test
 	public void testdoPutrequestExKeyExistsIsIssueStategyFalse() {
-		assertEquals( Response.ok().build().getClass(),confRest.setIssueStrategy(req, "TEST", "false").getClass());
+		assertEquals(Response.ok().build().getClass(), confRest.setIssueStrategy(request, "TEST", "false").getClass());
 	}
-	
+
 	@Test
 	public void testdoPutUserUnauthorized() {
-		req.setAttribute("WithFails", true);
-		req.setAttribute("NoFails", false);
-		assertEquals(Status.UNAUTHORIZED.getStatusCode(),confRest.setIssueStrategy(req, "NotTEST", "false").getStatus());
+		request.setAttribute("WithFails", true);
+		request.setAttribute("NoFails", false);
+		assertEquals(Status.UNAUTHORIZED.getStatusCode(),
+				confRest.setIssueStrategy(request, "NotTEST", "false").getStatus());
 	}
-	
+
 	@Test
 	public void testdoPutUserNull() {
-		req.setAttribute("WithFails", false);
-		req.setAttribute("NoFails", false);
-		assertEquals(Status.UNAUTHORIZED.getStatusCode(),confRest.setIssueStrategy(req, "NotTEST", "false").getStatus());
+		request.setAttribute("WithFails", false);
+		request.setAttribute("NoFails", false);
+		assertEquals(Status.UNAUTHORIZED.getStatusCode(),
+				confRest.setIssueStrategy(request, "NotTEST", "false").getStatus());
 	}
 }
