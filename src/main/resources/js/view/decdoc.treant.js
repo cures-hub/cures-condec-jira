40fc78c8c2c1c926bc49f28a101a38a97970f145
Tim <tim.kuchenbuch@gmx.net>
2018-06-07 09:39:38 +0200

    ConDec-50: Add functionality to the drop function

--- src/main/resources/js/view/decdoc.treant.js
+++ src/main/resources/js/view/decdoc.treant.js
@@ -1,3 +1,6 @@
+//Gloabal Variable for DnD
+var dragId;
+
 function buildTreant(decisionKnowledgeElement) {
 	var depthOfTree = document.getElementById("depthOfTreeInput").value;
 	getTreant(decisionKnowledgeElement.key, depthOfTree, function(treant) {
@@ -22,20 +25,28 @@ function addingDragAndDropSupport() {
 	var i;
 	for(i=0; i<nodeDivs.length; i++) {
         nodeDivs[i].draggable = true;
-        nodeDivs[i].classList.add("drop");
         nodeDivs[i].addEventListener('dragstart', drag, false);
-        nodeDivs[i].addEventListener('drop',drop, false);
-        nodeDivs[i].addEventListener('dragover', allowDrop, false);
+        nodeDivs[i].addEventListener('drop',function(){drop(event,this);});
+
     }
+    var nodeDesc = document.getElementsByClassName("node-desc");
+    for(i=0; i<nodeDesc.length; i++) {
+	nodeDesc[i].addEventListener('dragover', allowDrop, false);
+    }
+
 }
 
 function drag(event) {
     event.dataTransfer.setData("text", event.target.id);
+    dragId = event.target.id;
 }
 
-function drop(event){
+function drop(event, target){
 	event.preventDefault();
-	console.log("Drop");
+	var parentId = target.id;
+	var childId = dragId;
+    createLinkToExistingElement(parentId, childId);
+
 }
 
 function  allowDrop(event) {
