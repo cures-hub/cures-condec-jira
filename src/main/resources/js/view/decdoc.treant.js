bfe9b1639c4f391de8ce1e88d35e0ad60dd6db25
Tim <tim.kuchenbuch@gmx.net>
2018-06-07 08:27:10 +0200

    ConDec-50: Call java script event listener functions

--- src/main/resources/js/view/decdoc.treant.js
+++ src/main/resources/js/view/decdoc.treant.js
@@ -20,14 +20,26 @@ function createContextMenuForTreantNodes() {
 function addingDragAndDropSupport() {
 	var nodeDivs = document.getElementsByClassName("node");
 	var i;
-	for(i=0; i<nodeDivs.length; i++){
-		nodeDivs[i].draggable=true;
-		nodeDivs[i].ondragend="drag(event)";
-	}
+	for(i=0; i<nodeDivs.length; i++) {
+        nodeDivs[i].draggable = true;
+        nodeDivs[i].classList.add("drop");
+        nodeDivs[i].addEventListener('dragstart', drag, false);
+        nodeDivs[i].addEventListener('drop',drop, false);
+        nodeDivs[i].addEventListener('dragover', allowDrop, false);
+    }
 }
-//TODO Getting this function called instead of the ondragend default function
-function drag(event) {
-	event.preventDefault();
-	console.log(event);
 
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
 }
+
+function drop(event){
+	event.preventDefault();
+	console.log("Drop");
+}
+
+function  allowDrop(event) {
+	event.preventDefault();
+}
+
+
