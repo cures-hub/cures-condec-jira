cd59ec70e41f249a9204a4bca5c2d13befdcd6e6
Tim <tim.kuchenbuch@gmx.net>
2018-06-14 07:47:22 +0200

    ConDec-154: Fix frozen TreeViewer problem

--- src/main/resources/js/controller/decdoc.viewhandler.js
+++ src/main/resources/js/controller/decdoc.viewhandler.js
@@ -1,6 +1,11 @@
 function updateView(nodeId){
+    if ($('#evts').jstree(true)) {
+        var tree = $('#evts').jstree(true);
+        tree.destroy();
+    }
     $('#evts').on("select_node.jstree", function(error, data) {
         var node = data.node.data;
+        console.log(node);
         fillAccordion(node);
         buildTreant(node);
     });
