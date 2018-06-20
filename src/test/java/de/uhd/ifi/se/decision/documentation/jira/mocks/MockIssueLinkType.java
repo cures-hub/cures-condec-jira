c4e6807bc006f10b29abf64e505480634ba2f7c7
Tim <tim.kuchenbuch@gmx.net>
2018-06-20 10:33:49 +0200

    ConDec-71: Avoid final local variable

--- src/test/java/de/uhd/ifi/se/decision/documentation/jira/mocks/MockIssueLinkType.java
+++ src/test/java/de/uhd/ifi/se/decision/documentation/jira/mocks/MockIssueLinkType.java
@@ -89,7 +89,7 @@ public class MockIssueLinkType implements IssueLinkType {
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
+		int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
