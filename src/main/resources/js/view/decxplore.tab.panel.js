ad8b14f4e15c5a78e7eaeadc919b9e6a9b74920a
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2018-06-29 00:00:50 +0200

    ConDec-152: Integrate decision knowledge from Commits using the REST API of the Git Integration for JIRA plug-in

--- src/main/resources/js/view/decxplore.tab.panel.js
+++ src/main/resources/js/view/decxplore.tab.panel.js
@@ -1,64 +1,29 @@
 var issueTabTreantTree;
 
 function fillIssueTabPanel() {
-	console.log("init");
-	var pathname = window.location.pathname;
-	var stringArray = pathname.split("/");
-	var issueKey = stringArray[stringArray.length - 1];
-	if (issueKey.substring(0, issueKey.indexOf('?')) != '') {
-		issueKey = issueKey.substring(0, issueKey.indexOf('?'));
-	}
-	console.log("key:" + issueKey);
+	var issueKey = JIRA.Issue.getIssueKey();
 	buildTreantIssueTabPanel(issueKey);
 }
 
-function buildTreantIssueTabPanel(decisionKnowledgeElementKey) {
-	//var depthOfTree = document.getElementById("depthOfTreeInput").value;
+function buildTreantIssueTabPanel(elementKey) {
+	// var depthOfTree = document.getElementById("depthOfTreeInput").value;
 	var depthOfTree = 4;
-	getTreant(decisionKnowledgeElementKey, depthOfTree, function(treant) {
+	getTreant(elementKey, depthOfTree, function(treeStructure) {
 		document.getElementById("treant-container").innerHTML = "";
-		new Treant(treant);
 
-		//createContextMenuForTreantNodes();
-		//addDragAndDropSupportForTreant();
+		var commitData = getData(AJS.contextPath() + "/rest/gitplugin/1.0/issues/" + elementKey + "/commits");
+		var commits = commitData.commits;
+		if (commits.length > 0) {
+			treeStructure.nodeStructure.children = addCommits(commits, treeStructure.nodeStructure.children);
+		}
+
+		console.log(treeStructure);
+
+		new Treant(treeStructure);
+		// createContextMenuForTreantNodes();
+		// addDragAndDropSupportForTreant();
 	});
 }
-
-//function buildTreantIssueTabPanel(issueKey) {
-//	console.log(issueKey);
-//
-//	var treeStructure = [];
-//
-//	var infoData = getData(AJS.contextPath() + "/rest/api/latest/issue/" + issueKey);
-//
-//	var thisLevelElement = {
-//		text : {
-//			title : infoData.fields.issuetype.name,
-//			name : infoData.fields.summary
-//		}
-//	};
-//	treeStructure.push(thisLevelElement);
-//
-//	var linkedIssues = infoData.fields.issuelinks;
-//
-//	if (linkedIssues) {
-//		treeStructure = checkForTopLevelElements(treeStructure, linkedIssues, thisLevelElement);
-//	}
-//
-//	var commitData = getData(AJS.contextPath() + "/rest/gitplugin/1.0/issues/" + issueKey + "/commits");
-//	var commits = commitData.commits;
-//	if (commits.length > 0) {
-//		treeStructure = addCommits(commits, thisLevelElement, treeStructure);
-//	}
-//	console.log(treeStructure);
-//
-//	//New Try
-//    getTreant(treeStructure ,function(treant) {
-//        document.getElementById("treant-container").innerHTML = "";
-//        console.log(treant);
-//        treantTree = new Treant(treant);
-//    });
-//}
 
 function getData(url) {
 	var request = new XMLHttpRequest(); // a new request
@@ -70,83 +35,50 @@ function getData(url) {
 	return JSON.parse(data);
 }
 
-function checkForTopLevelElements(tree_structure, linkedIssues, thisLevelElement) {
-	var element;
-	linkedIssues.forEach(function(linkedIssue) {
-		if (linkedIssue.inwardIssue) {
-			element = {
-				parent : thisLevelElement,
-				HTMLclass : 'blue',
-				text : {
-					title : linkedIssue.inwardIssue.fields.issuetype.name,
-					name : linkedIssue.inwardIssue.fields.summary
-				}
-			};
-			tree_structure.push(element);
-		} else if (linkedIssue.outwardIssue) {
-			element = {
-				parent : thisLevelElement,
-				HTMLclass : 'red',
-				text : {
-					title : linkedIssue.outwardIssue.fields.issuetype.name,
-					name : linkedIssue.outwardIssue.fields.summary
-				}
-			};
-			tree_structure.push(element);
-			var commitData = getData(AJS.contextPath() + "/rest/gitplugin/1.0/issues/" + linkedIssue.outwardIssue.key
-					+ "/commits");
-			var commits = commitData.commits;
-			if (commits.length > 0) {
-				tree_structure = addCommits(commits, element, tree_structure);
-			}
-		}
-	});
-	return tree_structure;
-}
-
-function openDetails(commit) {
-	url = AJS.contextPath() + "/secure/bbb.gp.gitviewer.Commit.jspa?repoId=" + commit.repository.id + "&commitId="
-			+ commit.commitId;
-	window.open(url);
-}
-
-function addCommits(commits, parentElement, tree_structure) {
+function addCommits(commits, elementArray) {
 	commits.forEach(function(commit) {
 		var message = commit.message;
 
 		var splitMessage = message.split("@");
+		splitMessage.shift();
 
 		var decision;
 		var element;
-		for ( var i in splitMessage) {
+		for (var i in splitMessage) {
 			var split = splitMessage[i].split(" ");
 			var message = splitMessage[i].substr(splitMessage[i].indexOf(" ") + 1);
 			switch (split[0]) {
 			case "Decision:":
 				decision = {
-					parent : parentElement,
-					HTMLclass : 'yellow',
+					children : [],
+					HTMLclass : 'decision',
 					innerHTML : "<p class=\"node-title\">Decision:</p><p class=\"node-name\">" + message
 							+ "</p><a href=\"#\" id=\"" + commit.commitId + "\">More Information</a>"
 				};
 				$(document).on('click', '#' + commit.commitId + '', function() {
 					openDetails(commit);
 				});
-				tree_structure.push(decision);
+				elementArray.push(decision);
 				break;
 			default:
 				element = {
-					parent : decision,
-					HTMLclass : 'yellow',
+					children : [],
+					HTMLclass : 'decision',
 					text : {
 						title : split[0],
 						name : message
 					}
 				};
-				tree_structure.push(element);
+				elementArray.push(element);
 				break;
 			}
 		}
 	});
-	return tree_structure;
+	return elementArray;
+}
+
+function openDetails(commit) {
+	url = AJS.contextPath() + "/secure/bbb.gp.gitviewer.Commit.jspa?repoId=" + commit.repository.id + "&commitId="
+			+ commit.commitId;
+	window.open(url);
 }
