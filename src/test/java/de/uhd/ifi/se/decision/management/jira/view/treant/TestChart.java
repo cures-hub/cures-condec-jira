dc84a3f63e526f2c4f29d370c9aa9fc0a6242bea
Tim <tim.kuchenbuch@gmx.net>
2018-07-03 08:19:43 +0200

    ConDec-68: Test getter methods in Chart class

--- src/test/java/de/uhd/ifi/se/decision/management/jira/view/treant/TestChart.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/view/treant/TestChart.java
@@ -1,14 +1,58 @@
 package de.uhd.ifi.se.decision.management.jira.view.treant;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import com.google.common.collect.ImmutableMap;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestChart {
 
+	private Chart chart;
+
+	@Before
+	public void setUp(){
+		chart = new Chart();
+	}
+
 	@Test
 	public void testExist() {
-		Chart chart = new Chart();
 		assertNotNull(chart);
+	}
+
+	@Test
+	public void testGetContainer(){
+		assertEquals("#treant-container", chart.getContainer());
+	}
+
+	@Test
+	public void testGetConnectors(){
+		assertEquals(ImmutableMap.of("type", "straight"), chart.getConnectors());
+	}
+
+	@Test
+	public void testGetRootOrientation(){
+		assertEquals("NORTH", chart.getRootOrientation());
+	}
+
+	@Test
+	public void testGetLevelSeparation(){
+		assertEquals(30, chart.getLevelSeparation(), 0.0);
+	}
+
+	@Test
+	public void testGetSiblingSeparation(){
+		assertEquals(30, chart.getSiblingSeparation(), 0.0);
+	}
+
+	@Test
+	public void testGetSubTreeSeparation(){
+		assertEquals(30, chart.getSubTreeSeparation(), 0.0);
+	}
+
+	@Test
+	public void testGetNode(){
+		assertEquals(ImmutableMap.of("collapsable", true), chart.getNode());
 	}
 }
