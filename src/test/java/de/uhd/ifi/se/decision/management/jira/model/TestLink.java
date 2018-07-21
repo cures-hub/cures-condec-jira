1f7edf02918974eff2cdca222ea6e2f09492c3fe
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2018-07-21 17:47:44 +0200

    ConDec-28: Refactor unit tests for Link model class

--- src/test/java/de/uhd/ifi/se/decision/management/jira/model/TestLink.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/model/TestLink.java
@@ -1,60 +1,126 @@
 package de.uhd.ifi.se.decision.management.jira.model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import com.atlassian.jira.issue.link.IssueLink;
+
+import de.uhd.ifi.se.decision.management.jira.mocks.MockIssueLink;
+import de.uhd.ifi.se.decision.management.jira.persistence.LinkEntity;
+
 /**
- * @description Test class for link getter and setter methods
+ * Test class for links between decision knowledge elements
  */
 public class TestLink {
-	private String linkType;
-	private long ingoingId;
-	private long outgoingId;
+	private String type;
+	private long idOfSourceElement;
+	private long idOfDestinationElement;
 	private Link link;
 
 	@Before
 	public void setUp() {
-		linkType = "Test";
-		ingoingId = 100;
-		outgoingId = 100;
+		type = "contain";
+		idOfSourceElement = 14;
+		idOfDestinationElement = 15;
 		link = new LinkImpl();
-		link.setType(linkType);
-		link.setSourceElement(ingoingId);
-		link.setDestinationElement(outgoingId);
+		link.setType(type);
+		DecisionKnowledgeElement sourceElement = new DecisionKnowledgeElementImpl();
+		sourceElement.setId(idOfSourceElement);
+		sourceElement.setKey("TestSourceElement");
+		DecisionKnowledgeElement destinationElement = new DecisionKnowledgeElementImpl();
+		destinationElement.setId(idOfDestinationElement);
+		destinationElement.setKey("TestDestinationElement");
+		link.setSourceElement(sourceElement);
+		link.setDestinationElement(destinationElement);
 	}
 
 	@Test
-	public void testgetLinkType() {
-		assertEquals(this.linkType, this.link.getType());
+	public void testGetType() {
+		assertEquals(type, link.getType());
 	}
 
 	@Test
-	public void testGetIngoingId() {
-		assertEquals(this.ingoingId, this.link.getSourceElement().getId(), 0.0);
+	public void testSetType() {
+		link.setType(type + "New");
+		assertEquals(type + "New", link.getType());
 	}
 
 	@Test
-	public void testGetoutGoingId() {
-		assertEquals(this.outgoingId, this.link.getDestinationElement().getId(), 0.0);
+	public void testGetIdOfSourceElement() {
+		assertEquals(idOfSourceElement, link.getSourceElement().getId(), 0.0);
 	}
 
 	@Test
-	public void testSetLinkType() {
-		this.link.setType(this.linkType + "New");
-		assertEquals(this.linkType + "New", this.link.getType());
+	public void testGetIdOfDestinationElement() {
+		assertEquals(idOfDestinationElement, link.getDestinationElement().getId(), 0.0);
 	}
 
 	@Test
-	public void testSetIngoingId() {
-		this.link.setSourceElement(ingoingId + 1);
-		assertEquals(this.ingoingId + 1, this.link.getSourceElement().getId(), 0.0);
+	public void testSetSourceElementById() {
+		link.setSourceElement(idOfSourceElement + 1);
+		assertEquals(idOfSourceElement + 1, link.getSourceElement().getId(), 0.0);
 	}
 
 	@Test
-	public void testSetOutgoingId() {
-		this.link.setDestinationElement(outgoingId + 1);
-		assertEquals(this.outgoingId + 1, this.link.getDestinationElement().getId(), 0.0);
+	public void testSetDestinationElementById() {
+		link.setDestinationElement(idOfDestinationElement + 1);
+		assertEquals(idOfDestinationElement + 1, link.getDestinationElement().getId(), 0.0);
 	}
+
+	@Test
+	public void testGetSourceElement() {
+		assertEquals("TestSourceElement", link.getSourceElement().getKey());
+	}
+
+	@Test
+	public void testSetSourceElement() {
+		DecisionKnowledgeElement element = new DecisionKnowledgeElementImpl();
+		element.setKey("TestNew");
+		link.setSourceElement(element);
+		assertEquals(element.getKey(), link.getSourceElement().getKey());
+	}
+
+	@Test
+	public void testGetDestinationElement() {
+		assertEquals("TestDestinationElement", link.getDestinationElement().getKey());
+	}
+
+	@Test
+	public void testSetDestinationElement() {
+		DecisionKnowledgeElement element = new DecisionKnowledgeElementImpl();
+		element.setKey("TestNew");
+		link.setDestinationElement(element);
+		assertEquals(element.getKey(), link.getDestinationElement().getKey());
+	}
+
+	@Test
+	public void testConstructorLinkEntity() {
+		LinkEntity linkEntity = mock(LinkEntity.class);
+		Link link = new LinkImpl(linkEntity);
+		assertNotNull(link);
+	}
+
+	@Test
+	public void testConstructorDecisionKnowledgeElement() {
+		DecisionKnowledgeElement sourceElement = new DecisionKnowledgeElementImpl();
+		sourceElement.setId(14);
+		sourceElement.setKey("TestSourceElement");
+		DecisionKnowledgeElement destinationElement = new DecisionKnowledgeElementImpl();
+		destinationElement.setId(15);
+		destinationElement.setKey("TestDestinationElement");
+		Link link = new LinkImpl(sourceElement, destinationElement);
+		assertEquals("TestSourceElement", link.getSourceElement().getKey());
+	}
+
+	@Test
+	public void testConstructorIssueLink() {
+		IssueLink issueLink = new MockIssueLink((long) 54);
+		Link link = new LinkImpl(issueLink);
+		assertNotNull(link);
+	}
+
 }
