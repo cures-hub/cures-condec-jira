c1c9450a24af074a95a9bf9a308ab8f4a536dfa6
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2018-07-29 20:17:04 +0200

    ConDec-90: Remove unused imports for issue module in atlassian-plugin.xml
    [decision] Issue module is not interactive (no context menu and D&D) since there is a jQuery conflict with the internal JIRA jQuery version that we could not resolve [/decision]

--- src/main/resources/js/view.tree.viewer.js
+++ src/main/resources/js/view.tree.viewer.js
@@ -1,9 +1,8 @@
 function buildTreeViewer() {
 	resetTreeViewer();
-	resizeTreeViewer();
 	var rootElementType = $("select[name='select-root-element-type']").val();
 	getTreeViewer(rootElementType, function(core) {
-		$('#jstree').jstree({
+		$("#jstree").jstree({
 			"core" : core,
 			"plugins" : [ "dnd", "contextmenu", "wholerow", "sort", "search" ],
 			"search" : {
@@ -15,14 +14,15 @@ function buildTreeViewer() {
 		});
 		$("#jstree-search-input").keyup(function() {
 			var searchString = $(this).val();
-			$('#jstree').jstree(true).search(searchString);
+			$("#jstree").jstree(true).search(searchString);
 		});
 	});
+	makeTreeViewerResizable();
 	addDragAndDropSupportForTreeViewer();
 }
 
 function resetTreeViewer() {
-	var treeViewer = $('#jstree').jstree(true);
+	var treeViewer = $("#jstree").jstree(true);
 	if (treeViewer) {
 		treeViewer.destroy();
 	}
@@ -32,12 +32,12 @@ function getTreeViewerNodeById(nodeId) {
 	if (nodeId === "#") {
 		return nodeId;
 	}
-	return $('#jstree').jstree(true).get_node(nodeId);
+	return $("#jstree").jstree(true).get_node(nodeId);
 }
 
 function selectNodeInTreeViewer(nodeId) {
-	$('#jstree').on("ready.jstree", function() {
-		var treeViewer = $('#jstree').jstree(true);
+	$("#jstree").on("ready.jstree", function() {
+		var treeViewer = $("#jstree").jstree(true);
 		if (treeViewer) {
 			treeViewer.select_node(nodeId);
 		}
@@ -45,7 +45,7 @@ function selectNodeInTreeViewer(nodeId) {
 }
 
 function addDragAndDropSupportForTreeViewer() {
-	$('#jstree').on('move_node.jstree', function(object, nodeInContext) {
+	$("#jstree").on('move_node.jstree', function(object, nodeInContext) {
 		var node = nodeInContext.node;
 		var parentNode = getTreeViewerNodeById(nodeInContext.parent);
 		var oldParentNode = getTreeViewerNodeById(nodeInContext.old_parent);
@@ -67,8 +67,6 @@ function addDragAndDropSupportForTreeViewer() {
 	});
 }
 
-function resizeTreeViewer() {
-    $(function () {
-        $("#jstree").resizable();
-    });
+function makeTreeViewerResizable() {
+	$("#jstree").resizable();
 }
