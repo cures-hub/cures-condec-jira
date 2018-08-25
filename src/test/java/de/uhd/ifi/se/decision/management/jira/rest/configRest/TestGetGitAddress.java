9464313eb3a6bae38e0a05f45ef94e01e360ea1b
Tim <tim.kuchenbuch@gmx.net>
2018-08-25 07:51:26 +0200

    ConDec-25: Add new test for the new functions of configRest

---
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/rest/configRest/TestGetGitAddress.java
@@ -0,0 +1,33 @@
+package de.uhd.ifi.se.decision.management.jira.rest.configRest;
+
+import net.java.ao.test.junit.ActiveObjectsJUnitRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(ActiveObjectsJUnitRunner.class)
+public class TestGetGitAddress extends TestConfigSuper {
+
+    @Test
+    public void testProNull(){
+        assertEquals(getBadRequestResponse(INVALID_PROJECTKEY).getStatus(),confRest.getGitAddress(null).getStatus());
+    }
+
+    @Test
+    public void testProEmpty(){
+        assertEquals(getBadRequestResponse(INVALID_PROJECTKEY).getStatus(),confRest.getGitAddress("").getStatus());
+    }
+
+    @Test
+    public void testProjectKeyInvalid(){
+        assertEquals(Response.status(Response.Status.OK).build().getStatus(),confRest.getGitAddress("InvalidKey").getStatus());
+    }
+
+    @Test
+    public void testProjectKeyValid(){
+        assertEquals(Response.status(Response.Status.OK).build().getStatus(),confRest.getGitAddress("TEST").getStatus());
+    }
+}
---
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/rest/configRest/TestSetWebhookData.java
@@ -0,0 +1,109 @@
+package de.uhd.ifi.se.decision.management.jira.rest.configRest;
+
+import net.java.ao.test.junit.ActiveObjectsJUnitRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(ActiveObjectsJUnitRunner.class)
+public class TestSetWebhookData extends TestConfigSuper{
+
+    @Test
+    public void testReqNullProNullAdrNullSecNull(){
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),
+                confRest.setWebhookData(null,null,null, null).getStatus());
+    }
+
+    @Test
+    public void testReqNullProNullAdrNullSecFilled(){
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),
+                confRest.setWebhookData(null,null,null, "TEST").getStatus());
+    }
+
+    @Test
+    public void testReqNullProNullAdrFilledSecNull(){
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),
+                confRest.setWebhookData(null,null,"TEST", null).getStatus());
+    }
+
+    @Test
+    public void testReqNullProNullAdrFilledSecFilled(){
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),
+                confRest.setWebhookData(null,null,"TEST", "TEST").getStatus());
+    }
+
+    @Test
+    public void testReqNullProFilledAdrNullSecNull(){
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),
+                confRest.setWebhookData(null,"TEST",null, null).getStatus());
+    }
+
+    @Test
+    public void testReqNullProFilledAdrNullSecFilled(){
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),
+                confRest.setWebhookData(null,"TEST",null, "TEST").getStatus());
+    }
+
+    @Test
+    public void testReqNullProFilledAdrFilledSecNull(){
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),
+                confRest.setWebhookData(null,"TEST","TEST", null).getStatus());
+    }
+
+    @Test
+    public void testReqNullProFilledAdrFilledSecFilled(){
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),
+                confRest.setWebhookData(null,"TEST","TEST", "TEST").getStatus());
+    }
+
+    @Test
+    public void testReqFilledProNullAdrNullSecNull(){
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),
+                confRest.setWebhookData(request,null,null, null).getStatus());
+    }
+
+    @Test
+    public void testReqFilledProNullAdrNullSecFilled(){
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),
+                confRest.setWebhookData(request,null,null, "TEST").getStatus());
+    }
+
+    @Test
+    public void testReqFilledProNullAdrFilledSecNull(){
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),
+                confRest.setWebhookData(request,null,"TEST", null).getStatus());
+    }
+
+    @Test
+    public void testReqFilledProNullAdrFilledSecFilled(){
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),
+                confRest.setWebhookData(request,null,"TEST", "TEST").getStatus());
+    }
+
+    @Test
+    public void testReqFilledProFilledAdrNullSecNull(){
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),
+                confRest.setWebhookData(request,"TEST",null, null).getStatus());
+    }
+
+    @Test
+    public void testReqFilledProFilledAdrNullSecFilled(){
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),
+                confRest.setWebhookData(request,"TEST",null, "TEST").getStatus());
+    }
+
+    @Test
+    public void testReqFilledProFilledAdrFilledSecNull(){
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),
+                confRest.setWebhookData(request,"TEST","TEST", null).getStatus());
+    }
+
+    @Test
+    public void testReqFilledProFilledAdrFilledSecFilled(){
+        assertEquals(Response.Status.OK.getStatusCode(),
+                confRest.setWebhookData(request,"TEST","TEST", "TEST").getStatus());
+    }
+}
