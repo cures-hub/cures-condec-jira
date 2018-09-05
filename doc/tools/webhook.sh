a50734df4bd937f5981021576ca029f94abf4cb9
Eleftherios Matios <e.matios@gmail.com>
2018-09-05 13:03:15 +0200

    Escape payload data

--- doc/tools/webhook.sh
+++ doc/tools/webhook.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
 WEBHOOK_SECRET=$1
-if [ -z "$WEBHOOK_SECRET" ]; then
-	echo "Please provide the secret token"
+API_ENDPOINT=$2
+
+if [ "$#" -ne 2 ]; then
+	echo "Please provide the secret token and the api endpoint"
+    echo "Example: ./webhook SECRETTOKEN https://apiendpoint"
 	exit 1
 fi
+
 
 refname=123456
 
 tmpfile=$(mktemp --suffix=.json)
 
 #data="{\"issueKey\": \"CONDEC-1234\", \"ConDeTree\": {}}"
-data={}
+data='{"issueKey": "CONDEC-1234", "ConDeTree": {"chart":{"container":"#treant-container","connectors":{"type":"straight"},"rootOrientation":"NORTH","levelSeparation":30,"siblingSeparation":30,"subTreeSeparation":30,"node":{"collapsable":true}},"nodeStructure":{"children":[{"children":[],"text":{"name":"Issue","title":"How can we detect homologous points on stereo images?","desc":"RAGRAM-2"},"HTMLclass":"problem","HTMLid":11605},{"children":[{"children":[{"text":{"name":"Alternative","title":"Use OpenCV library","desc":"RAGRAM-4"},"HTMLclass":"solution","HTMLid":11607}],"text":{"name":"Argument","title":"Users might not have installed this library","desc":"RAGRAM-7"},"HTMLclass":"rationale","HTMLid":11610},{"children":[],"text":{"name":"Constraint","title":"Users might not have installed this library","desc":"RAGRAM-5"},"HTMLclass":"context","HTMLid":11608}],"text":{"name":"Solution","title":"Use own implementation","desc":"RAGRAM-3"},"HTMLclass":"solution","HTMLid":11606},{"children":[{"children":[],"text":{"name":"Argument","title":"Less implementation effort","desc":"RAGRAM-6"},"HTMLclass":"rationale","HTMLid":11609}],"text":{"name":"Alternative","title":"Use OpenCV library","desc":"RAGRAM-4"},"HTMLclass":"solution","HTMLid":11607}],"text":{"name":"Decision","title":"Implement image matching based on ZNCC","desc":"RAGRAM-1"},"HTMLclass":"decision","HTMLid":11604},"graph":{"rootElement":{"id":11604,"summary":"Implement image matching based on ZNCC","description":"Image matching is implemented using ZNCC, as it is done here: https://martin-thoma.com/zero-mean-normalized-cross-correlation/","type":"Decision","key":"RAGRAM-1"}}}}'
+data={\"issueKey\": \"CONDEC-1234\", \"ConDeTree\": {\"chart\":{\"container\":\"#treant-container\",\"connectors\":{\"type\":\"straight\"},\"rootOrientation\":\"NORTH\",\"levelSeparation\":30,\"siblingSeparation\":30,\"subTreeSeparation\":30,\"node\":{\"collapsable\":true}},\"nodeStructure\":{\"children\":[{\"children\":[],\"text\":{\"name\":\"Issue\",\"title\":\"How can we detect homologous points on stereo images?\",\"desc\":\"RAGRAM-2\"},\"HTMLclass\":\"problem\",\"HTMLid\":11605},{\"children\":[{\"children\":[{\"text\":{\"name\":\"Alternative\",\"title\":\"Use OpenCV library\",\"desc\":\"RAGRAM-4\"},\"HTMLclass\":\"solution\",\"HTMLid\":11607}],\"text\":{\"name\":\"Argument\",\"title\":\"Users might not have installed this library\",\"desc\":\"RAGRAM-7\"},\"HTMLclass\":\"rationale\",\"HTMLid\":11610},{\"children\":[],\"text\":{\"name\":\"Constraint\",\"title\":\"Users might not have installed this library\",\"desc\":\"RAGRAM-5\"},\"HTMLclass\":\"context\",\"HTMLid\":11608}],\"text\":{\"name\":\"Solution\",\"title\":\"Use own implementation\",\"desc\":\"RAGRAM-3\"},\"HTMLclass\":\"solution\",\"HTMLid\":11606},{\"children\":[{\"children\":[],\"text\":{\"name\":\"Argument\",\"title\":\"Less implementation effort\",\"desc\":\"RAGRAM-6\"},\"HTMLclass\":\"rationale\",\"HTMLid\":11609}],\"text\":{\"name\":\"Alternative\",\"title\":\"Use OpenCV library\",\"desc\":\"RAGRAM-4\"},\"HTMLclass\":\"solution\",\"HTMLid\":11607}],\"text\":{\"name\":\"Decision\",\"title\":\"Implement image matching based on ZNCC\",\"desc\":\"RAGRAM-1\"},\"HTMLclass\":\"decision\",\"HTMLid\":11604},\"graph\":{\"rootElement\":{\"id\":11604,\"summary\":\"Implement image matching based on ZNCC\",\"description\":\"Image matching is implemented using ZNCC, as it is done here: https://martin-thoma.com/zero-mean-normalized-cross-correlation/\",\"type\":\"Decision\",\"key\":\"RAGRAM-1\"}}}}
 echo $data
 
 sig=$(echo -n $data | openssl dgst -sha256 -hmac $WEBHOOK_SECRET | awk '{print "X-Hub-Signature: sha256="$2}')
 echo $sig
 
-curl -X POST -H "Content-Type: application/json" -H  "accept: application/json" -H "$sig" --data "$data" https://cuu-staging.ase.in.tum.de/api/v1/projects/ConDecDev/integrations/conDec
+curl -X POST -H "Content-Type: application/json" -H  "accept: application/json" -H "$sig" --data "$data" $API_ENDPOINT
 
 rm -f "${tmpfile}"
+echo ""
+
