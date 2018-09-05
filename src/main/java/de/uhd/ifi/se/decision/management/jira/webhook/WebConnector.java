7eb08fec9755833c8c5980104447ed3290edb450
dekome <tim.kuchenbuch@gmx.net>
2018-09-05 11:56:44 +0200

    ConDec-100: Add new test function for webhook Submit

--- src/main/java/de/uhd/ifi/se/decision/management/jira/webhook/WebConnector.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/webhook/WebConnector.java
@@ -1,12 +1,22 @@
 package de.uhd.ifi.se.decision.management.jira.webhook;
 
+import com.atlassian.jira.util.json.JSONObject;
 import de.uhd.ifi.se.decision.management.jira.persistence.ConfigPersistence;
 import org.apache.commons.httpclient.*;
 import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WebConnector{
     private static final Logger LOGGER = LoggerFactory.getLogger(WebConnector.class);
@@ -89,14 +99,29 @@ public class WebConnector{
         this.secret = secret;
     }
 
+    public void submitTestPost(String projectKey, String gitHash){
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(this.url);
+        httpPost.setHeader("accept", "application/json");
+        WebBodyProvider provider = new WebBodyProvider(projectKey,gitHash);
+        List<BasicNameValuePair> nvps = provider.getNvm();
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+            CloseableHttpResponse response2 = httpclient.execute(httpPost);
+            System.out.println(response2.getStatusLine());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private boolean submitPostMethod(){
         try {
             HttpClient httpClient = new HttpClient();
-            
             Header header = new Header();
             header.setName("X-Hub-Signature");
             header.setValue(this.secret);
-            postMethod.setRequestHeader(header);
             postMethod.setRequestHeader(header);
             postMethod.setURI(new HttpsURL(url));
             int respEntity = httpClient.executeMethod(postMethod);
