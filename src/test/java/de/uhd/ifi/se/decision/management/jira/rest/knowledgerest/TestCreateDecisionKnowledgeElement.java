12bf609de010268312fa4d510e6d2978f3d6173d
Tim <tim.kuchenbuch@gmx.net>
2018-09-11 08:27:56 +0200

    ConDec-100: Change Test for KnowledgeRest

--- src/test/java/de/uhd/ifi/se/decision/management/jira/rest/knowledgerest/TestCreateDecisionKnowledgeElement.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/rest/knowledgerest/TestCreateDecisionKnowledgeElement.java
@@ -5,6 +5,11 @@ import static org.junit.Assert.assertEquals;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import de.uhd.ifi.se.decision.management.jira.ComponentGetter;
+import de.uhd.ifi.se.decision.management.jira.TestSetUp;
+import de.uhd.ifi.se.decision.management.jira.mocks.MockTransactionTemplateWebhook;
+import net.java.ao.test.jdbc.Data;
+import net.java.ao.test.jdbc.NonTransactional;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +19,7 @@ import com.google.common.collect.ImmutableMap;
 import net.java.ao.test.junit.ActiveObjectsJUnitRunner;
 
 @RunWith(ActiveObjectsJUnitRunner.class)
+@Data(TestSetUp.AoSentenceTestDatabaseUpdater.class)
 public class TestCreateDecisionKnowledgeElement extends TestKnowledgeRestSetUp {
 
 	private final static String CREATION_ERROR = "Creation of decision knowledge element failed.";
@@ -41,9 +47,11 @@ public class TestCreateDecisionKnowledgeElement extends TestKnowledgeRestSetUp {
 	}
 
 	@Ignore
+    @NonTransactional
 	public void testRequestFilledElementFilled() {
 		request.setAttribute("WithFails", false);
 		request.setAttribute("NoFails", true);
+		ComponentGetter.setTransactionTemplate(new MockTransactionTemplateWebhook());
 		assertEquals(Status.OK.getStatusCode(),
 				knowledgeRest.createDecisionKnowledgeElement(request, decisionKnowledgeElement).getStatus());
 	}
