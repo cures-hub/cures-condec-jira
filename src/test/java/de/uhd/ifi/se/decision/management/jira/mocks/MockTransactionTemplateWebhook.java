49598b9b2f59d124f769d028abfe832d3a65a6ff
Tim <tim.kuchenbuch@gmx.net>
2018-09-13 10:53:36 +0200

    Create new Version without bugs

--- src/test/java/de/uhd/ifi/se/decision/management/jira/mocks/MockTransactionTemplateWebhook.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/mocks/MockTransactionTemplateWebhook.java
@@ -3,7 +3,7 @@ package de.uhd.ifi.se.decision.management.jira.mocks;
 import com.atlassian.sal.api.transaction.TransactionCallback;
 import com.atlassian.sal.api.transaction.TransactionTemplate;
 
-public class MockTransactionTemplateWebHook implements TransactionTemplate {
+public class MockTransactionTemplateWebhook implements TransactionTemplate {
 	@Override
 	@SuppressWarnings("unchecked")
 	public <T> T execute(TransactionCallback<T> arg0) {
