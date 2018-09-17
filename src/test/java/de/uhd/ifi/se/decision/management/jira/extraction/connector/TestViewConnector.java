a3bf3a1d76d06a3d5fdac8b4d252f6c7684fb27d
Jochen <J.Clormann@stud.uni-heidelberg.de>
2018-09-17 23:17:38 +0200

    ConDec-135: Create TestClass for ViewConnector class

---
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/connector/TestViewConnector.java
@@ -0,0 +1,40 @@
+package de.uhd.ifi.se.decision.management.jira.extraction.connector;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.atlassian.activeobjects.test.TestActiveObjects;
+
+import de.uhd.ifi.se.decision.management.jira.TestComponentGetter;
+import de.uhd.ifi.se.decision.management.jira.TestSetUp;
+import de.uhd.ifi.se.decision.management.jira.extraction.model.TestSentence;
+import de.uhd.ifi.se.decision.management.jira.mocks.MockDefaultUserManager;
+import de.uhd.ifi.se.decision.management.jira.mocks.MockTransactionTemplate;
+import net.java.ao.EntityManager;
+import net.java.ao.test.jdbc.Data;
+import net.java.ao.test.junit.ActiveObjectsJUnitRunner;
+
+@RunWith(ActiveObjectsJUnitRunner.class)
+@Data(TestSentence.AoSentenceTestDatabaseUpdater.class) 
+public class TestViewConnector extends TestSetUp {
+	
+
+	private EntityManager entityManager;
+
+	@Before
+	public void setUp() {
+		initialization();
+		TestComponentGetter.init(new TestActiveObjects(entityManager), new MockTransactionTemplate(),
+				new MockDefaultUserManager());
+	}
+	
+	@Test
+	public void testConstructor() {
+		ViewConnector vc = new ViewConnector(null, false);
+		assertNotNull(vc);
+	}
+
+}
