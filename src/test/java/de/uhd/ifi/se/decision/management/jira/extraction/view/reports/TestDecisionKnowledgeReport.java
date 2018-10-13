a752469fbdd7b2b5f8e463ccac9324a8f9397413
Jochen <J.Clormann@stud.uni-heidelberg.de>
2018-10-13 15:01:49 +0200

    ConDec-294: Fix codacy issue

--- src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/view/reports/TestDecisionKnowledgeReport.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/view/reports/TestDecisionKnowledgeReport.java
@@ -8,8 +8,6 @@ import java.util.Map;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.ofbiz.core.entity.GenericEntityException;
-
 import com.atlassian.activeobjects.test.TestActiveObjects;
 import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.project.ProjectManager;
