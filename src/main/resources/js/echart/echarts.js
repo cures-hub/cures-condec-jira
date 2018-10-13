cbd27c7442931ca49894b3717e20ecf5356e8946
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2018-10-13 08:36:42 +0200

    ConDec-294: Disable eslint code analysis in echarts.js and dataTool.js

--- src/main/resources/js/echart/echarts.js
+++ src/main/resources/js/echart/echarts.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
