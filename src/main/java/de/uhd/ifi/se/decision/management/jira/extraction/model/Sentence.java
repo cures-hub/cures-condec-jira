da871129a0fc9af48cc96918a9f0162c0d3924e8
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2018-10-31 18:55:40 +0100

    ConDec-372: Add JsonIgnore annotations to every method due to "object is not an instance of declaring class" exception
    [issue] Some of the exported elements don't have an id and have an undefined key. Why? [/issue]

--- src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/model/Sentence.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/model/Sentence.java
@@ -2,75 +2,105 @@ package de.uhd.ifi.se.decision.management.jira.extraction.model;
 
 import java.util.Date;
 
-import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.annotate.JsonIgnore;
 
 import de.uhd.ifi.se.decision.management.jira.model.DecisionKnowledgeElement;
-import de.uhd.ifi.se.decision.management.jira.model.DecisionKnowledgeElementImpl;
 
-@JsonSerialize(as = DecisionKnowledgeElementImpl.class)
 public interface Sentence extends DecisionKnowledgeElement {
 
+	@JsonIgnore
 	boolean isRelevant();
 
+	@JsonIgnore
 	void setRelevant(boolean isRelevant);
 
+	@JsonIgnore
 	void setRelevant(Double prediction);
 
+	@JsonIgnore
 	boolean isTagged();
 
+	@JsonIgnore
 	void setTagged(boolean isTagged);
 
+	@JsonIgnore
 	boolean isTaggedManually();
 
+	@JsonIgnore
 	void setTaggedManually(boolean isTaggedManually);
 
+	@JsonIgnore
 	boolean isTaggedFineGrained();
 
+	@JsonIgnore
 	void setTaggedFineGrained(boolean isTaggedFineGrained);
 
+	@JsonIgnore
 	long getCommentId();
 
+	@JsonIgnore
 	void setCommentId(long id);
 
+	@JsonIgnore
 	long getUserId();
 
+	@JsonIgnore
 	void setUserId(long id);
 
+	@JsonIgnore
 	int getStartSubstringCount();
 
+	@JsonIgnore
 	void setStartSubstringCount(int count);
 
+	@JsonIgnore
 	int getEndSubstringCount();
 
+	@JsonIgnore
 	void setEndSubstringCount(int count);
 
+	@JsonIgnore
 	String getKnowledgeTypeString();
 
+	@JsonIgnore
 	void setKnowledgeTypeString(String type);
 
+	@JsonIgnore
 	void setKnowledgeType(double[] prediction);
 
+	@JsonIgnore
 	void setArgument(String argument);
 
+	@JsonIgnore
 	String getArgument();
 
+	@JsonIgnore
 	String getProjectKey();
 
+	@JsonIgnore
 	void setProjectKey(String key);
 
+	@JsonIgnore
 	void setIssueId(long issueid);
 
+	@JsonIgnore
 	long getIssueId();
 
+	@JsonIgnore
 	boolean isPlainText();
 
+	@JsonIgnore
 	void setPlainText(boolean isPlainText);
 
+	@JsonIgnore
 	String getBody();
 
+	@JsonIgnore
 	void setBody(String body);
 
+	@JsonIgnore
 	Date getCreated();
 
+	@JsonIgnore
 	void setCreated(Date date);
 }
