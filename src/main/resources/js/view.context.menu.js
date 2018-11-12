6d450e61507f0eed97723523dca91e6097b5b3fc
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2018-11-12 11:26:22 +0100

    ConDec-352: Replace global with window in view.context.menu.js since this is not a closure object yet

--- src/main/resources/js/view.context.menu.js
+++ src/main/resources/js/view.context.menu.js
@@ -397,10 +397,10 @@ var contextMenuSetAsRootAction = {
 	"name" : makeRootText,
 	"callback" : function(key, options) {
 		var id = getSelectedTreantNodeId(options);
-		if (global.conDecIssueModule !== undefined) {
-			global.conDecIssueModule.setAsRootElement(id);
-		} else if (global.conDecKnowledgePage !== undefined) {
-			global.conDecKnowledgePage.setAsRootElement(id);
+		if (window.conDecIssueModule !== undefined) {
+			window.conDecIssueModule.setAsRootElement(id);
+		} else if (window.conDecKnowledgePage !== undefined) {
+			window.conDecKnowledgePage.setAsRootElement(id);
 		}		
 	}
 };
@@ -410,8 +410,8 @@ var contextMenuOpenJiraIssueAction = {
 	"name" : openIssueText,
 	"callback" : function(key, options) {
 		var id = getSelectedTreantNodeId(options);
-		if (global.conDecKnowledgePage !== undefined) {
-			global.conDecKnowledgePage.openIssue(id);
+		if (window.conDecKnowledgePage !== undefined) {
+			window.conDecKnowledgePage.openIssue(id);
 		}				
 	}
 };
