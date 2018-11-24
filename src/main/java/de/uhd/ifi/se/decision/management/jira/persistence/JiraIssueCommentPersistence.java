c0122cd28ba4e027f23d26cd82c4cde282ac4e7d
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2018-11-24 22:12:12 +0100

    ConDec-359: Add the JiraIssueCommentPersistence class that extends the AbstractPersistenceManager

---
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/persistence/JiraIssueCommentPersistence.java
@@ -0,0 +1,91 @@
+package de.uhd.ifi.se.decision.management.jira.persistence;
+
+import java.util.List;
+
+import com.atlassian.jira.user.ApplicationUser;
+
+import de.uhd.ifi.se.decision.management.jira.model.DecisionKnowledgeElement;
+import de.uhd.ifi.se.decision.management.jira.model.Link;
+
+public class JiraIssueCommentPersistence extends AbstractPersistenceManager {
+
+	private String projectKey;
+
+	public JiraIssueCommentPersistence(String projectKey) {
+		this.projectKey = projectKey;
+	}
+
+	@Override
+	public boolean deleteDecisionKnowledgeElement(DecisionKnowledgeElement element, ApplicationUser user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteLink(Link link, ApplicationUser user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public DecisionKnowledgeElement getDecisionKnowledgeElement(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DecisionKnowledgeElement getDecisionKnowledgeElement(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<DecisionKnowledgeElement> getDecisionKnowledgeElements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<DecisionKnowledgeElement> getElementsLinkedWithInwardLinks(DecisionKnowledgeElement element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<DecisionKnowledgeElement> getElementsLinkedWithOutwardLinks(DecisionKnowledgeElement element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Link> getInwardLinks(DecisionKnowledgeElement element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Link> getOutwardLinks(DecisionKnowledgeElement element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DecisionKnowledgeElement insertDecisionKnowledgeElement(DecisionKnowledgeElement element,
+			ApplicationUser user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long insertLink(Link link, ApplicationUser user) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean updateDecisionKnowledgeElement(DecisionKnowledgeElement element, ApplicationUser user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
