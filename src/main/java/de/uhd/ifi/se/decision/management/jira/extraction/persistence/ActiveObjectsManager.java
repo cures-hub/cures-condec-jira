9222007db69b776ec7493ecb54e11fe4dd37eb2c
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2018-11-24 19:31:40 +0100

    ConDec-399: Remove unused deleteSentenceCounter variable

--- src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/persistence/ActiveObjectsManager.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/persistence/ActiveObjectsManager.java
@@ -37,8 +37,6 @@ public class ActiveObjectsManager {
 
 	public static ActiveObjects ActiveObjects;
 
-	private static int deleteSentenceCounter = 0;
-
 	private static final Logger LOGGER = LoggerFactory.getLogger(ActiveObjectsManager.class);
 
 	public static void init() {
