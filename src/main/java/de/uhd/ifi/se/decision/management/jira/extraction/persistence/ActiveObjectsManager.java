941f090197895b3f7ff614c3a8dd5c29f30b0229
Jochen <J.Clormann@stud.uni-heidelberg.de>
2018-12-13 17:24:05 +0100

    ConDec-430: Set Propper KnowledgeTypes for smart Links

--- src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/persistence/ActiveObjectsManager.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/persistence/ActiveObjectsManager.java
@@ -644,6 +644,26 @@ public class ActiveObjectsManager {
 		IssueLinkManager issueLinkManager = ComponentAccessor.getIssueLinkManager();
 		long linkTypeId = JiraIssuePersistenceManager.getLinkTypeId("contain");
 
+		for(Link oldLink: GenericLinkManager.getLinksForElement("s"+aoId)) {
+			Link newLink = oldLink;
+			if(newLink.getIdOfDestinationElementWithPrefix().equals("s"+aoId)) {
+				newLink.setDestinationElement(new DecisionKnowledgeElementImpl(issue));
+			}else if(newLink.getIdOfSourceElementWithPrefix().equals("s"+aoId)) {
+				newLink.setSourceElement(new DecisionKnowledgeElementImpl(issue));
+			}
+			GenericLinkManager.deleteGenericLink(oldLink);
+			if(GenericLinkManager.isIssueLink(newLink)) {
+				try {
+					issueLinkManager.createIssueLink(element.getIssueId(), issue.getId(), linkTypeId, (long) 0, user);
+				} catch (CreateException e) {
+					
+				}
+			}else {
+				GenericLinkManager.insertLink(newLink, user);
+			}
+		}
+		
+		
 		try {
 			issueLinkManager.createIssueLink(element.getIssueId(), issue.getId(), linkTypeId, (long) 0, user);
 		} catch (CreateException e) {
