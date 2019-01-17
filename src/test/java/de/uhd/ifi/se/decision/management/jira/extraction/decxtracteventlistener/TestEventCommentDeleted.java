c9b43cdc528fc0e5d39dc19ad5b83dbf8a5082fe
Tim <tim.kuchenbuch@gmx.net>
2019-01-17 20:00:24 +0100

    ConDec-446: Test Comment Deleted

---
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/decxtracteventlistener/TestEventCommentDeleted.java
@@ -0,0 +1,44 @@
+package de.uhd.ifi.se.decision.management.jira.extraction.decxtracteventlistener;
+
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.event.issue.IssueEvent;
+import com.atlassian.jira.event.type.EventType;
+import com.atlassian.jira.mock.ofbiz.MockGenericValue;
+import de.uhd.ifi.se.decision.management.jira.TestSetUpWithIssues;
+import net.java.ao.test.jdbc.Data;
+import net.java.ao.test.junit.ActiveObjectsJUnitRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.HashMap;
+
+@RunWith(ActiveObjectsJUnitRunner.class)
+@Data(TestSetUpWithIssues.AoSentenceTestDatabaseUpdater.class)
+public class TestEventCommentDeleted extends TestSetUpEventListener{
+
+    private IssueEvent issueEvent = new IssueEvent(issue,user,comment,null, new MockGenericValue("test"),new HashMap(), EventType.ISSUE_COMMENT_DELETED_ID);
+
+    @Test
+    public void testNoCommentContain(){
+        comment = ComponentAccessor.getCommentManager().create(issue, user, "", true);
+        listener.onIssueEvent(issueEvent);
+    }
+
+    @Test
+    public void testRationalTag(){
+        comment = ComponentAccessor.getCommentManager().create(issue, user, "{issue} fewfwewf{/issue}", true);
+        listener.onIssueEvent(issueEvent);
+    }
+
+    @Test
+    public void testExclusindTags(){
+        comment = ComponentAccessor.getCommentManager().create(issue, user, "{code}fviowerf{/code}", true);
+        listener.onIssueEvent(issueEvent);
+    }
+
+    @Test
+    public void testRationalIcons(){
+        comment = ComponentAccessor.getCommentManager().create(issue, user, "(!) rtwefhowiuhf", true);
+        listener.onIssueEvent(issueEvent);
+    }
+}
