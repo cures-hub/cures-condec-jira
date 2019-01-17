baf9775c0dd542aefdcc4d28272f2784d193e15d
Tim <tim.kuchenbuch@gmx.net>
2019-01-17 19:59:37 +0100

    ConDec-446: Test EventIssueDeleted

--- src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/decxtracteventlistener/TestEventIssueDeleted.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/decxtracteventlistener/TestEventIssueDeleted.java
@@ -4,19 +4,13 @@ import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.event.issue.IssueEvent;
 import com.atlassian.jira.event.type.EventType;
 import com.atlassian.jira.mock.ofbiz.MockGenericValue;
-import de.uhd.ifi.se.decision.management.jira.TestSetUpWithIssues;
-import net.java.ao.test.jdbc.Data;
-import net.java.ao.test.junit.ActiveObjectsJUnitRunner;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import java.util.HashMap;
 
-@RunWith(ActiveObjectsJUnitRunner.class)
-@Data(TestSetUpWithIssues.AoSentenceTestDatabaseUpdater.class)
-public class TestOnIssueEventIssueComment extends TestSetUpEventListener {
+public class TestEventIssueDeleted extends TestSetUpEventListener {
 
-    private IssueEvent issueEvent = new IssueEvent(issue,user,comment,null, new MockGenericValue("test"),new HashMap(),EventType.ISSUE_COMMENTED_ID);
+    private IssueEvent issueEvent = new IssueEvent(issue,user,comment,null, new MockGenericValue("test"),new HashMap(), EventType.ISSUE_DELETED_ID);
 
     @Test
     public void testNoCommentContain(){
@@ -32,7 +26,6 @@ public class TestOnIssueEventIssueComment extends TestSetUpEventListener {
 
     @Test
     public void testExclusindTags(){
-        IssueEvent issueEvent = new IssueEvent(issue,user,comment,null, new MockGenericValue("test"),new HashMap(),EventType.ISSUE_COMMENTED_ID);
         comment = ComponentAccessor.getCommentManager().create(issue, user, "{code}fviowerf{/code}", true);
         listener.onIssueEvent(issueEvent);
     }
