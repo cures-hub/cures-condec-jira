44293bd6e9a0dc4e6594081e6394e1c3e12affec
Eleftherios Matios <e.matios@gmail.com>
2019-01-21 11:22:12 +0100

    ConDec-446: Remove unused imports

--- src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/decxtracteventlistener/TestRegisterFunctions.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/decxtracteventlistener/TestRegisterFunctions.java
@@ -2,20 +2,11 @@ package de.uhd.ifi.se.decision.management.jira.extraction.decxtracteventlistener
 
 import static org.junit.Assert.assertNotNull;
 
-import com.atlassian.event.api.EventPublisher;
-import com.atlassian.jira.event.MockEventPublisher;
-import de.uhd.ifi.se.decision.management.jira.extraction.DecXtractEventListener;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import com.atlassian.activeobjects.test.TestActiveObjects;
 
-import de.uhd.ifi.se.decision.management.jira.TestComponentGetter;
 import de.uhd.ifi.se.decision.management.jira.TestSetUpWithIssues;
-import de.uhd.ifi.se.decision.management.jira.mocks.MockTransactionTemplate;
-import de.uhd.ifi.se.decision.management.jira.mocks.MockUserManager;
-import net.java.ao.EntityManager;
 import net.java.ao.test.jdbc.Data;
 import net.java.ao.test.jdbc.NonTransactional;
 import net.java.ao.test.junit.ActiveObjectsJUnitRunner;
