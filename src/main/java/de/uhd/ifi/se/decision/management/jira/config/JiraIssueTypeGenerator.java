9fd36444ad1a79cb1215642f617ae90abe56d10f
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2019-02-04 23:58:44 +0100

    ConDec-377: Enable to get JIRA project in JiraIssueTypeGenerator for DecisionKnowledgeReport
    [issue] Which JIRA issue types should the user be able to select when creating the decision knowledge report? [/issue]
    [decision] Show all JIRA issue types from the selected project. [/decision]
    [alternative] Only show a static list of types, e.g. Work Item and Bug [/alternative]
    [con] JIRA issue types differ between projects and should not be this static [/con]
    [alternative] Show all JIRA issue types available on the JIRA server [/alternative]
    [pro] Easy to implement [/pro]
    [con] Information overload [/con]

--- src/main/java/de/uhd/ifi/se/decision/management/jira/config/JiraIssueTypeGenerator.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/config/JiraIssueTypeGenerator.java
@@ -4,31 +4,37 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.ofbiz.core.entity.GenericValue;
+
+import com.atlassian.configurable.ValuesGenerator;
 import com.atlassian.jira.component.ComponentAccessor;
-import com.atlassian.jira.config.IssueTypeManager;
 import com.atlassian.jira.issue.fields.config.manager.IssueTypeSchemeManager;
 import com.atlassian.jira.issue.issuetype.IssueType;
-import com.atlassian.jira.portal.SortingValuesGenerator;
 import com.atlassian.jira.project.Project;
 
 /**
  * Provides a list of JIRA issue types for the decision knowledge report. The
  * user needs to select one from this list.
  */
-public class JiraIssueTypeGenerator extends SortingValuesGenerator {
+public class JiraIssueTypeGenerator implements ValuesGenerator<IssueType> {
 
 	@Override
 	@SuppressWarnings("rawtypes")
 	// @issue: How can we get the project id for the selected project? Is the
 	// projectId part of params?
-	public Map<String, String> getValues(Map params) {
-		Map<String, String> jiraIssueTypes = new HashMap<String, String>();
+	public Map<IssueType, String> getValues(Map params) {
+		GenericValue valueProject = (GenericValue) params.get("project");
+		long projectId = (long) valueProject.get("id");
 
-		IssueTypeManager issueTypeManager = ComponentAccessor.getComponent(IssueTypeManager.class);
-		Collection<IssueType> types = issueTypeManager.getIssueTypes();
+		Collection<IssueType> jiraIssueTypesList = getJiraIssueTypes(projectId);
+		Map<IssueType, String> jiraIssueTypes = new HashMap<IssueType, String>();
 
-		for (IssueType type : types) {
-			jiraIssueTypes.put(type.getId(), type.getName());
+		// IssueTypeManager issueTypeManager =
+		// ComponentAccessor.getComponent(IssueTypeManager.class);
+		// Collection<IssueType> types = issueTypeManager.getIssueTypes();
+
+		for (IssueType type : jiraIssueTypesList) {
+			jiraIssueTypes.put(type, type.getName());
 		}
 
 		return jiraIssueTypes;
