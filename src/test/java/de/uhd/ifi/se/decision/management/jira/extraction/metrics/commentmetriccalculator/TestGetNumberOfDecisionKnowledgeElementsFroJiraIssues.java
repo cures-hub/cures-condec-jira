348177a0ee39800c9f9567ccea74534284804022
Tim <tim.kuchenbuch@gmx.net>
2019-02-05 13:11:22 +0100

    ConDec-377: Test GetNumberOfDecisionKnowledgeElements

---
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/metrics/commentmetriccalculator/TestGetNumberOfDecisionKnowledgeElementsFroJiraIssues.java
@@ -0,0 +1,26 @@
+package de.uhd.ifi.se.decision.management.jira.extraction.metrics.commentmetriccalculator;
+
+import de.uhd.ifi.se.decision.management.jira.TestSetUpWithIssues;
+import de.uhd.ifi.se.decision.management.jira.model.KnowledgeType;
+import net.java.ao.test.jdbc.Data;
+import net.java.ao.test.jdbc.NonTransactional;
+import net.java.ao.test.junit.ActiveObjectsJUnitRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(ActiveObjectsJUnitRunner.class)
+@Data(TestSetUpWithIssues.AoSentenceTestDatabaseUpdater.class)
+public class TestGetNumberOfDecisionKnowledgeElementsFroJiraIssues extends TestSetupCalculator  {
+	@Test
+	public void testTypeNull(){
+		assertEquals(0, calculator.getNumberOfDecisionKnowledgeElementsForJiraIssues(null).size(), 0.0);
+	}
+
+	@Test
+	@NonTransactional
+	public void testTypeFilled(){
+		assertEquals(1, calculator.getNumberOfDecisionKnowledgeElementsForJiraIssues(KnowledgeType.ARGUMENT).size(), 0.0);
+	}
+}
