fe45c92ef5d2cee2e907d61360eb224380d3f1ec
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2019-02-15 09:58:12 +0100

    ConDec-267: Move macro test class to separate package

--- src/test/java/de/uhd/ifi/se/decision/management/jira/view/macros/TestKnowledgeClassificationMacro.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/view/macros/TestKnowledgeClassificationMacro.java
@@ -1,4 +1,4 @@
-package de.uhd.ifi.se.decision.management.jira.view;
+package de.uhd.ifi.se.decision.management.jira.view.macros;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
