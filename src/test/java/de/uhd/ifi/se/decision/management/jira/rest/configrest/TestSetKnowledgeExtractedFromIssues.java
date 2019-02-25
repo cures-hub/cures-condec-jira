b6c4e42e9fa4404b643b5798333cbd959001fc28
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2019-02-25 21:05:33 +0100

    ConDec-465: Fix typo

--- src/test/java/de/uhd/ifi/se/decision/management/jira/rest/configrest/TestSetKnowledgeExtractedFromIssues.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/rest/configrest/TestSetKnowledgeExtractedFromIssues.java
@@ -12,7 +12,7 @@ import javax.ws.rs.core.Response;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(ActiveObjectsJUnitRunner.class)
-public class TestSetKnowledgeExtractedFormIssues extends TestConfigSuper {
+public class TestSetKnowledgeExtractedFromIssues extends TestConfigSuper {
     private static final String INVALID_EXTRACTEDISSUES = "isKnowledgeExtractedFromIssues = null";
     @Test
     public void testRequestNullProjectKeyNullIsKnowledgeExtractedNull(){
