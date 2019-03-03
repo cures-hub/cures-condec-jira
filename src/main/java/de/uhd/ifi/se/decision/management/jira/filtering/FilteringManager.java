4e38633af5e34a27c00e2ec84d1bd99fd5605bb4
Fabian Gronert <fagronert@gmail.com>
2019-03-03 10:20:09 +0100

    ConDec-466 Fix codacy issues in correct branch

--- src/main/java/de/uhd/ifi/se/decision/management/jira/filtering/FilteringManager.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/filtering/FilteringManager.java
@@ -35,8 +35,9 @@ public class FilteringManager {
 	public static List<List<DecisionKnowledgeElement>> getGraphsMatchingQuery(ApplicationUser user, String projectKey,
 			String query, String linkedQuery) {
 		//Default filter for adjecent Elements
-		if("".equals(linkedQuery)){
-			linkedQuery="?filter=allissues";
+		String linkedQueryNotEmpty=linkedQuery;
+		if("".equals(linkedQueryNotEmpty)){
+			linkedQueryNotEmpty="?filter=allissues";
 		}
 		List<DecisionKnowledgeElement> tempQueryResult = getElementsMatchingQuery(user, projectKey, query);
 		List<DecisionKnowledgeElement> addedElements = new ArrayList<DecisionKnowledgeElement>();
@@ -48,7 +49,7 @@ public class FilteringManager {
 			if (!addedElements.contains(current)) {
 				// if not get the connected tree
 				String currentElementKey = current.getKey();
-				List<DecisionKnowledgeElement> filteredElements = getElementsInGraph(user, projectKey, linkedQuery,
+				List<DecisionKnowledgeElement> filteredElements = getElementsInGraph(user, projectKey, linkedQueryNotEmpty,
 						currentElementKey);
 				// add each element to the list
 				addedElements.addAll(filteredElements);
