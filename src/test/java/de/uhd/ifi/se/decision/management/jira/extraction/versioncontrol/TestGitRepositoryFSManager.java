de7745aabc7d821af72bdcd1361054e46099b479
Lukasz Wisniowski <u123456.lw@gmail.com>
2019-05-16 15:04:55 +0200

    CONDEC-498: manage paths for git repo branches
    * assuming usage of multiple independent gitclient instances in this project, branches should be checked-out in their own folders on the file system to prevent conflicts with work on other branches. GitRepositoryFSManager arranges the file system and provides interfaces which can be later used by Gitclient implementations.
    
    [issue] Cloning git repositories costs a lot of time. How can providing branch related folder be accelerated? [/issue]
    [alternative] Copy or rename already existing repository folders to branch dedicated folder, pull updates and checkout the branch files in the dedicated folder![/alternative]
    [pro] Simple but sufficient implementation. [/pro]
    [con] Inefficient usage of disk space. [/con]
    [con] Not any more used branches should be recycled or deleted. [/con]
    [pro] Disk space is cheaper compared to network, RAM and CPU resources.[/pro]
    [pro] Branch folder recycle strategies could be imlemented.[/pro]

--- src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/versioncontrol/TestGitRepositoryFSManager.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/versioncontrol/TestGitRepositoryFSManager.java
@@ -17,8 +17,15 @@ public class TestGitRepositoryFSManager {
 	private String baseDir;
 	private String baseProjectUriDir;
 	private String baseProjectUriDefaultDir;
+	private String baseProjectUriTempDir;
+	private String expectedBaseProjectUriBranchDir;
 
+	private static final String branchName = "myTempBranch";
+	private static final String distinctFileInBranchFolder = "branch.txt";
+	private static final String distinctFileInDefaultFolder = "develop.txt";
+	private static final String distinctFileInTempFolder = "temp.txt";
 	private static final String folderForDefaultBranchName = "develop";
+	private static final String folderForTempBranchDirName = "TEMP123456";
 	private static final String projectName = "TEST";
 	private static final String repoUri = "https://jira-se.ifi.uni-heidelberg.de/test.git";
 
@@ -39,14 +46,141 @@ public class TestGitRepositoryFSManager {
 				+File.separator+getHash(repoUri);
 		baseProjectUriDefaultDir = baseProjectUriDir
 				+File.separator+ folderForDefaultBranchName;
+		baseProjectUriTempDir = baseProjectUriDir
+				+File.separator+ folderForTempBranchDirName;
+		expectedBaseProjectUriBranchDir = baseProjectUriDir
+				+File.separator
+				+getHash(branchName);
+
 		// init FS Manager
 		FSmanager = new GitRepositoryFSManager(baseDir,
 				projectName, repoUri, folderForDefaultBranchName);
 	}
 
 	@Test
+	public void testReleaseBranchDirectoryNameToTemp() {
+		// setup
+		addDistinctFileInDefaultDir();
+
+		// branch and temp folders do not exist
+		assertFalse(new File(expectedBaseProjectUriBranchDir).isDirectory());
+		assertEquals(0,findTemporaryDirectoryNames().length);
+
+		// create branch folder from default
+		FSmanager.prepareBranchDirectory(branchName);
+		assertTrue(new File(expectedBaseProjectUriBranchDir).isDirectory());
+
+		// test releasing branch folder to temporary folders pool
+		FSmanager.releaseBranchDirectoryNameToTemp(branchName);
+		assertFalse(new File(expectedBaseProjectUriBranchDir).isDirectory());
+		assertEquals(1,findTemporaryDirectoryNames().length);
+
+		String expectedFileName = baseProjectUriDir+File.separator
+				+ findTemporaryDirectoryNames()[0]
+				+ File.separator
+				+ distinctFileInDefaultFolder;
+
+		assertTrue(new File(expectedFileName).isFile());
+	}
+
+	@Test
 	public void testGetDefaultBranchPath(){
 		assertEquals(baseProjectUriDefaultDir,FSmanager.getDefaultBranchPath());
+	}
+
+	@Test
+	public void testPrepareBranchDirectoryFailure() {
+		// no directories for neither default, temporary nor branch exist
+		assertNull(FSmanager.prepareBranchDirectory(branchName));
+	}
+
+	@Test
+	public void testPrepareBranchDirectoryFromBranch() {
+		// setup
+		addDistinctFileInBranchDir();
+		String expectedContentPath = expectedBaseProjectUriBranchDir
+				+ File.separator
+				+ distinctFileInBranchFolder;
+
+		assertEquals(0,findTemporaryDirectoryNames().length);
+		// test
+		testBranchFolderPreparation(branchName, expectedBaseProjectUriBranchDir, expectedContentPath);
+		// preparation from branch directory itself should not affect temporary folders
+		assertEquals(0,findTemporaryDirectoryNames().length);
+	}
+
+	@Test
+	public void testPrepareBranchDirectoryFromBranchPrecedenceOverDefaultAndTemp() {
+		// setup
+		addDistinctFileInBranchDir();
+		addDistinctFileInDefaultDir();
+		addDistinctFileInTempDir();
+		String expectedContentPath = expectedBaseProjectUriBranchDir
+				+ File.separator
+				+ distinctFileInBranchFolder;
+
+		assertEquals(1,findTemporaryDirectoryNames().length);
+
+		// test
+		testBranchFolderPreparation(branchName, expectedBaseProjectUriBranchDir, expectedContentPath);
+		assertEquals(1,findTemporaryDirectoryNames().length);
+	}
+
+	@Test
+	public void testPrepareBranchDirectoryFromTemp() {
+		// setup
+		addDistinctFileInTempDir();
+		String expectedContentPath = expectedBaseProjectUriBranchDir
+				+ File.separator
+				+ distinctFileInTempFolder;
+
+		assertEquals(1,findTemporaryDirectoryNames().length);
+
+		// test
+		testBranchFolderPreparation(branchName, expectedBaseProjectUriBranchDir, expectedContentPath);
+		// preparation should take the folder from temporary folders pool
+		assertEquals(0,findTemporaryDirectoryNames().length);
+
+		addDistinctFileInTempDir();
+		assertEquals(1,findTemporaryDirectoryNames().length);
+	}
+
+	@Test
+	public void testPrepareBranchDirectoryFromTempPrecedenceOverDefault() {
+		// setup
+		addDistinctFileInDefaultDir();
+		addDistinctFileInTempDir();
+		String expectedContentPath = expectedBaseProjectUriBranchDir
+				+ File.separator
+				+ distinctFileInTempFolder;
+
+		assertEquals(1,findTemporaryDirectoryNames().length);
+
+		// test
+		testBranchFolderPreparation(branchName, expectedBaseProjectUriBranchDir, expectedContentPath);
+		assertEquals(0,findTemporaryDirectoryNames().length);
+	}
+	@Test
+	public void testPrepareBranchDirectoryFromDefault() {
+		// setup
+		addDistinctFileInDefaultDir();
+		String expectedContentPath = expectedBaseProjectUriBranchDir
+				+ File.separator
+				+ distinctFileInDefaultFolder;
+
+		assertEquals(0,findTemporaryDirectoryNames().length);
+		// test
+		testBranchFolderPreparation(branchName, expectedBaseProjectUriBranchDir, expectedContentPath);
+		// preparation from default folder should not affect temporary folders
+		assertEquals(0,findTemporaryDirectoryNames().length);
+	}
+
+	private void testBranchFolderPreparation(String branchName, String expectedDir, String expectedContentPath) {
+		String actualDir = FSmanager.prepareBranchDirectory(branchName);
+		assertNotNull(actualDir);
+		assertEquals(expectedDir,actualDir);
+		assertTrue(new File(expectedDir).isDirectory());
+		assertTrue(new File(expectedContentPath).isFile());
 	}
 
 	private static File getExampleDirectory() {
@@ -71,5 +205,60 @@ public class TestGitRepositoryFSManager {
 		catch (NoSuchAlgorithmException e) {;
 			return "";
 		}
+	}
+
+	/* helper for fetching temporary folders */
+	private String[] findTemporaryDirectoryNames() {
+		File file = new File(baseProjectUriDir);
+		String[] directories = file.list((current, name) ->
+				(name.toString().startsWith("TEMP")
+						&& new File(current, name).isDirectory()));
+		return directories;
+	}
+
+	/* helpers for adding files */
+
+	private void addDistinctFileInDefaultDir() {
+		addDistinctFile("default");
+	}
+
+	private void addDistinctFileInTempDir() {
+		addDistinctFile("temp");
+	}
+
+	private void addDistinctFileInBranchDir() {
+		addDistinctFile("branch");
+	}
+
+	private boolean addDistinctFile(String target) {
+		File file;
+		File dir;
+		if (target.endsWith("branch")){
+			dir = new File (expectedBaseProjectUriBranchDir);
+			// add distinct file name to default branch directory
+			file = new File(dir, distinctFileInBranchFolder);
+		}
+		else if (target.endsWith("default")){
+			dir = new File (baseProjectUriDefaultDir);
+			// add distinct file name to default branch directory
+			file = new File(dir, distinctFileInDefaultFolder);
+		}
+		else if (target.endsWith("temp")) {
+			dir = new File (baseProjectUriTempDir);
+			// add distinct file name to temporary branch directory
+			file = new File(dir, distinctFileInTempFolder);
+		}
+		else {
+			return false;
+		}
+		try {
+			dir.mkdirs();
+			file.createNewFile();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 }
