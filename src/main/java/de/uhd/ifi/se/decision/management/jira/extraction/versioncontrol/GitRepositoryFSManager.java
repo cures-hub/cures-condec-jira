53531c9fdd9504dcc90a70bc9939738d90990f34
Lukasz Wisniowski <u123456.lw@gmail.com>
2019-05-16 15:04:55 +0200

    CONDEC-498: manage paths for git repo branches
    * assuming usage of multiple independent gitclient instances in this project, branches should be checked-out in their own folders on the file system to prevent conflicts with work on other branches. GitRepositoryFSManager arranges the file system and provides interfaces which can be later used by Gitclient implementations.
    
    [issue] Cloning git repositories costs a lot of time. How can providing branch related folder be accelerated? [/issue]
    [alternative] Copy or rename already existing repository folders to branch dedicated folder, pull updates and checkout the branch files in the dedicated folder![/alternative]
    [pro] Simple but sufficient implementation. [/pro]
    [con] Inefficient usage of disk space. [/con]
    [con] Not any more used branches should be recycled or deleted. [/con]
    [pro] Disk space is cheaper compared to network, RAM and CPU resources.[/pro]
    [pro] Branch folder recycle strategies could be imlemented.[/pro]

--- src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/versioncontrol/GitRepositoryFSManager.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/versioncontrol/GitRepositoryFSManager.java
@@ -1,11 +1,13 @@
 package de.uhd.ifi.se.decision.management.jira.extraction.versioncontrol;
 
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.xml.bind.DatatypeConverter;
 import java.io.File;
+import java.nio.file.Files;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Date;
@@ -28,11 +30,66 @@ public class GitRepositoryFSManager {
 
 	/**
 	 * Returns target directory path for the default branch of the repository.
-	 *
 	 * @return absolute path to directory of the default branch
 	 */
 	public String getDefaultBranchPath() {
 		return baseProjectUriDefaultPath;
+	}
+
+	/**
+	 * Makes branch's folder available in the temporary pool.
+	 * It can significantly contribute to improving the speed
+	 * of check-outs of other branches as folder renaming
+	 * is not costly compared to copying.
+	 *
+	 * @param branchShortName branch name
+	 * @return null on failure, absolute path to new temporary directory
+	 */
+	public String releaseBranchDirectoryNameToTemp(String branchShortName) {
+		File oldDir = new File(getBranchPath(branchShortName));
+		if (!oldDir.isDirectory()) {
+			return null;
+		}
+		else {
+			Date date= new Date();
+			long time = date.getTime();
+			String tempDirString = baseProjectUriPath
+					+File.separator
+					+TEMP_DIR_PREFIX+String.valueOf(time);
+			File tempDir = new File(tempDirString);
+			try {
+				oldDir.renameTo(tempDir);
+			}
+			catch (Exception e) {
+				LOGGER.error("Could not rename "+oldDir
+						+" to "+tempDirString+". "+e.getMessage());
+				return null;
+			}
+			return tempDirString;
+		}
+	}
+
+	/**
+	 * Provides filesystem directory for targeted branch.
+	 * Best case: branch already exists, costs no I/O operations.
+	 * Good case: temporary folder exists and can be renamed to branch's
+	 * 	target folder name.
+	 * Bad case: branch folder is copied in I/O havey operation
+	 * 	from default branch.
+	 *
+	 * @param branchShortName branch name
+	 * @return null on failure, absolute path to branch's directory
+	 */
+	public String prepareBranchDirectory(String branchShortName) {
+		if (!useFromExistingBranchFolder(branchShortName)
+			&& !useFromTemporaryFolder(branchShortName)
+			&& !useFromDefaultFolder(branchShortName)) {
+			LOGGER.warn("Neither branch, nor temporary," +
+					" nor default folder could be found under: "
+					+ baseProjectUriPath);
+			return null;
+		}
+		return getBranchPath(branchShortName);
 	}
 
 	private String getHash(String text) {
@@ -46,5 +103,60 @@ public class GitRepositoryFSManager {
 			LOGGER.error("MD5 does not exist??");
 			return "";
 		}
+	}
+
+	private String getBranchPath(String branchShortName) {
+		return baseProjectUriPath+File.separator+getHash(branchShortName);
+	}
+
+	private boolean useFromDefaultFolder(String branchShortName) {
+		File defaultDir = new File(baseProjectUriDefaultPath);
+		if (!defaultDir.isDirectory()) {
+			return false;
+		}
+		else {
+			try {
+				File newDir = new File(getBranchPath(branchShortName));
+				FileUtils.copyDirectory(defaultDir,newDir);
+			}
+			catch (Exception e) {
+				LOGGER.error("Could not copy "+defaultDir
+						+" to "+getBranchPath(branchShortName)+".\n\t"+e.getMessage());
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private boolean useFromExistingBranchFolder(String branchShortName) {
+		File dir = new File(getBranchPath(branchShortName));
+		return dir.isDirectory();
+	}
+
+	private boolean useFromTemporaryFolder(String branchShortName) {
+		String[] tempDirs = findTemporaryDirectoryNames();
+		if (tempDirs==null || tempDirs.length<1) {
+			return false;
+		}
+		try {
+			File dir = new File(baseProjectUriPath+File.separator
+					+tempDirs[0]); // get the 1st of temp dirs
+			File newDir = new File(getBranchPath(branchShortName));
+			dir.renameTo(newDir);
+		}
+		catch (Exception e) {
+			LOGGER.error("Could not rename "+tempDirs[0]
+					+" to "+getBranchPath(branchShortName)+". "+e.getMessage());
+			return false;
+		}
+		return true;
+	}
+
+	private String[] findTemporaryDirectoryNames() {
+		File file = new File(baseProjectUriPath);
+		String[] directories = file.list((current, name) ->
+				(name.toString().startsWith(TEMP_DIR_PREFIX)
+				&& new File(current, name).isDirectory()));
+		return directories;
 	}
 }
