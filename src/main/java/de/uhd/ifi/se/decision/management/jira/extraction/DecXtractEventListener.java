514ef46686824ac8ee40b967ac9a0cca7098ab30
Tim <tim.kuchenbuch.gmx.net>
2019-05-20 15:55:28 +0200

    ConDec-499: Exchange e.stacktrace to logger.error

--- src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/DecXtractEventListener.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/DecXtractEventListener.java
@@ -201,7 +201,7 @@ public class DecXtractEventListener implements InitializingBean, DisposableBean 
 				changedString.put(oldValue, newValue);
 			}
 		} catch (NullPointerException | GenericEntityException e) {
-			e.printStackTrace();
+			LOGGER.error("Get changed string in a issue event failed. Message: " + e.getMessage());
 		}
 
 		return changedString;
