3c537371b1e7d0d5879fed2751c42317cd5f3844
Tim <tim.kuchenbuch.gmx.net>
2019-05-22 10:42:38 +0200

    ConDec-499: Fix Nullpointer of the PluginSettingFactory

--- src/test/java/de/uhd/ifi/se/decision/management/jira/rest/knowledgerest/TestGetSummarizedCode.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/rest/knowledgerest/TestGetSummarizedCode.java
@@ -4,18 +4,29 @@ import static org.junit.Assert.assertEquals;
 
 import javax.ws.rs.core.Response.Status;
 
+import com.atlassian.activeobjects.test.TestActiveObjects;
+import de.uhd.ifi.se.decision.management.jira.TestComponentGetter;
+import de.uhd.ifi.se.decision.management.jira.mocks.MockTransactionTemplate;
+import de.uhd.ifi.se.decision.management.jira.mocks.MockUserManager;
+import net.java.ao.EntityManager;
+import net.java.ao.test.junit.ActiveObjectsJUnitRunner;
 import org.junit.Before;
 import org.junit.Test;
 
 import de.uhd.ifi.se.decision.management.jira.extraction.gitclient.TestSetUpGit;
 import de.uhd.ifi.se.decision.management.jira.rest.KnowledgeRest;
+import org.junit.runner.RunWith;
 
+@RunWith(ActiveObjectsJUnitRunner.class)
 public class TestGetSummarizedCode extends TestSetUpGit {
 
+	private EntityManager entityManager;
 	private KnowledgeRest knowledgeRest;
 
 	@Before
 	public void setUp() {
+		TestComponentGetter.init(new TestActiveObjects(entityManager), new MockTransactionTemplate()
+				,new MockUserManager());
 		super.setUp();
 		knowledgeRest = new KnowledgeRest();
 	}
