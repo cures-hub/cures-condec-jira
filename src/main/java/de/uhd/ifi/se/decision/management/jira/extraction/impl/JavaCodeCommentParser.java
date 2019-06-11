aea30b9f0a9e77756528bb4562ff495e4d624f28
Lukasz Wisniowski <u123456.lw@gmail.com>
2019-06-11 15:22:09 +0200

    CONDEC-502: add Java code comment parser
    * just fetches comment's contents and positions within a source file

---
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/impl/JavaCodeCommentParser.java
@@ -0,0 +1,60 @@
+package de.uhd.ifi.se.decision.management.jira.extraction.impl;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseProblemException;
+import com.github.javaparser.Position;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.comments.Comment;
+import de.uhd.ifi.se.decision.management.jira.extraction.CodeCommentParser;
+import de.uhd.ifi.se.decision.management.jira.extraction.CodeCommentWithRange;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JavaCodeCommentParser  implements CodeCommentParser {
+	private static final Logger LOGGER = LoggerFactory.getLogger(CodeCommentParser.class);
+	@Override
+	public List<CodeCommentWithRange> getComments(File inspectedFile) {
+
+		CompilationUnit compilationUnit = parseJavaFile(inspectedFile);
+		if (compilationUnit == null) {
+			return null;
+		} else {
+			return getComments(compilationUnit);
+		}
+	}
+
+	private CompilationUnit parseJavaFile(File inspectedFile) {
+		CompilationUnit compilationUnit = null;
+		try {
+			FileInputStream fileInputStream = new FileInputStream(inspectedFile.toString());
+			compilationUnit = JavaParser.parse(fileInputStream);
+			fileInputStream.close();
+		} catch (ParseProblemException | IOException e) {
+			LOGGER.error(e.getMessage());
+		}
+		return compilationUnit;
+	}
+
+	private List<CodeCommentWithRange> getComments(CompilationUnit compilationUnit) {
+		List<Comment> comments = compilationUnit.getComments();
+		List<CodeCommentWithRange> positionedComments =
+			comments.stream()
+				.filter(comment -> (comment.getBegin().isPresent()
+						&& comment.getEnd().isPresent()))
+				.map( comment -> {
+					Position begin = comment.getBegin().get();
+					Position end = comment.getEnd().get();
+					return new CodeCommentWithRange(comment.getContent()
+							,begin.column, begin.line
+							,end.column, end.line);
+				})
+				.collect(Collectors.toList());
+		return positionedComments;
+	}
+}
