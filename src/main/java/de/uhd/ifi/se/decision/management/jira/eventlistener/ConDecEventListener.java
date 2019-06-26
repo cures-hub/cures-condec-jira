ddcb914a6649659bbf2a80f6a40b941f108a601a
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2019-06-26 17:33:01 +0200

    Remove unused imports

--- src/main/java/de/uhd/ifi/se/decision/management/jira/eventlistener/ConDecEventListener.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/eventlistener/ConDecEventListener.java
@@ -1,19 +1,21 @@
 package de.uhd.ifi.se.decision.management.jira.eventlistener;
 
-import aQute.bnd.annotation.component.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.atlassian.event.api.EventListener;
 import com.atlassian.event.api.EventPublisher;
 import com.atlassian.jira.event.issue.IssueEvent;
 import com.atlassian.jira.event.issue.link.IssueLinkCreatedEvent;
 import com.atlassian.jira.event.issue.link.IssueLinkDeletedEvent;
 import com.atlassian.plugin.spring.scanner.annotation.imports.JiraImport;
+
+import aQute.bnd.annotation.component.Component;
 import de.uhd.ifi.se.decision.management.jira.model.DecisionKnowledgeElement;
 import de.uhd.ifi.se.decision.management.jira.model.impl.DecisionKnowledgeElementImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Component
 public class ConDecEventListener implements InitializingBean, DisposableBean {
@@ -21,17 +23,17 @@ public class ConDecEventListener implements InitializingBean, DisposableBean {
 	@JiraImport
 	private final EventPublisher eventPublisher;
 	private WebhookEventListener webhookEventListener;
-	private DecXtractEventListener decXtractEventListener;
+	private JiraIssueTextExtractionEventListener jiraIssueTextExtractionEventListener;
 	private SummarizationEventListener summarizationEventListener;
 
 	protected static final Logger LOGGER = LoggerFactory.getLogger(ConDecEventListener.class);
 
 	@Autowired
-	public ConDecEventListener(EventPublisher eventPublisher){
+	public ConDecEventListener(EventPublisher eventPublisher) {
 		this.eventPublisher = eventPublisher;
 		LOGGER.info("ConDec event listener was added to JIRA");
 		webhookEventListener = new WebhookEventListener();
-		decXtractEventListener = new DecXtractEventListener();
+		jiraIssueTextExtractionEventListener = new JiraIssueTextExtractionEventListener();
 		summarizationEventListener = new SummarizationEventListener();
 	}
 
@@ -58,26 +60,28 @@ public class ConDecEventListener implements InitializingBean, DisposableBean {
 	}
 
 	@EventListener
-	public void onIssueEvent(IssueEvent issueEvent){
+	public void onIssueEvent(IssueEvent issueEvent) {
 		if (issueEvent == null) {
 			return;
 		}
 		webhookEventListener.onIssueEvent(issueEvent);
-		decXtractEventListener.onIssueEvent(issueEvent);
+		jiraIssueTextExtractionEventListener.onIssueEvent(issueEvent);
 		summarizationEventListener.onIssueEvent(issueEvent);
 	}
 
 	@EventListener
-	public void onLinkCreatedIssueEvent(IssueLinkCreatedEvent linkCreatedEvent){
-		DecisionKnowledgeElement element = new DecisionKnowledgeElementImpl(linkCreatedEvent.getIssueLink().getSourceObject());
+	public void onLinkCreatedIssueEvent(IssueLinkCreatedEvent linkCreatedEvent) {
+		DecisionKnowledgeElement element = new DecisionKnowledgeElementImpl(
+				linkCreatedEvent.getIssueLink().getSourceObject());
 		webhookEventListener.onLinkEvent(element);
-		LOGGER.info("ConDec event listener on link create issue event triggered");
+		LOGGER.info("ConDec event listener on link create issue event triggered.");
 	}
 
 	@EventListener
 	public void onLinkDeletedIssueEvent(IssueLinkDeletedEvent linkDeletedEvent) {
-		DecisionKnowledgeElement element = new DecisionKnowledgeElementImpl(linkDeletedEvent.getIssueLink().getSourceObject());
+		DecisionKnowledgeElement element = new DecisionKnowledgeElementImpl(
+				linkDeletedEvent.getIssueLink().getSourceObject());
 		webhookEventListener.onLinkEvent(element);
-		LOGGER.info("ConDec event listener on link deleted issue event triggered");
+		LOGGER.info("ConDec event listener on link deleted issue event triggered.");
 	}
 }
