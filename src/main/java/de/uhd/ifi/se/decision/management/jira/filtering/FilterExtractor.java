d207d353c97cdaa4359a87b8fff9946ada003c8d
Tim <tim.kuchenbuch.gmx.net>
2019-07-01 13:49:23 +0200

    ConDec-514: Remove FilterManager and add the implementation to the FilterExtractor

--- src/main/java/de/uhd/ifi/se/decision/management/jira/filtering/FilterExtractor.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/filtering/FilterExtractor.java
@@ -3,6 +3,9 @@ package de.uhd.ifi.se.decision.management.jira.filtering;
 
 import com.atlassian.jira.user.ApplicationUser;
 import de.uhd.ifi.se.decision.management.jira.model.DecisionKnowledgeElement;
+import de.uhd.ifi.se.decision.management.jira.model.Graph;
+import de.uhd.ifi.se.decision.management.jira.model.impl.GraphImpl;
+import de.uhd.ifi.se.decision.management.jira.model.impl.GraphImplFiltered;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -58,5 +61,48 @@ public class FilterExtractor {
 	 */
 	public List<DecisionKnowledgeElement> getFilteredDecisions(){
 		return this.decisionKnowledgeElements;
+	}
+
+
+	/** Treant and Treeview filter  */
+
+	private static List<DecisionKnowledgeElement> getElementsInGraph(ApplicationUser user, String projectKey,
+	                                                                 String query, String elementKey) {
+		Graph graph;
+		if ((query.matches("\\?jql=(.)+")) || (query.matches("\\?filter=(.)+"))) {
+			GraphFiltering filter = new GraphFiltering(projectKey, query, user, false);
+			filter.produceResultsFromQuery();
+			graph = new GraphImplFiltered(projectKey, elementKey, filter);
+		} else {
+			graph = new GraphImpl(projectKey, elementKey);
+		}
+		return graph.getAllElements();
+	}
+
+	public List<List<DecisionKnowledgeElement>> getGraphsMatchingQuery(String linkedQuery) {
+		// Default filter for adjecent Elements
+		String linkedQueryNotEmpty = linkedQuery;
+		if ("".equals(linkedQueryNotEmpty)) {
+			linkedQueryNotEmpty = "?filter=allissues";
+		}
+		List<DecisionKnowledgeElement> tempQueryResult = new FilterExtractor(projectKey, user, filterString).getFilteredDecisions();
+		List<DecisionKnowledgeElement> addedElements = new ArrayList<DecisionKnowledgeElement>();
+		List<List<DecisionKnowledgeElement>> elementsQueryLinked = new ArrayList<List<DecisionKnowledgeElement>>();
+
+		// now iti over query result
+		for (DecisionKnowledgeElement current : tempQueryResult) {
+			// check if in addedElements list
+			if (!addedElements.contains(current)) {
+				// if not get the connected tree
+				String currentElementKey = current.getKey();
+				List<DecisionKnowledgeElement> filteredElements = getElementsInGraph(user, projectKey,
+						linkedQueryNotEmpty, currentElementKey);
+				// add each element to the list
+				addedElements.addAll(filteredElements);
+				// add list to the big list
+				elementsQueryLinked.add(filteredElements);
+			}
+		}
+		return elementsQueryLinked;
 	}
 }
