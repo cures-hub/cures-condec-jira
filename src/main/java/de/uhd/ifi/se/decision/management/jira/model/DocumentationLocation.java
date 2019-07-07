4514e229c73687d95f4607863512d301f30848d9
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2019-07-07 16:19:51 +0200

    Add identifier and name to DocumentationLocation enum values

--- src/main/java/de/uhd/ifi/se/decision/management/jira/model/DocumentationLocation.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/model/DocumentationLocation.java
@@ -7,91 +7,74 @@ import java.util.Locale;
 import de.uhd.ifi.se.decision.management.jira.model.text.PartOfJiraIssueText;
 
 /**
- * Possible documentation locations of decision knowledge
+ * Possible documentation locations of decision knowledge.
  */
 public enum DocumentationLocation {
-	JIRAISSUE, ACTIVEOBJECT, JIRAISSUETEXT, COMMIT, PULLREQUEST, UNKNOWN;
+	JIRAISSUE("i", "JiraIssue"), // store as first-class entities with own JIRA issue type
+	ACTIVEOBJECT("a", "ActiveObject"), // store as first-class entities but not as JIRA issues
+	JIRAISSUETEXT("s", "JiraIssueText"), // store in the body of existing JIRA issues, e.g. work items or requirements
+	COMMIT("c", "Commit"), // store in commit message
+	PULLREQUEST("p", "PullRequest"), // store in pull requests (currently not used)
+	UNKNOWN("", "Unknown");
+
+	private String identifier;
+	private String name;
+
+	private DocumentationLocation(String identifier, String name) {
+		this.identifier = identifier;
+		this.name = name;
+	}
 
 	/**
-	 * Convert a string to a documentation type.
+	 * Converts an identifier to a documentation location objects.
 	 *
-	 * @param identifier as a String.
+	 * @param identifier
+	 *            of the documentation location as a String, e.g. "i" for JIRA
+	 *            issue.
 	 */
 	public static DocumentationLocation getDocumentationLocationFromIdentifier(String identifier) {
 		if (identifier == null) {
-			return DocumentationLocation.UNKNOWN;
+			return UNKNOWN;
 		}
-		switch (identifier.toLowerCase(Locale.ENGLISH)) {
-			case "i":
-				return DocumentationLocation.JIRAISSUE;
-			case "a":
-				return DocumentationLocation.ACTIVEOBJECT;
-			case "s":
-				return DocumentationLocation.JIRAISSUETEXT;
-			case "c":
-				return DocumentationLocation.COMMIT;
-			case "p":
-				return DocumentationLocation.PULLREQUEST;
-			case "":
-				// TODO This should be the same as the default persistence strategy
-				return DocumentationLocation.JIRAISSUE;
-			default:
-				return DocumentationLocation.UNKNOWN;
+		// TODO: This should be the default persistence location.
+		if (identifier.equals("")) {
+			return JIRAISSUE;
 		}
+		for (DocumentationLocation location : values()) {
+			if (location.identifier == identifier) {
+				return location;
+			}
+		}
+		return UNKNOWN;
 	}
 
 	public static String getIdentifier(DocumentationLocation documentationLocation) {
 		if (documentationLocation == null) {
-			return "";
+			return UNKNOWN.identifier;
 		}
-		switch (documentationLocation) {
-			case JIRAISSUE:
-				return "i";
-			case JIRAISSUETEXT:
-				return "s";
-			case ACTIVEOBJECT:
-				return "a";
-			case COMMIT:
-				return "c";
-			case PULLREQUEST:
-				return "p";
-			case UNKNOWN:
-				return "u";
-			default:
-				return "";
-		}
+		return documentationLocation.identifier;
 	}
 
 	public String getIdentifier() {
-		return getIdentifier(this);
+		return identifier;
 	}
 
 	public static String getName(DocumentationLocation documentationLocation) {
 		if (documentationLocation == null) {
-			return "";
+			return UNKNOWN.name;
 		}
-		switch (documentationLocation) {
-			case JIRAISSUE:
-				return "JiraIssues";
-			case JIRAISSUETEXT:
-				return "JiraIssueText";
-			case ACTIVEOBJECT:
-				return "ActiveObject";
-			case COMMIT:
-				return "Commit";
-			case PULLREQUEST:
-				return "PullRequest";
-			case UNKNOWN:
-				return "Unknown";
-			default:
-				return "";
-		}
+		return documentationLocation.name;
+	}
+
+	public String getName() {
+		return name;
 	}
 
 	public static String getIdentifier(DecisionKnowledgeElement element) {
 		if (element == null || element.getDocumentationLocation() == null) {
 			return "";
-		} else if (element instanceof PartOfJiraIssueText || element.getDocumentationLocation().equals(DocumentationLocation.JIRAISSUETEXT)) {
+		} else if (element instanceof PartOfJiraIssueText
+				|| element.getDocumentationLocation().equals(DocumentationLocation.JIRAISSUETEXT)) {
 			return "s";
 		} else if (element.getDocumentationLocation().equals(DocumentationLocation.ACTIVEOBJECT)) {
 			return "a";
@@ -103,39 +86,34 @@ public enum DocumentationLocation {
 	}
 
 	public static DocumentationLocation getDocumentationLocationFromString(String locationString) {
-		String lowCaseLocationString = locationString.toLowerCase();
-		switch (lowCaseLocationString) {
-			case "jiraissue":
-				return JIRAISSUE;
-			case "jiraissuetext":
-				return JIRAISSUETEXT;
-			case "activeobject":
-				return ACTIVEOBJECT;
-			case "commit":
-				return COMMIT;
-			case "pullrequest":
-				return PULLREQUEST;
-			default:
-				return UNKNOWN;
-
+		if (locationString == null) {
+			return UNKNOWN;
 		}
+		for (DocumentationLocation location : values()) {
+			if (location.name.equalsIgnoreCase(locationString)) {
+				return location;
+			}
+		}
+		return UNKNOWN;
 	}
 
 	/**
-	 * Convert the documentation locations to a String starting with a capital letter, e.g.,
-	 * Pullrequest, Jiraissue, Commit
+	 * Convert the documentation locations to a String starting with a capital
+	 * letter, e.g., Pullrequest, Jiraissue, Commit
+	 * 
 	 * @return documentation locations as a String starting with a capital letter.
 	 */
 	@Override
 	public String toString() {
 		return this.name().substring(0, 1).toUpperCase(Locale.ENGLISH)
-				       + this.name().substring(1).toLowerCase(Locale.ENGLISH);
+				+ this.name().substring(1).toLowerCase(Locale.ENGLISH);
 	}
 
 	/**
 	 * Convert all documentation locations to a list of String.
 	 *
-	 * @return list of documentation locations as Strings starting with a capital letter.
+	 * @return list of documentation locations as Strings starting with a capital
+	 *         letter.
 	 */
 	public static List<String> toList() {
 		List<String> documentationLocationList = new ArrayList<String>();
@@ -145,9 +123,9 @@ public enum DocumentationLocation {
 		return documentationLocationList;
 	}
 
-	public static List<DocumentationLocation> getAllDocumentationLocations(){
+	public static List<DocumentationLocation> getAllDocumentationLocations() {
 		List<DocumentationLocation> locations = new ArrayList<>();
-		for(String location: DocumentationLocation.toList()){
+		for (String location : DocumentationLocation.toList()) {
 			locations.add(DocumentationLocation.getDocumentationLocationFromString(location));
 		}
 		return locations;
--- src/test/java/de/uhd/ifi/se/decision/management/jira/model/TestDocumentationLocation.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/model/TestDocumentationLocation.java
@@ -1,24 +1,29 @@
 package de.uhd.ifi.se.decision.management.jira.model;
 
-import org.junit.Test;
-
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 public class TestDocumentationLocation {
 
 	@Test
-	public void testGetDocumentationLocationFromIdentifier(){
-		for(String location: DocumentationLocation.toList()){
+	public void testGetDocumentationLocationFromIdentifier() {
+		for (String location : DocumentationLocation.toList()) {
 			assertTrue(location.equals(DocumentationLocation.getDocumentationLocationFromString(location).toString()));
 		}
 	}
 
 	@Test
-	public void testGetIdentifier(){
-		for(DocumentationLocation location : DocumentationLocation.values()){
-			String identifier = DocumentationLocation.getIdentifier(location);
-			DocumentationLocation newLocation = DocumentationLocation.getDocumentationLocationFromIdentifier(identifier);
-			assertTrue(location.toString().equals(newLocation.toString()));
+	public void testGetIdentifier() {
+		for (DocumentationLocation location : DocumentationLocation.values()) {
+			String identifier = location.getIdentifier();
+			DocumentationLocation newLocation = DocumentationLocation
+					.getDocumentationLocationFromIdentifier(identifier);
+			if (location == DocumentationLocation.UNKNOWN) {
+				continue;
+			}
+			assertEquals(location, newLocation);
 		}
 	}
 }
