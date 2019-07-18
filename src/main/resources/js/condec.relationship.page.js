afb717edf48422506909cf216d389af06af8f8bb
Ines Hamma <ineshamma@Iness-MacBook-Pro.local>
2019-07-18 13:36:57 +0200

    CONDEC-523: Add relationship view

---
+++ src/main/resources/js/condec.relationship.page.js
@@ -0,0 +1,60 @@
+(function(global) {
+
+	/* private vars */
+	var conDecObservable = null;
+	var conDecAPI = null;
+
+	var ConDecRelationshipPage = function ConDecRelationshipPage() {
+	};
+
+	ConDecRelationshipPage.prototype.init = function(_conDecAPI, _conDecObservable) {
+		console.log("ConDecRelationshipPage init");
+		if (isConDecAPIType(_conDecAPI) && isConDecObservableType(_conDecObservable)) {
+			conDecAPI = _conDecAPI;
+			conDecObservable = _conDecObservable;
+
+			conDecObservable.subscribe(this);
+			return true;
+		}
+		return false;
+	};
+
+	ConDecRelationshipPage.prototype.buildDecisionGraph = function() {
+		console.log("ConDec build Decision Relationship Graph");
+
+		conDecAPI.getVis("KB-7", "", function(data){
+			var dataset = {
+				nodes: data.nodes,
+				edges: data.edges
+			};
+
+			var graphContainer = document.getElementById('graph-container');
+
+			var options = {};
+
+			var graphNetwork = new vis.Network(graphContainer, dataset, options);
+		})
+
+	};
+
+	/*
+	 * Init Helpers
+	 */
+	function isConDecAPIType(conDecAPI) {
+		if (!(conDecAPI !== undefined && conDecAPI.getDecisionKnowledgeElement !== undefined && typeof conDecAPI.getDecisionKnowledgeElement === 'function')) {
+			console.warn("ConDecKnowledgePage: invalid ConDecAPI object received.");
+			return false;
+		}
+		return true;
+	}
+
+	function isConDecObservableType(conDecObservable) {
+		if (!(conDecObservable !== undefined && conDecObservable.notify !== undefined && typeof conDecObservable.notify === 'function')) {
+			console.warn("ConDecKnowledgePage: invalid ConDecObservable object received.");
+			return false;
+		}
+		return true;
+	}
+
+	global.conDecRelationshipPage = new ConDecRelationshipPage();
+})(window);
