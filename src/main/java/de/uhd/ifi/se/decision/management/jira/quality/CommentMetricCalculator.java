ec7ce7ec52bb4a54ab8ed1c0c4b3aedf34aa2bb2
Lukasz Wisniowski <u123456.lw@gmail.com>
2019-08-03 18:00:44 +0200

    CONDEC-535 add navigation from report charts
    * squash merge branch LWs.PoC-NavigationFromReportsToJiraTasks

--- src/main/java/de/uhd/ifi/se/decision/management/jira/quality/CommentMetricCalculator.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/quality/CommentMetricCalculator.java
@@ -1,18 +1,17 @@
 package de.uhd.ifi.se.decision.management.jira.quality;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import de.uhd.ifi.se.decision.management.jira.extraction.GitClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.atlassian.jira.bc.issue.search.SearchService;
 import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.issue.Issue;
-import com.atlassian.jira.issue.issuetype.IssueType;
 import com.atlassian.jira.issue.search.SearchException;
 import com.atlassian.jira.issue.search.SearchResults;
 import com.atlassian.jira.jql.builder.JqlClauseBuilder;
@@ -21,41 +20,25 @@ import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.jira.web.bean.PagerFilter;
 import com.atlassian.query.Query;
 
-import de.uhd.ifi.se.decision.management.jira.config.JiraIssueTypeGenerator;
-import de.uhd.ifi.se.decision.management.jira.extraction.GitClient;
-import de.uhd.ifi.se.decision.management.jira.extraction.impl.GitClientImpl;
 import de.uhd.ifi.se.decision.management.jira.filtering.JiraSearchServiceHelper;
 import de.uhd.ifi.se.decision.management.jira.model.DecisionKnowledgeElement;
-import de.uhd.ifi.se.decision.management.jira.model.DocumentationLocation;
-import de.uhd.ifi.se.decision.management.jira.model.Graph;
-import de.uhd.ifi.se.decision.management.jira.model.KnowledgeType;
-import de.uhd.ifi.se.decision.management.jira.model.Link;
-import de.uhd.ifi.se.decision.management.jira.model.impl.DecisionKnowledgeElementImpl;
-import de.uhd.ifi.se.decision.management.jira.model.impl.GraphImpl;
 import de.uhd.ifi.se.decision.management.jira.model.text.PartOfJiraIssueText;
-import de.uhd.ifi.se.decision.management.jira.persistence.GenericLinkManager;
 import de.uhd.ifi.se.decision.management.jira.persistence.JiraIssueTextPersistenceManager;
-import de.uhd.ifi.se.decision.management.jira.view.treant.Node;
 
 public class CommentMetricCalculator {
 
 	private String projectKey;
 	private ApplicationUser user;
-	private JiraIssueTextPersistenceManager persistenceManager;
-	private String jiraIssueTypeId;
 	private List<Issue> jiraIssues;
 	private int absolutDepth;
 	private GitClient gitClient;
 
 	protected static final Logger LOGGER = LoggerFactory.getLogger(CommentMetricCalculator.class);
 
-	public CommentMetricCalculator(long projectId, ApplicationUser user, String jiraIssueTypeId) {
+	public CommentMetricCalculator(long projectId, ApplicationUser user) {
 		this.projectKey = ComponentAccessor.getProjectManager().getProjectObj(projectId).getKey();
 		this.user = user;
-		this.persistenceManager = new JiraIssueTextPersistenceManager(projectKey);
-		this.jiraIssueTypeId = jiraIssueTypeId;
 		this.jiraIssues = getJiraIssuesForProject(projectId);
-		this.gitClient = new GitClientImpl(projectKey);
 	}
 
 	private List<Issue> getJiraIssuesForProject(long projectId) {
@@ -88,25 +71,6 @@ public class CommentMetricCalculator {
 		return numberOfCommentsForJiraIssues;
 	}
 
-	public Map<String, Integer> getNumberOfDecisionKnowledgeElementsForJiraIssues(KnowledgeType type) {
-		if (type == null) {
-			return new HashMap<String, Integer>();
-		}
-		Map<String, Integer> numberOfSentencesForJiraIssues = new HashMap<String, Integer>();
-		for (Issue jiraIssue : jiraIssues) {
-			int numberOfElements = 0;
-			List<DecisionKnowledgeElement> elements = JiraIssueTextPersistenceManager
-					.getElementsForIssue(jiraIssue.getId(), projectKey);
-			for (DecisionKnowledgeElement element : elements) {
-				if (element.getType().equals(type)) {
-					numberOfElements++;
-				}
-			}
-			numberOfSentencesForJiraIssues.put(jiraIssue.getKey(), numberOfElements);
-		}
-		return numberOfSentencesForJiraIssues;
-	}
-
 	public Map<String, Integer> getNumberOfRelevantSentences() {
 		Map<String, Integer> numberOfRelevantSentences = new HashMap<String, Integer>();
 		int isRelevant = 0;
@@ -129,202 +93,6 @@ public class CommentMetricCalculator {
 		numberOfRelevantSentences.put("Irrelevant Sentences", isIrrelevant);
 
 		return numberOfRelevantSentences;
-	}
-
-	public Map<String, Integer> getNumberOfCommitsForJiraIssues() {
-		Map<String, Integer> resultMap = new HashMap<String, Integer>();
-		for (Issue jiraIssue : jiraIssues) {
-			int numberOfCommits = gitClient.getNumberOfCommits(jiraIssue);
-			resultMap.put(jiraIssue.getKey(), numberOfCommits);
-		}
-		return resultMap;
-	}
-
-	public Map<String, Integer> getDistributionOfKnowledgeTypes() {
-		Map<String, Integer> distributionOfKnowledgeTypes = new HashMap<String, Integer>();
-		for (KnowledgeType type : KnowledgeType.getDefaultTypes()) {
-			distributionOfKnowledgeTypes.put(type.toString(),
-					persistenceManager.getDecisionKnowledgeElements(type).size());
-		}
-		return distributionOfKnowledgeTypes;
-	}
-
-	public Map<String, Integer> getNumberOfLinksToOtherElement(KnowledgeType linkFrom, KnowledgeType linkTo) {
-		if (linkFrom == null || linkTo == null) {
-			return new HashMap<String, Integer>();
-		}
-		Integer[] statistics = new Integer[4];
-		Arrays.fill(statistics, 0);
-
-		List<DecisionKnowledgeElement> listOfIssues = this.persistenceManager.getDecisionKnowledgeElements(linkFrom);
-
-		for (DecisionKnowledgeElement issue : listOfIssues) {
-			List<Link> links = GenericLinkManager.getLinksForElement(issue.getId(),
-					DocumentationLocation.JIRAISSUETEXT);
-			boolean hastOtherElementLinked = false;
-
-			for (Link link : links) {
-				if (link.isValid()) {
-					DecisionKnowledgeElement dke = link.getOppositeElement(issue.getId());
-					if (dke instanceof PartOfJiraIssueText && dke.getType().equals(linkTo)) { // alt
-						hastOtherElementLinked = true;
-					}
-				}
-			}
-			if (hastOtherElementLinked) {
-				statistics[0] = statistics[0] + 1;
-			} else if (!hastOtherElementLinked) {
-				statistics[1] = statistics[1] + 1;
-			}
-		}
-
-		// Hashmaps as counter suck
-		Map<String, Integer> dkeCount = new HashMap<String, Integer>();
-		dkeCount.put(linkFrom.toString() + " has " + linkTo.toString(), statistics[0]);
-		dkeCount.put(linkFrom.toString() + " has no " + linkTo.toString(), statistics[1]);
-
-		return dkeCount;
-	}
-
-	public String issuesWithNoExistingLinksToDecisionKnowledge(KnowledgeType linkFrom) {
-		if (linkFrom == null) {
-			return "";
-		}
-		String listOfElementsWithoutLink = " ";
-		List<DecisionKnowledgeElement> listOfIssues = this.persistenceManager.getDecisionKnowledgeElements(linkFrom);
-
-		for (DecisionKnowledgeElement issue : listOfIssues) {
-			if (issue instanceof PartOfJiraIssueText
-					&& !listOfElementsWithoutLink.contains(((PartOfJiraIssueText) issue).getKey().split(":")[0])) {
-				listOfElementsWithoutLink += ((PartOfJiraIssueText) issue).getKey().split(":")[0] + " ";
-			}
-		}
-		return listOfElementsWithoutLink;
-	}
-
-	public Map<String, Integer> getAlternativeArguments() {
-		int alternativesHaveArgument = 0;
-		int alternativesHaveNoArgument = 0;
-
-		List<DecisionKnowledgeElement> alternatives = this.persistenceManager
-				.getDecisionKnowledgeElements(KnowledgeType.ALTERNATIVE);
-
-		for (DecisionKnowledgeElement currentAlternative : alternatives) {
-			List<Link> links = GenericLinkManager.getLinksForElement(currentAlternative.getId(),
-					DocumentationLocation.JIRAISSUETEXT);
-			boolean hasArgument = false;
-			for (Link link : links) {
-				if (link.isValid()) {
-					DecisionKnowledgeElement dke = link.getOppositeElement(currentAlternative.getId());
-					if (dke instanceof PartOfJiraIssueText && dke.getType().equals(KnowledgeType.ARGUMENT)) {
-						hasArgument = true;
-					}
-				}
-			}
-			if (hasArgument) {
-				alternativesHaveArgument++;
-			} else {
-				alternativesHaveNoArgument++;
-
-			}
-		}
-		Map<String, Integer> dkeCount = new HashMap<String, Integer>();
-		dkeCount.put("Alternative with Argument", alternativesHaveArgument);
-		dkeCount.put("Alternative without Argument", alternativesHaveNoArgument);
-
-		return dkeCount;
-	}
-
-	public List<Integer> getLinkDistance(KnowledgeType type) {
-		if (type == null) {
-			return new ArrayList<Integer>();
-		}
-		List<Integer> linkDistances = new ArrayList<Integer>();
-
-		List<DecisionKnowledgeElement> listOfIssues = persistenceManager.getDecisionKnowledgeElements(type);
-
-		for (DecisionKnowledgeElement currentAlternative : listOfIssues) {
-			int depth = graphRecursionBot(currentAlternative);
-			linkDistances.add(depth);
-		}
-
-		return linkDistances;
-	}
-
-	public Object getLinksToIssueTypeMap(KnowledgeType knowledgeType) {
-		if (knowledgeType == null) {
-			return null;
-		}
-		Map<String, Integer> result = new HashMap<String, Integer>();
-		int withLink = 0;
-		int withoutLink = 0;
-		for (Issue issue : jiraIssues) {
-			boolean linkExisting = false;
-			if (!checkEqualIssueTypeIssue(issue.getIssueType())) {
-				continue;
-			}
-			for (Link link : GenericLinkManager.getLinksForElement(issue.getId(), DocumentationLocation.JIRAISSUE)) {
-				if (link.isValid()) {
-					DecisionKnowledgeElement dke = link.getOppositeElement(new DecisionKnowledgeElementImpl(issue));
-					if (dke.getType().equals(knowledgeType)) {
-						linkExisting = true;
-					}
-				}
-			}
-
-			if (linkExisting) {
-				withLink++;
-			} else {
-				withoutLink++;
-			}
-		}
-
-		String jiraIssueTypeName = JiraIssueTypeGenerator.getJiraIssueTypeName(jiraIssueTypeId);
-
-		result.put("Links from " + jiraIssueTypeName + " to " + knowledgeType.toString(), withLink);
-		result.put("No links from " + jiraIssueTypeName + " to " + knowledgeType.toString(), withoutLink);
-		return result;
-	}
-
-	private boolean checkEqualIssueTypeIssue(IssueType issueType2) {
-		if (issueType2 == null) {
-			return false;
-		}
-
-		String jiraIssueTypeName = JiraIssueTypeGenerator.getJiraIssueTypeName(jiraIssueTypeId);
-		return issueType2.getName().equalsIgnoreCase(jiraIssueTypeName);
-	}
-
-	private int graphRecursionBot(DecisionKnowledgeElement dke) {
-		this.absolutDepth = 0;
-		Graph graph = new GraphImpl(projectKey, dke.getKey());
-		this.createNodeStructure(dke, null, 100, 1, graph);
-		return absolutDepth;
-	}
-
-	private Node createNodeStructure(DecisionKnowledgeElement element, Link link, int depth, int currentDepth,
-			Graph graph) {
-		if (element == null || element.getProject() == null || element.getType() == KnowledgeType.OTHER) {
-			return new Node();
-		}
-		Map<DecisionKnowledgeElement, Link> childrenAndLinks = graph.getAdjacentElementsAndLinks(element);
-		Node node;
-		if (link != null) {
-			node = new Node(element, link, false, false);
-		} else {
-			node = new Node(element, false, false);
-		}
-		List<Node> nodes = new ArrayList<Node>();
-		for (Map.Entry<DecisionKnowledgeElement, Link> childAndLink : childrenAndLinks.entrySet()) {
-			Node newChildNode = createNodeStructure(childAndLink.getKey(), childAndLink.getValue(), depth,
-					currentDepth + 1, graph);
-			if (absolutDepth < currentDepth) {
-				absolutDepth = currentDepth;
-			}
-			nodes.add(newChildNode);
-		}
-		node.setChildren(nodes);
-		return node;
 	}
 
 }
--- src/main/java/de/uhd/ifi/se/decision/management/jira/view/DecisionKnowledgeReport.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/view/DecisionKnowledgeReport.java
@@ -6,10 +6,12 @@ import java.util.Map;
 import com.atlassian.jira.plugin.report.impl.AbstractReport;
 import com.atlassian.jira.util.ParameterUtils;
 import com.atlassian.jira.web.action.ProjectActionSupport;
+import com.atlassian.jira.config.properties.APKeys;
 
 import de.uhd.ifi.se.decision.management.jira.config.JiraIssueTypeGenerator;
 import de.uhd.ifi.se.decision.management.jira.model.KnowledgeType;
 import de.uhd.ifi.se.decision.management.jira.quality.CommentMetricCalculator;
+import de.uhd.ifi.se.decision.management.jira.quality.CommonMetricCalculator;
 
 /**
  * Renders the report page.
@@ -27,64 +29,125 @@ public class DecisionKnowledgeReport extends AbstractReport {
 	}
 
 	public Map<String, Object> createValues(ProjectActionSupport action) {
-		CommentMetricCalculator calculator = new CommentMetricCalculator(projectId, action.getLoggedInUser(),
+		CommentMetricCalculator calculatorForSentences = new CommentMetricCalculator(projectId, action.getLoggedInUser());
+		CommonMetricCalculator calculator = new CommonMetricCalculator(projectId, action.getLoggedInUser(),
 				jiraIssueTypeId);
 
 		Map<String, Object> velocityParams = new HashMap<String, Object>();
+
+		// Push some basic parameters
+		String issueTypeName = JiraIssueTypeGenerator.getJiraIssueTypeName(jiraIssueTypeId);
+		String jiraBaseUrl = action.getApplicationProperties().getString(APKeys.JIRA_BASEURL);
+
 		velocityParams.put("projectName", action.getProjectManager().getProjectObj(projectId).getName());
+		velocityParams.put("jiraBaseUrl", jiraBaseUrl);
 
-		// Name of selected JIRA issue type
-		velocityParams.put("issueType", JiraIssueTypeGenerator.getJiraIssueTypeName(jiraIssueTypeId));
+		// prepare calculations
+		Map<String, String> chartNamesAndPurpose = new HashMap<String, String>();
+		Map<String, Object> chartNamesAndData = new HashMap<String, Object>();
+		String chartId = "";
 
-		// Number of comments per JIRA issue (of the selected JIRA issue type)
-		velocityParams.put("numberOfCommentsForJiraIssues", calculator.getNumberOfCommentsForJiraIssues());
+		/* general statistics */
+		chartId = "piechartInteger-RelevantSentences";
+		chartNamesAndPurpose.put(chartId, "Relevance of Sentences in JIRA Issue Comments");
+		chartNamesAndData.put(chartId, calculatorForSentences.getNumberOfRelevantSentences());
 
-		// Number of commits per JIRA issue
-		velocityParams.put("numberOfCommitsForJiraIssues", calculator.getNumberOfCommitsForJiraIssues());
+		chartId = "piechartInteger-KnowledgeTypeDistribution";
+		chartNamesAndPurpose.put(chartId, "Distribution of Knowledge Types");
+		chartNamesAndData.put(chartId, calculator.getDistributionOfKnowledgeTypes());
 
-		// Number of issues (decision problems) per JIRA issue
-		velocityParams.put("numberOfIssuesForJiraIssues",
-				calculator.getNumberOfDecisionKnowledgeElementsForJiraIssues(KnowledgeType.ISSUE));
+		/* selected issue type stats */
+		chartId = "piechartRich-DecisionDocumentedForSelectedJiraIssue";
+		chartNamesAndPurpose.put(chartId, "For how many JIRA issues of type "+issueTypeName+" is the issue documented?");
+		chartNamesAndData.put(chartId, calculator.getLinksToIssueTypeMap(KnowledgeType.ISSUE));
 
-		// Number of decisions per JIRA issue
-		velocityParams.put("numberOfDecisionsForJiraIssues",
+		chartId = "piechartRich-IssueDocumentedForSelectedJiraIssue";
+		chartNamesAndPurpose.put(chartId, "For how many JIRA issues of type "+issueTypeName+" is the decision documented?");
+		chartNamesAndData.put(chartId, calculator.getLinksToIssueTypeMap(KnowledgeType.DECISION));
+
+		/* towards rationale completeness and inconsistencies */
+		chartId = "piechartRich-IssuesSolvedByDecision";
+		chartNamesAndPurpose.put(chartId, "How many issues (=decision problems) are solved by a decision?");
+		chartNamesAndData.put(chartId,
+				calculator.getDecKnowlElementsOfATypeGroupedByHavingElementsOfOtherType(KnowledgeType.ISSUE
+						, KnowledgeType.DECISION)
+		);
+		chartId = "piechartRich-DecisionsSolvingIssues";
+		chartNamesAndPurpose.put(chartId, "For how many decisions is the issue (=decision problem) documented?");
+		chartNamesAndData.put(chartId,
+				calculator.getDecKnowlElementsOfATypeGroupedByHavingElementsOfOtherType(KnowledgeType.DECISION
+						, KnowledgeType.ISSUE)
+		);
+
+		chartId = "piechartRich-ProArgumentDocumentedForAlternative";
+		chartNamesAndPurpose.put(chartId, "How many alternatives have at least one pro argument documented?");
+		chartNamesAndData.put(chartId
+				, calculator.getDecKnowlElementsOfATypeGroupedByHavingElementsOfOtherType(KnowledgeType.ALTERNATIVE
+						, KnowledgeType.PRO));
+
+		chartId = "piechartRich-ConArgumentDocumentedForAlternative";
+		chartNamesAndPurpose.put(chartId, "How many alternatives have at least one con argument documented?");
+		chartNamesAndData.put(chartId
+				, calculator.getDecKnowlElementsOfATypeGroupedByHavingElementsOfOtherType(KnowledgeType.ALTERNATIVE
+						, KnowledgeType.CON));
+
+		chartId = "piechartRich-ProArgumentDocumentedForDecision";
+		chartNamesAndPurpose.put(chartId, "How many decisions have at least one pro argument documented?");
+		chartNamesAndData.put(chartId
+				, calculator.getDecKnowlElementsOfATypeGroupedByHavingElementsOfOtherType(KnowledgeType.DECISION
+						, KnowledgeType.PRO));
+
+		chartId = "piechartRich-ConArgumentDocumentedForDecision";
+		chartNamesAndPurpose.put(chartId, "How many decisions have at least one con argument documented?");
+		chartNamesAndData.put(chartId
+				, calculator.getDecKnowlElementsOfATypeGroupedByHavingElementsOfOtherType(KnowledgeType.DECISION
+						, KnowledgeType.CON));
+
+		/* rationale inconsistencies */
+		chartId = "piechartRich-IssuesSolvedByManyDecisions";
+		chartNamesAndPurpose.put(chartId, "Issues (=decision problems) solved by more than one decision");
+		chartNamesAndData.put(chartId,
+				calculator.getDecKnowlElementsOfATypeGroupedByHavingMoreThanOneElementsOfOtherType(KnowledgeType.ISSUE
+						, KnowledgeType.DECISION)
+		);
+
+		/* distances */
+		chartId = "boxplot-CommentsPerJiraIssue";
+		chartNamesAndPurpose.put(chartId, "\\#Comments per JIRA Issue");
+		chartNamesAndData.put(chartId, calculatorForSentences.getNumberOfCommentsForJiraIssues());
+		chartId = "boxplot-CommitsPerJiraIssue";
+		chartNamesAndPurpose.put(chartId, "\\#Commits per JIRA Issue");
+		chartNamesAndData.put(chartId, calculator.getNumberOfCommitsForJiraIssues());
+
+		chartId = "boxplot-DecisionsPerJiraIssue";
+		chartNamesAndPurpose.put(chartId, "\\#Decisions per JIRA Issue");
+		chartNamesAndData.put(chartId,
 				calculator.getNumberOfDecisionKnowledgeElementsForJiraIssues(KnowledgeType.DECISION));
 
-		// Link distance from elements with certain knowledge type
-		velocityParams.put("numLinkDistanceIssue", calculator.getLinkDistance(KnowledgeType.ISSUE));
-		velocityParams.put("numLinkDistanceAlternative", calculator.getLinkDistance(KnowledgeType.ALTERNATIVE));
-		velocityParams.put("numLinkDistanceDecision", calculator.getLinkDistance(KnowledgeType.DECISION));
+		chartId = "boxplot-IssuesPerJiraIssue";
+		chartNamesAndPurpose.put(chartId, "\\#Issues per JIRA Issue");
+		chartNamesAndData.put(chartId,
+				calculator.getNumberOfDecisionKnowledgeElementsForJiraIssues(KnowledgeType.ISSUE));
 
-		// Number of relevant sentences per JIRA issue
-		velocityParams.put("numRelevantSentences", calculator.getNumberOfRelevantSentences());
+		chartId = "boxplot-LinkDistanceFromIssue";
+		chartNamesAndPurpose.put(chartId, "Link Distance from Issue");
+		chartNamesAndData.put(chartId,
+				calculator.getLinkDistance(KnowledgeType.ISSUE));
 
-		// Distribution of Knowledge Types in JIRA project
-		velocityParams.put("distriutionOfKnowledgeTypesInProject", calculator.getDistributionOfKnowledgeTypes());
+		chartId = "boxplot-LinkDistanceFromAlternative";
+		chartNamesAndPurpose.put(chartId, "Link Distance from Alternative");
+		chartNamesAndData.put(chartId,
+				calculator.getLinkDistance(KnowledgeType.ALTERNATIVE));
 
-		// How many issues (=decision problems) are solved by a decision?
-		velocityParams.put("numberOfLinksFromIssuesToDecisions",
-				calculator.getNumberOfLinksToOtherElement(KnowledgeType.ISSUE, KnowledgeType.DECISION));
-		velocityParams.put("issuesWithoutDecisionLinks",
-				calculator.issuesWithNoExistingLinksToDecisionKnowledge(KnowledgeType.ISSUE));
+		chartId = "boxplot-LinkDistanceFromDecision";
+		chartNamesAndPurpose.put(chartId, "Link Distance from Decision");
+		chartNamesAndData.put(chartId,
+				calculator.getLinkDistance(KnowledgeType.DECISION));
 
-		// For how many decisions is the issue (=decision problem) documented?
-		velocityParams.put("numberOfLinksFromDecisionsToIssues",
-				calculator.getNumberOfLinksToOtherElement(KnowledgeType.DECISION, KnowledgeType.ISSUE));
-		velocityParams.put("decisionsWithoutIssueLinks",
-				calculator.issuesWithNoExistingLinksToDecisionKnowledge(KnowledgeType.DECISION));
 
-		// How many alternatives have at least one argument documented?
-		velocityParams.put("numAlternativeWoArgument", calculator.getAlternativeArguments());
-		velocityParams.put("issuesWithAltWoArg",
-				calculator.issuesWithNoExistingLinksToDecisionKnowledge(KnowledgeType.ALTERNATIVE));
-
-		velocityParams.put("numLinksToIssueTypeIssue", calculator.getLinksToIssueTypeMap(KnowledgeType.ISSUE));
-		velocityParams.put("jiraIssuesWithoutLinksToIssue",
-				calculator.issuesWithNoExistingLinksToDecisionKnowledge(KnowledgeType.ISSUE));
-
-		velocityParams.put("numLinksToIssueTypeDecision", calculator.getLinksToIssueTypeMap(KnowledgeType.DECISION));
-		velocityParams.put("jiraIssuesWithoutLinksToDecision",
-				calculator.issuesWithNoExistingLinksToDecisionKnowledge(KnowledgeType.DECISION));
+		// push gathered data to velocity template
+		velocityParams.put("chartNamesAndPurpose", chartNamesAndPurpose);
+		velocityParams.put("chartNamesAndData", chartNamesAndData);
 
 		return velocityParams;
 	}
