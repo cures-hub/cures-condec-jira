dd2408f74b20f8929ca6059cf89c4a3213e7bb4c
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2019-08-09 17:30:16 +0200

    ConDec-539: Move removal of non-Java classes to createSummary(Diff diff) method since this method is called by createSummary(RevCommit commit) and createSummary(Issue jiraIssue)

--- src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/impl/CodeSummarizerImpl.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/impl/CodeSummarizerImpl.java
@@ -11,9 +11,6 @@ import de.uhd.ifi.se.decision.management.jira.extraction.TangledChangeDetector;
 import de.uhd.ifi.se.decision.management.jira.model.git.ChangedFile;
 import de.uhd.ifi.se.decision.management.jira.model.git.Diff;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class CodeSummarizerImpl implements CodeSummarizer {
 
 	private GitClient gitClient;
@@ -48,13 +45,6 @@ public class CodeSummarizerImpl implements CodeSummarizer {
 			return "";
 		}
 		Diff diff = gitClient.getDiff(commit);
-		List<ChangedFile> isNotJava = new ArrayList<>();
-		for (ChangedFile changedFile :diff.getChangedFiles()) {
-			if(!changedFile.isExistingJavaClass()){
-				isNotJava.add(changedFile);
-			}
-		}
-		diff.getChangedFiles().removeAll(isNotJava);
 		return createSummary(diff);
 	}
 
@@ -64,6 +54,7 @@ public class CodeSummarizerImpl implements CodeSummarizer {
 			return "";
 		}
 
+		diff.getChangedFiles().removeIf(changedFile -> !changedFile.isExistingJavaClass());
 		TangledChangeDetector tangledCommitDetection = new TangledChangeDetectorImpl();
 		tangledCommitDetection.estimateWhetherChangedFilesAreCorrectlyIncludedInDiff(diff);
 
