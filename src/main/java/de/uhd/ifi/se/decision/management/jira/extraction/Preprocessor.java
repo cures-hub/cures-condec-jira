925eabbe1851109738bf43835ff0fcb4d4ac0cb0
Philipp de Sombre <philipp@desomb.re>
2019-09-02 16:25:10 +0200

    Added text preprocessing interface and started implementation.

---
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/Preprocessor.java
@@ -0,0 +1,104 @@
+package de.uhd.ifi.se.decision.management.jira.extraction;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+public interface Preprocessor {
+
+    /**
+     * Generates a List of tokenized word from a sentence.
+     *
+     * @param sentence
+     * @return List of word tokens
+     */
+    public List<String> tokenize(String sentence);
+
+    /**
+     * Removes unwanted patterns from the String using regular expressions.
+     * E.g.: removing newline character.
+     *
+     * @param sentence Sentence that has to be cleaned.
+     * @param regex    Regular Expression used to be filter out unwanted parts of text.
+     * @return Cleaned sentence.
+     */
+    public String removeUsingRegEx(String sentence, Pattern regex);
+
+    /**
+     * Converts a list of tokens into their lemmatized form.
+     * What is lemmatisation? https://en.wikipedia.org/wiki/Lemmatisation
+     * E.g.: "better" -> "good"
+     *
+     * @param tokens of words to be lemmatized.
+     * @return List of lemmatized tokens.
+     */
+    public List<String> lemmatize(List<String> tokens);
+
+    /**
+     * Converts a word-token into its lemmatized form.
+     *
+     * @param token word to be lemmatized
+     * @return lemmatized word-token
+     */
+    public String lemmatize(String token);
+
+
+    /**
+     *  Stems a list of word tokens.
+     *  What is stemming? https://en.wikipedia.org/wiki/Stemming
+     *  E.g.: "running" -> "run"
+     *
+     * @param tokens to be stemmed
+     * @return List of stemmed tokend
+     *
+     *  Not useful when already using lemmatizer.
+     *  public List stem(List<String> tokens);
+     */
+
+
+    /**
+     *  Stems a single word token String.
+     *
+     * @param token to be stemmed
+     * @return stemmed word token
+     *
+     * Not useful when already using lemmatizer.
+     * public String stem(String token);
+     */
+
+    /**
+     * Generates N-Grams from list of tokens.
+     * What are N-Grams? https://en.wikipedia.org/wiki/N-gram
+     * E.g.: "The cow jumps over the moon". N=3 -> "the cow jumps", "cow jumps over", "jumps over the", "over the moon"
+     * (example from: http://text-analytics101.rxnlp.com/2014/11/what-are-n-grams.html)
+     *
+     * @param tokens tokenized setntence used t generate N-Grams
+     * @param N      N-Gram number
+     * @return List of N-Grams
+     */
+    public List generateNGram(List tokens, Integer N);
+
+    /**
+     * Converts word tokens to a numerical representation. This is necessary for calculations for the classification.
+     *
+     * @param tokens List of words in String-representation
+     * @return List of words in numerical representation
+     */
+    public List<Double> convertToNumbers(List<String> tokens);
+
+    /**
+     * Converts a single word to its numerical representation.
+     *
+     * @param token word to be converted
+     * @return numerical representation of the parameter word
+     */
+    public Double convertToNumbers(String token);
+
+    /**
+     * This method executes all necessary preprocessing steps.
+     *
+     * @param sentence to be preprocessed
+     * @return N-Gram numerical representation of sentence
+     */
+    public List<Double> preprocess(String sentence);
+
+}
