d2d94b871bd773064f04f4d4874ad2d6c8621eb0
Philipp de Sombre <philipp@desomb.re>
2019-09-03 18:11:40 +0200

    Finished writing tests.
    Coverage:  Method, % 100% (9/ 9); Line, % 96.2% (50/ 52)

--- src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/classification/TestPreprocessorImpl.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/classification/TestPreprocessorImpl.java
@@ -1,12 +1,10 @@
 package de.uhd.ifi.se.decision.management.jira.extraction.classification;
 
 import de.uhd.ifi.se.decision.management.jira.TestSetUp;
-import de.uhd.ifi.se.decision.management.jira.extraction.DecisionKnowledgeClassifier;
 import de.uhd.ifi.se.decision.management.jira.extraction.Preprocessor;
 import de.uhd.ifi.se.decision.management.jira.extraction.impl.PreprocessorImpl;
 import org.junit.Test;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -25,12 +23,22 @@ public class TestPreprocessorImpl extends TestSetUp {
     }
 
     @Test
-    public void testLemmatizingWorks() {
+    public void testLemmatizationWorksStandalone() {
         Preprocessor pp = new PreprocessorImpl();
         List<String> tokenizedTestSentence = Arrays.asList("The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog", ".");
-        System.out.println(pp.preprocess(TestPreprocessorImpl.testSentence));
-        assertEquals(pp.tokenize(TestPreprocessorImpl.testSentence), tokenizedTestSentence);
+        assertEquals(pp.lemmatize(tokenizedTestSentence), new ArrayList<>());
     }
 
+    @Test
+    public void testReplaceUsingRegExWorks() {
+        Preprocessor pp = new PreprocessorImpl();
+        assertEquals(pp.replaceUsingRegEx(TestPreprocessorImpl.testSentence, "jumps", "hops"), "The quick brown fox hops over the lazy dog.");
+    }
+
+    @Test
+    public void testPreprocessingWorks() {
+        Preprocessor pp = new PreprocessorImpl();
+        assertEquals(pp.preprocess(TestPreprocessorImpl.testSentence).size(), 8);
+    }
 
 }
