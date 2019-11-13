78929ddeb00c44adf82aa6dc2908c5b39ec7dfa6
Ines Hamma <ineshamma@dhcp560-065.laptop-wlc.uni-heidelberg.de>
2019-11-13 14:47:53 +0100

    CONDEC-605: Add jUnit tests

--- src/test/java/de/uhd/ifi/se/decision/management/jira/rest/viewrest/TestGetDecisionGraphAndMatrix.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/rest/viewrest/TestGetDecisionGraphAndMatrix.java
@@ -4,6 +4,8 @@ import com.atlassian.jira.mock.servlet.MockHttpServletRequest;
 import com.atlassian.jira.user.ApplicationUser;
 import com.google.common.collect.ImmutableMap;
 import de.uhd.ifi.se.decision.management.jira.TestSetUp;
+import de.uhd.ifi.se.decision.management.jira.filtering.FilterSettings;
+import de.uhd.ifi.se.decision.management.jira.filtering.impl.FilterSettingsImpl;
 import de.uhd.ifi.se.decision.management.jira.rest.ViewRest;
 import de.uhd.ifi.se.decision.management.jira.testdata.JiraUsers;
 import net.java.ao.test.jdbc.NonTransactional;
@@ -17,6 +19,7 @@ import static org.junit.Assert.assertEquals;
 
 public class TestGetDecisionGraphAndMatrix extends TestSetUp {
 	private ViewRest viewRest;
+	private FilterSettings settings;
 	protected HttpServletRequest request;
 	private static final String INVALID_PROJECTKEY = "Decision knowledge elements cannot be shown since project key is invalid.";
 
@@ -27,6 +30,7 @@ public class TestGetDecisionGraphAndMatrix extends TestSetUp {
 		ApplicationUser user = JiraUsers.BLACK_HEAD.getApplicationUser();
 		request = new MockHttpServletRequest();
 		request.setAttribute("user", user);
+		settings = new FilterSettingsImpl();
 	}
 
 	@Test
@@ -45,6 +49,24 @@ public class TestGetDecisionGraphAndMatrix extends TestSetUp {
 	@NonTransactional
 	public void testDecisionGraphProjectKeyExistent() {
 		assertEquals(200, viewRest.getDecisionGraph(request,"TEST").getStatus());
+	}
+
+	@Test
+	public void testDecisionGraphFilteredProjectKeyNull() {
+		assertEquals(Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ImmutableMap.of("error", INVALID_PROJECTKEY))
+			.build().getEntity(), viewRest.getDecisionGraphFiltered(request, settings, null).getEntity());
+	}
+
+	@Test
+	public void testDecisionGraphFilteredProjectKeyNonExistent() {
+		assertEquals(Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ImmutableMap.of("error", INVALID_PROJECTKEY))
+			.build().getEntity(), viewRest.getDecisionGraphFiltered(request, settings,"NotTEST").getEntity());
+	}
+
+	@Test
+	@NonTransactional
+	public void testDecisionGraphFilteredProjectKeyExistent() {
+		assertEquals(200, viewRest.getDecisionGraphFiltered(request, settings,"TEST").getStatus());
 	}
 
 	@Test
