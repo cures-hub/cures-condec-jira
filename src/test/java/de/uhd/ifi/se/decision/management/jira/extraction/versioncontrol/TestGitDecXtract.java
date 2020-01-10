54f611f270bea912c9d013fa71fb9e8ede12bb8c
Philipp de Sombre <philipp@desomb.re>
2020-01-10 14:18:05 +0100

    Readded import statement that was deleted in merge.

--- src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/versioncontrol/TestGitDecXtract.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/versioncontrol/TestGitDecXtract.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class TestGitDecXtract extends TestSetUpGit {
 	private GitDecXtract gitDecX;
