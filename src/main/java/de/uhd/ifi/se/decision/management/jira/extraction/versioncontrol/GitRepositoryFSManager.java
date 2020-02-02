effda6851e0d6478879bfedc0d132e6a21cbade0
Michael Anders <M.Anders@stud.uni-heidelberg.de>
2020-02-02 17:48:57 +0100

    Rewrite GitRepositoryFSManager and start rewrite on GitClient

--- src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/versioncontrol/GitRepositoryFSManager.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/versioncontrol/GitRepositoryFSManager.java
@@ -1,41 +1,59 @@
 package de.uhd.ifi.se.decision.management.jira.extraction.versioncontrol;
 
-import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.xml.bind.DatatypeConverter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.xml.bind.DatatypeConverter;
+
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GitRepositoryFSManager {
 	private static final String TEMP_DIR_PREFIX = "TEMP";
 	private static final long BRANCH_OUTDATED_AFTER = 60 * 60 * 1000; // ex. 1 day = 24 hours * 60 minutes * 60 seconds
 																		// * 1000 miliseconds
-	private String baseProjectUriPath;
-	private String baseProjectUriDefaultPath;
+	private Map<String, String> baseProjectUriPaths;
+	private Map<String, String> baseProjectUriDefaultPaths;
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(GitRepositoryFSManager.class);
 
-	public GitRepositoryFSManager(String home, String project, String repoUri, String defaultBranchName) {
+	public GitRepositoryFSManager(String home, String project, List<String> repoUris,
+			Map<String, String> defaultBranchNames) {
+		baseProjectUriPaths = new HashMap<String, String>();
+		baseProjectUriDefaultPaths = new HashMap<String, String>();
 		String baseProjectPath = home + File.separator + project;
-		baseProjectUriPath = baseProjectPath + File.separator + getShortHash(repoUri);
-		baseProjectUriDefaultPath = baseProjectUriPath + File.separator + defaultBranchName;
+		for (int i = 0; i < repoUris.size(); i++) {
+			baseProjectUriPaths.put(repoUris.get(i), baseProjectPath + File.separator + getShortHash(repoUris.get(i)));
+			if (repoUris.size() == defaultBranchNames.keySet().size()) {
+				baseProjectUriDefaultPaths.put(repoUris.get(i), baseProjectUriPaths.get(repoUris.get(i))
+						+ File.separator + defaultBranchNames.get(repoUris.get(i)));
+			} else {
+				baseProjectUriDefaultPaths.put(repoUris.get(i),
+						baseProjectUriPaths.get(repoUris.get(i)) + File.separator + "develop");
+			}
+		}
 		// clean up if possible after previous requests.
 		maintainNotUsedBranchPaths();
 	}
 
 	/**
-	 * Returns target directory path for the default branch of the repository.
+	 * Returns target directory paths for the default branch of the repositories.
 	 *
-	 * @return absolute path to directory of the default branch
+	 * @return absolute path to directories of the default branch
 	 */
-	public String getDefaultBranchPath() {
-		return baseProjectUriDefaultPath;
+	public Map<String, String> getDefaultBranchPaths() {
+		return baseProjectUriDefaultPaths;
 	}
 
 	/**
@@ -43,36 +61,42 @@ public class GitRepositoryFSManager {
 	 * contribute to improving the speed of check-outs of other branches as folder
 	 * renaming is not costly compared to copying.
 	 *
-	 * This mehod stays public as the developer might intend to release the folder
+	 * This method stays public as the developer might intend to release the folder
 	 * and not wait for maintenance strategy to trigger it.
 	 *
-	 * @param branchShortName
-	 *            branch name
+	 * @param branchShortName branch name, Repository Uri for branch
 	 * @return null on failure, absolute path to new temporary directory
 	 */
-	public String releaseBranchDirectoryNameToTemp(String branchShortName) {
-		File oldDir = new File(getCheckoutPath(branchShortName));
-		if (!oldDir.isDirectory()) {
-			return null;
-		} else {
-			Date date = new Date();
-			long time = date.getTime();
-			String tempDirString = baseProjectUriPath + File.separator + TEMP_DIR_PREFIX + time;
-			File tempDir = new File(tempDirString);
-			boolean renameResult = false;
-			try {
-				renameResult = oldDir.renameTo(tempDir);
-			} catch (Exception e) {
-				LOGGER.error("Could not rename " + oldDir + " to " + tempDirString + ". " + e.getMessage());
-				return null;
+	public String releaseBranchDirectoryNameToTemp(String branchShortName, String repoUri) {
+		if (baseProjectUriPaths.containsKey(repoUri)) {
+			String checkoutPath = getCheckoutPath(branchShortName, repoUri);
+			if (!checkoutPath.equals("")) {
+				File oldDir = new File(getCheckoutPath(branchShortName, repoUri));
+				if (!oldDir.isDirectory()) {
+					return null;
+				} else {
+					Date date = new Date();
+					long time = date.getTime();
+					String tempDirString = baseProjectUriPaths.get(repoUri) + File.separator + TEMP_DIR_PREFIX + time;
+					File tempDir = new File(tempDirString);
+					boolean renameResult = false;
+					try {
+						renameResult = oldDir.renameTo(tempDir);
+					} catch (Exception e) {
+						LOGGER.error("Could not rename " + oldDir + " to " + tempDirString + ". " + e.getMessage());
+						return null;
+					}
+					if (!renameResult) {
+						LOGGER.error(
+								"Could not rename " + oldDir + " to " + tempDirString + ". The reason is not known.");
+						return null;
+					}
+					removeBranchPathMarker(branchShortName, repoUri);
+					return tempDirString;
+				}
 			}
-			if (!renameResult) {
-				LOGGER.error("Could not rename " + oldDir + " to " + tempDirString + ". The reason is not known.");
-				return null;
-			}
-			removeBranchPathMarker(branchShortName);
-			return tempDirString;
 		}
+		return null;
 	}
 
 	/**
@@ -81,19 +105,22 @@ public class GitRepositoryFSManager {
 	 * be renamed to branch's target folder name. Bad case: branch folder is copied
 	 * in I/O heavy operation from default branch.
 	 *
-	 * @param branchShortName
-	 *            branch name
+	 * @param branchShortName branch name, Repository Uri of branch
 	 * @return null on failure, absolute path to branch's directory
 	 */
-	public String prepareBranchDirectory(String branchShortName) {
-		if (!useFromExistingBranchFolder(branchShortName) && !useFromTemporaryFolder(branchShortName)
-				&& !useFromDefaultFolder(branchShortName)) {
-			LOGGER.warn("Neither branch, nor temporary," + " nor default folder could be found under: "
-					+ baseProjectUriPath);
-			return null;
+	public String prepareBranchDirectory(String branchShortName, String repoUri) {
+		if (baseProjectUriPaths.containsKey(repoUri)) {
+			if (!useFromExistingBranchFolder(branchShortName, repoUri)
+					&& !useFromTemporaryFolder(branchShortName, repoUri)
+					&& !useFromDefaultFolder(branchShortName, repoUri)) {
+				LOGGER.warn("Neither branch, nor temporary," + " nor default folder could be found under: "
+						+ baseProjectUriPaths.get(repoUri));
+				return null;
+			}
+			rememberBranchPathRequest(branchShortName, repoUri);
+			return getCheckoutPath(branchShortName, repoUri);
 		}
-		rememberBranchPathRequest(branchShortName);
-		return getCheckoutPath(branchShortName);
+		return "";
 	}
 
 	/**
@@ -141,120 +168,158 @@ public class GitRepositoryFSManager {
 			return;
 		}
 
-		String[] notUsedBranchPaths = findOutdatedBranchPaths();
-		if (notUsedBranchPaths != null)
-			for (String branch : notUsedBranchPaths) {
-				releaseBranchDirectoryNameToTemp(branch);
-				LOGGER.info("Returned " + branch + " to temporary directory pool.");
+		Map<String, List<String>> notUsedBranchPaths = findOutdatedBranchPaths();
+		Set<String> repoUris = notUsedBranchPaths.keySet();
+		for (String repoUri : repoUris) {
+			if (notUsedBranchPaths.get(repoUri) != null) {
+				for (String branch : notUsedBranchPaths.get(repoUri)) {
+					releaseBranchDirectoryNameToTemp(branch, repoUri);
+					LOGGER.info("Returned " + branch + " to temporary directory pool.");
+				}
 			}
+		}
+
 	}
 
 	/*
 	 * Writes files for each branch folder request, the creation date of these files
 	 * can be later used for branch folder clean-ups.
 	 */
-	private void rememberBranchPathRequest(String branchShortName) {
-		// ignore the last marker
-		removeBranchPathMarker(branchShortName);
-		// add new marker
-		File file = new File(baseProjectUriPath, branchShortName);
-		file.setWritable(true);
-		try {
-			// assumes branch names are valid file names
-			FileUtils.writeStringToFile(file, getShortHash(branchShortName), StandardCharsets.UTF_8);
-		} catch (IOException ex) {
-			LOGGER.info(ex.getMessage());
+	private void rememberBranchPathRequest(String branchShortName, String repoUri) {
+		if (baseProjectUriPaths.containsKey(repoUri)) {
+			// ignore the last marker
+			removeBranchPathMarker(branchShortName, repoUri);
+			// add new marker
+			File file = new File(baseProjectUriPaths.get(repoUri), branchShortName);
+			file.setWritable(true);
+			try {
+				// assumes branch names are valid file names
+				FileUtils.writeStringToFile(file, getShortHash(branchShortName), StandardCharsets.UTF_8);
+			} catch (IOException ex) {
+				LOGGER.info(ex.getMessage());
+			}
 		}
+
 	}
 
 	/*
 	 * If the branch file marker does not exist, the maintenance shall not try to
 	 * recycle the branch folder
 	 */
-	private void removeBranchPathMarker(String branchShortName) {
-		File file = new File(baseProjectUriPath, branchShortName);
-		file.delete();
-	}
-
-	private String getCheckoutPath(String checkoutPoint) {
-		return baseProjectUriPath + File.separator + getShortHash(checkoutPoint);
-	}
-
-	private boolean useFromDefaultFolder(String branchShortName) {
-		File defaultDir = new File(baseProjectUriDefaultPath);
-		if (!defaultDir.isDirectory()) {
-			return false;
-		} else {
-			try {
-				File newDir = new File(getCheckoutPath(branchShortName));
-				FileUtils.copyDirectory(defaultDir, newDir);
-			} catch (Exception e) {
-				LOGGER.error("Could not copy " + defaultDir + " to " + getCheckoutPath(branchShortName) + ".\n\t"
-						+ e.getMessage());
-				return false;
-			}
+	private void removeBranchPathMarker(String branchShortName, String repoUri) {
+		if (baseProjectUriPaths.containsKey(repoUri)) {
+			File file = new File(baseProjectUriPaths.get(repoUri), branchShortName);
+			file.delete();
 		}
-		return true;
 	}
 
-	private boolean useFromExistingBranchFolder(String branchShortName) {
-		File dir = new File(getCheckoutPath(branchShortName));
+	private String getCheckoutPath(String checkoutPoint, String repoUri) {
+		if (baseProjectUriPaths.containsKey(repoUri)) {
+			return baseProjectUriPaths.get(repoUri) + File.separator + getShortHash(checkoutPoint);
+		}
+		return "";
+	}
+
+	private boolean useFromDefaultFolder(String branchShortName, String repoUri) {
+		if (baseProjectUriDefaultPaths.containsKey(repoUri)) {
+			File defaultDir = new File(baseProjectUriDefaultPaths.get(repoUri));
+			if (!defaultDir.isDirectory()) {
+				return false;
+			} else {
+				try {
+					File newDir = new File(getCheckoutPath(branchShortName, repoUri));
+					FileUtils.copyDirectory(defaultDir, newDir);
+				} catch (Exception e) {
+					LOGGER.error("Could not copy " + defaultDir + " to " + getCheckoutPath(branchShortName, repoUri)
+							+ ".\n\t" + e.getMessage());
+					return false;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+
+	private boolean useFromExistingBranchFolder(String branchShortName, String repoUri) {
+		File dir = new File(getCheckoutPath(branchShortName, repoUri));
 		return dir.isDirectory();
 	}
 
-	private boolean useFromTemporaryFolder(String branchShortName) {
-		String[] tempDirs = findTemporaryDirectoryNames();
-		if (tempDirs == null || tempDirs.length < 1) {
-			return false;
+	private boolean useFromTemporaryFolder(String branchShortName, String repoUri) {
+		if (baseProjectUriPaths.containsKey(repoUri)) {
+			List<String> tempDirs = findTemporaryDirectoryNames();
+			if (tempDirs == null || tempDirs.size() < 1) {
+				return false;
+			}
+			try {
+				File dir = new File(baseProjectUriPaths.get(repoUri), tempDirs.get(0)); // get the 1st of temp dirs, but
+																						// is
+				// 1st the best?
+				File newDir = new File(getCheckoutPath(branchShortName, repoUri));
+				dir.renameTo(newDir);
+			} catch (Exception e) {
+				LOGGER.error("Could not rename " + tempDirs.get(0) + " to " + getCheckoutPath(branchShortName, repoUri)
+						+ ". " + e.getMessage());
+				return false;
+			}
+			return true;
 		}
-		try {
-			File dir = new File(baseProjectUriPath, tempDirs[0]); // get the 1st of temp dirs, but is 1st the best?
-			File newDir = new File(getCheckoutPath(branchShortName));
-			dir.renameTo(newDir);
-		} catch (Exception e) {
-			LOGGER.error("Could not rename " + tempDirs[0] + " to " + getCheckoutPath(branchShortName) + ". "
-					+ e.getMessage());
-			return false;
-		}
-		return true;
+		return false;
 	}
 
-	private String[] findTemporaryDirectoryNames() {
-		File file = new File(baseProjectUriPath);
-		String[] directories = file
-				.list((current, name) -> (name.startsWith(TEMP_DIR_PREFIX) && new File(current, name).isDirectory()));
+	private List<String> findTemporaryDirectoryNames() {
+		Set<String> repoUris = baseProjectUriPaths.keySet();
+		List<String> directories = new ArrayList<String>();
+		for (String repoUri : repoUris) {
+			File file = new File(baseProjectUriPaths.get(repoUri));
+			String[] repodirectories = file.list(
+					(current, name) -> (name.startsWith(TEMP_DIR_PREFIX) && new File(current, name).isDirectory()));
+			directories.addAll(Arrays.asList(repodirectories));
+		}
 		return directories;
 	}
 
 	/*
-	 * Searches for files inside baseProjectUriPath and looks at their creation
+	 * Searches for files inside baseProjectUriPaths and looks at their creation
 	 * dates
 	 */
-	private String[] findOutdatedBranchPaths() {
+	private Map<String, List<String>> findOutdatedBranchPaths() {
 		return findBranchPathFiles(true);
 	}
 
-	private String[] findBranchPathFiles(boolean getOutdated) {
-		File file = new File(baseProjectUriPath);
-		Date date = new Date();
-		String[] branchFilteredTouchFiles = file.list((current, name) -> {
-			long fileLifespan = date.getTime() - current.lastModified();
-			boolean lifeSpanCondition = fileLifespan > BRANCH_OUTDATED_AFTER;
-			if (!getOutdated) {
-				lifeSpanCondition = !lifeSpanCondition;
-			}
-			boolean isFile = new File(current, name).isFile();
-			return isFile && lifeSpanCondition;
-		});
-		return branchFilteredTouchFiles;
+	private Map<String, List<String>> findBranchPathFiles(boolean getOutdated) {
+		Set<String> repoUris = baseProjectUriPaths.keySet();
+		Map<String, List<String>> branchMap = new HashMap<String, List<String>>();
+		for (String key : repoUris) {
+			List<String> branchFilteredTouchFilesList = new ArrayList<String>();
+			String baseProjectUriPath = baseProjectUriPaths.get(key);
+			File file = new File(baseProjectUriPath);
+			Date date = new Date();
+			String[] branchFilteredTouchFiles = file.list((current, name) -> {
+				long fileLifespan = date.getTime() - current.lastModified();
+				boolean lifeSpanCondition = fileLifespan > BRANCH_OUTDATED_AFTER;
+				if (!getOutdated) {
+					lifeSpanCondition = !lifeSpanCondition;
+				}
+				boolean isFile = new File(current, name).isFile();
+				return isFile && lifeSpanCondition;
+			});
+			branchFilteredTouchFilesList.addAll(Arrays.asList(branchFilteredTouchFiles));
+			branchMap.put(key, branchFilteredTouchFilesList);
+		}
+		return branchMap;
+
 	}
 
 	public boolean isBranchDirectoryInUse(String branchShortName) {
-		String[] inUseList = findBranchPathFiles(false);
-		if (inUseList != null) {
-			for (String touchFileName : inUseList) {
-				if (touchFileName.equals(branchShortName)) {
-					return true;
+		Map<String, List<String>> inUseList = findBranchPathFiles(false);
+		Set<String> repoUris = inUseList.keySet();
+		for (String repoUri : repoUris) {
+			if (inUseList.get(repoUri) != null) {
+				for (String touchFileName : inUseList.get(repoUri)) {
+					if (touchFileName.equals(branchShortName)) {
+						return true;
+					}
 				}
 			}
 		}
--- src/main/java/de/uhd/ifi/se/decision/management/jira/view/FeatureBranchQualityDashboardItem.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/view/FeatureBranchQualityDashboardItem.java
@@ -1,58 +1,57 @@
 package de.uhd.ifi.se.decision.management.jira.view;
 
+import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.project.Project;
 import com.atlassian.plugin.PluginParseException;
 import com.atlassian.plugin.web.ContextProvider;
 import com.google.common.collect.Maps;
+
 import de.uhd.ifi.se.decision.management.jira.persistence.ConfigPersistenceManager;
 
+public class FeatureBranchQualityDashboardItem implements ContextProvider {
 
-import java.security.SecureRandom;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+	@Override
+	public void init(final Map<String, String> params) throws PluginParseException {
+		/**
+		 * No special behaviour is foreseen for now.
+		 */
+	}
 
-public class FeatureBranchQualityDashboardItem  implements ContextProvider {
+	@Override
+	public Map<String, Object> getContextMap(final Map<String, Object> context) {
+		final Map<String, Object> newContext = Maps.newHashMap(context);
 
-    @Override
-    public void init(final Map<String, String> params) throws PluginParseException {
-        /**
-         * No special behaviour is foreseen for now.
-         */
-    }
+		Map<String, Object> projectContext = attachProjectsMaps();
+		newContext.putAll(projectContext);
 
-    @Override
-    public Map<String, Object> getContextMap(final Map<String, Object> context) {
-        final Map<String, Object> newContext = Maps.newHashMap(context);
+		SecureRandom random = new SecureRandom();
+		String uid = String.valueOf(random.nextInt(10000));
+		String selectId = "condec-dashboard-item-project-selection" + uid;
+		newContext.put("selectID", selectId);
+		newContext.put("dashboardUID", uid);
 
-        Map<String, Object> projectContext = attachProjectsMaps();
-        newContext.putAll(projectContext);
+		return newContext;
+	}
 
-        SecureRandom random = new SecureRandom();
-        String uid = String.valueOf(random.nextInt(10000));
-        String selectId = "condec-dashboard-item-project-selection"+uid;
-        newContext.put("selectID", selectId);
-        newContext.put("dashboardUID", uid);
+	private Map<String, Object> attachProjectsMaps() {
+		Map<String, Object> newContext = new HashMap<>();
+		Map<String, String> projectNameMap = new TreeMap<String, String>();
+		Map<String, String> projectGitMap = new TreeMap<String, String>();
+		for (Project project : ComponentAccessor.getProjectManager().getProjects()) {
+			String projectKey = project.getKey();
+			String projectName = project.getName();
+			String projectGitUri = ConfigPersistenceManager.getGitUri(projectKey); // TODO: Change to multiple URIs
+			projectNameMap.put(projectKey, projectName);
+			projectGitMap.put(projectKey, projectGitUri);
+		}
+		newContext.put("projectNamesMap", projectNameMap);
+		newContext.put("projectGitMap", projectGitMap);
 
-        return newContext;
-    }
-
-    private Map<String, Object> attachProjectsMaps() {
-        Map<String, Object> newContext = new HashMap<>();
-        Map<String, String> projectNameMap = new TreeMap<String, String>();
-        Map<String, String> projectGitMap = new TreeMap<String, String>();
-        for (Project project : ComponentAccessor.getProjectManager().getProjects()) {
-            String projectKey = project.getKey();
-            String projectName = project.getName();
-            String projectGitUri = ConfigPersistenceManager.getGitUri(projectKey);
-            projectNameMap.put(projectKey,projectName);
-            projectGitMap.put(projectKey,projectGitUri);
-        }
-        newContext.put("projectNamesMap", projectNameMap);
-        newContext.put("projectGitMap", projectGitMap);
-
-        return newContext;
-    }
+		return newContext;
+	}
 }
