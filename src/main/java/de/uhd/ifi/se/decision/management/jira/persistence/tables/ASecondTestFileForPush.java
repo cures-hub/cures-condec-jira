8450688e973b03b7109c430ddf6ef0411845f18b
Michael Anders <M.Anders@stud.uni-heidelberg.de>
2020-04-01 13:59:39 +0200

    Test Push For Code Class Knowledge Maintenance

---
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/persistence/tables/ASecondTestFileForPush.java
@@ -0,0 +1,10 @@
+package de.uhd.ifi.se.decision.management.jira.persistence.tables;
+
+public class ASecondTestFileForPush {
+
+	public ASecondTestFileForPush() {
+		System.out.println("run");
+		int b = 2;
+		int a = b + 2;
+	}
+}
