28e23d832b176f57544ca0f2705635797d4bf859
Michael Anders <M.Anders@stud.uni-heidelberg.de>
2020-04-01 14:08:40 +0200

    Test Push For Code Class Knowledge Maintenance

--- src/main/java/de/uhd/ifi/se/decision/management/jira/persistence/tables/ASecondTestFileForPushRenamed.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/persistence/tables/ASecondTestFileForPushRenamed.java
@@ -1,8 +1,8 @@
 package de.uhd.ifi.se.decision.management.jira.persistence.tables;
 
-public class ASecondTestFileForPush {
+public class ASecondTestFileForPushRenamed {
 
-	public ASecondTestFileForPush() {
+	public ASecondTestFileForPushRenamed() {
 		System.out.println("run");
 		int b = 2;
 		int a = b + 2;
--- src/main/java/de/uhd/ifi/se/decision/management/jira/persistence/tables/ATestFileForPush.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/persistence/tables/ATestFileForPush.java
@@ -3,8 +3,10 @@ package de.uhd.ifi.se.decision.management.jira.persistence.tables;
 public class ATestFileForPush {
 
 	public ATestFileForPush() {
+		System.out.println("I am modfiying this");
 		System.out.println("run");
 		int b = 2;
-		int a = b + 2;
+		int c = 1;
+		int a = b + 2 + c;
 	}
 }
