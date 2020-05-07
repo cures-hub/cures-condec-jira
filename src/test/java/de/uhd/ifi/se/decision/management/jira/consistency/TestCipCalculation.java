0ff76e56e2ad69cf224edb1ade073a996703f85b
desombre <philipp@desomb.re>
2020-05-07 13:12:05 +0200

    FIXED: removed unused imports and methods

--- src/test/java/de/uhd/ifi/se/decision/management/jira/consistency/TestCipCalculation.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/consistency/TestCipCalculation.java
@@ -8,8 +8,6 @@ import com.atlassian.jira.issue.link.IssueLinkType;
 import com.atlassian.jira.issue.link.IssueLinkTypeManager;
 import com.atlassian.jira.project.Project;
 import com.atlassian.jira.user.ApplicationUser;
-import com.atlassian.jira.user.util.MockUserManager;
-import com.atlassian.jira.user.util.UserManager;
 import de.uhd.ifi.se.decision.management.jira.TestSetUp;
 import de.uhd.ifi.se.decision.management.jira.testdata.JiraIssues;
 import de.uhd.ifi.se.decision.management.jira.testdata.JiraProjects;
@@ -31,49 +29,13 @@ public class TestCipCalculation extends TestSetUp {
 	@BeforeClass
 	public static void setUp() {
 		Project project = JiraProjects.getTestProject();
-
 		List<MutableIssue> testIssues = JiraIssues.createJiraIssues(project);
-
 		TestCipCalculation.baseIssue = testIssues.get(0);
-
-	}
-
-	private static void linkMockIssues(Issue issue, Issue linkedIssue, ApplicationUser linkCreator) throws CreateException {
-		ComponentAccessor
-			.getIssueLinkManager()
-			.createIssueLink(issue.getId(), linkedIssue.getId(), getRandomLinkId(), (long) 0, linkCreator);
-	}
-
-	private static Issue generateMockIssue(Project project, String description, String key, ApplicationUser assignee, ApplicationUser creator, Timestamp created) {
-		MutableIssue issue = ComponentAccessor.getIssueManager().getIssueByCurrentKey(key);
-		//issue.setKey(key);
-		issue.setProjectObject(project);
-		issue.setDescription(description);
-		issue.setCreated(created);
-		issue.setAssignee(assignee);
-		//issue.setCreatorId(creator.getKey());
-		return issue;
-	}
-
-	private static long getRandomLinkId() {
-		long issueLinkTypeId = 0;
-		IssueLinkTypeManager issueLinkTypeManager = ComponentAccessor.getComponent(IssueLinkTypeManager.class);
-		Collection<IssueLinkType> issueLinkTypes = issueLinkTypeManager.getIssueLinkTypes(false); //b = excludeSystemLinks
-		int randomIndex = new Random().nextInt(issueLinkTypes.size());
-		int counter = 0;
-		for (IssueLinkType issueLinkType : issueLinkTypes) {
-			if (counter == randomIndex) {
-				issueLinkTypeId = issueLinkType.getId();
-			}
-			counter++;
-		}
-		return issueLinkTypeId;
 	}
 
 	@Test
 	public void testCIP() {
 		ContextInformation contextInformation = new ContextInformation(TestCipCalculation.baseIssue);
-
 		try {
 			Collection<LinkSuggestion> linkSuggestions = contextInformation.getLinkSuggestions();
 			List<LinkSuggestion> sortedLinkSuggestions = linkSuggestions
@@ -81,12 +43,11 @@ public class TestCipCalculation extends TestSetUp {
 				.sorted((LinkSuggestion::compareTo))
 				.collect(Collectors.toList());
 			assertEquals("The baseIssue should be the most similar to itself.", baseIssue.getKey(),
-				sortedLinkSuggestions.get(sortedLinkSuggestions.size()-1).getTargetIssue().getKey());
+				sortedLinkSuggestions.get(sortedLinkSuggestions.size() - 1).getTargetIssue().getKey());
 		} catch (NullPointerException | GenericEntityException e) {
 			System.out.println("ERROR:");
 			e.printStackTrace();
 			assertNull(e);
 		}
-
 	}
 }
