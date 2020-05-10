3039b8df294078303148ee811e6d080bb5eb1606
SilasHauk <44610517+SilasHauk@users.noreply.github.com>
2020-05-10 16:02:52 +0200

    CONDEC-662: Create a webhook for Slack (#264)
    * Add possibility to test the webhook in setting page
    
    * Add enum for WebhookType
    
    * Determine webhook type for Slack via URL
    
    * Split WebhookContentProvider in "WebhookContentProviderForTreant" and "WebhookContentProviderForSlack", add AbstractWebhookContentProvider
    
    * Add separate method "executePostMethod" for redundant HttpClient creation
    
    * Use AbstractWebhookContentProvider instead of concrete providers where possible
    
    * Add a break to every switch statement
    
    * Update README

--- src/main/java/de/uhd/ifi/se/decision/management/jira/webhook/WebhookConnector.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/webhook/WebhookConnector.java
@@ -16,11 +16,13 @@ import com.atlassian.jira.user.ApplicationUser;
 
 import de.uhd.ifi.se.decision.management.jira.model.KnowledgeElement;
 import de.uhd.ifi.se.decision.management.jira.model.KnowledgeGraph;
+import de.uhd.ifi.se.decision.management.jira.model.KnowledgeType;
 import de.uhd.ifi.se.decision.management.jira.persistence.ConfigPersistenceManager;
 import de.uhd.ifi.se.decision.management.jira.persistence.KnowledgePersistenceManager;
 
 /**
- * Webhook class that posts changed decision knowledge to a given URL.
+ * Webhook class that posts changed decision knowledge to a given URL. The
+ * format of the posted data is determined by the {@link WebhookType}.
  */
 public class WebhookConnector {
 	private static final Logger LOGGER = LoggerFactory.getLogger(WebhookConnector.class);
@@ -29,6 +31,7 @@ public class WebhookConnector {
 	private String projectKey;
 	private List<Long> elementIds;
 	private Collection<String> rootTypes;
+	private WebhookType type;
 
 	public WebhookConnector(String projectKey, String webhookUrl, String webhookSecret, Collection<String> rootTypes) {
 		this.projectKey = projectKey;
@@ -41,6 +44,7 @@ public class WebhookConnector {
 			this.rootTypes = rootTypes;
 		}
 		this.elementIds = new ArrayList<Long>();
+		this.type = WebhookType.getTypeFromUrl(url);
 	}
 
 	public WebhookConnector(String projectKey) {
@@ -49,13 +53,31 @@ public class WebhookConnector {
 				ConfigPersistenceManager.getEnabledWebhookTypes(projectKey));
 	}
 
-	public boolean sendElementChanges(KnowledgeElement changedElement) {
+	public boolean sendElement(KnowledgeElement sendElement) {
+		return sendElement(sendElement, "new");
+	}
+
+	public boolean sendElement(KnowledgeElement sendElement, String event) {
+		if (sendElement == null || event == null) {
+			return false;
+		}
 		boolean isSubmitted = false;
-		if (!checkIfDataIsValid(changedElement)) {
+
+		if (!checkIfDataIsValid(sendElement)) {
 			return isSubmitted;
 		}
-		List<KnowledgeElement> rootElements = getWebhookRootElements(changedElement);
-		isSubmitted = postKnowledgeTrees(rootElements);
+
+		switch (type) {
+		case TREANT:
+			isSubmitted = postKnowledgeTree(sendElement);
+			break;
+		case SLACK:
+			isSubmitted = postKnowledgeElement(sendElement, event);
+			break;
+		default:
+			LOGGER.error("Webhook type is unknown.");
+			break;
+		}
 		return isSubmitted;
 	}
 
@@ -81,6 +103,31 @@ public class WebhookConnector {
 		return isDeleted;
 	}
 
+	public boolean sendTestPost() {
+		PostMethod postMethod = new PostMethod();
+
+		KnowledgeElement testElement = new KnowledgeElement(1, this.projectKey, "i");
+		testElement.setType(KnowledgeType.ISSUE);
+		testElement.setSummary("Test Summary");
+
+		AbstractWebookContentProvider provider = null;
+		if (type == WebhookType.TREANT) {
+			provider = new WebhookContentProviderForTreant(projectKey, testElement, secret, type);
+		}
+		if (type == WebhookType.SLACK) {
+			provider = new WebhookContentProviderForSlack(projectKey, testElement, type);
+
+		}
+		if (provider != null) {
+			postMethod = provider.createTestPostMethod();
+		}
+
+		return executePostMethod(postMethod);
+	}
+
+	/**
+	 * Is Used for Treant
+	 */
 	private boolean postKnowledgeTrees(List<KnowledgeElement> rootElements) {
 		for (KnowledgeElement rootElement : rootElements) {
 			if (!postKnowledgeTree(rootElement)) {
@@ -90,6 +137,9 @@ public class WebhookConnector {
 		return true;
 	}
 
+	/**
+	 * Is Used for Treant
+	 */
 	private List<KnowledgeElement> getWebhookRootElements(KnowledgeElement element) {
 		List<KnowledgeElement> webhookRootElements = new ArrayList<KnowledgeElement>();
 		KnowledgeGraph graph = KnowledgeGraph.getOrCreate(projectKey);
@@ -111,9 +161,17 @@ public class WebhookConnector {
 		return webhookRootElements;
 	}
 
+	/**
+	 * Is Used for Treant (singel element)
+	 */
 	private boolean postKnowledgeTree(KnowledgeElement rootElement) {
-		WebhookContentProvider provider = new WebhookContentProvider(projectKey, rootElement.getKey(), secret);
+		WebhookContentProviderForTreant provider = new WebhookContentProviderForTreant(projectKey, rootElement, secret,
+				type);
 		PostMethod postMethod = provider.createPostMethod();
+		return executePostMethod(postMethod);
+	}
+
+	private boolean executePostMethod(PostMethod postMethod) {
 		try {
 			HttpClient httpClient = new HttpClient();
 			postMethod.setURI(new HttpsURL(url));
@@ -128,16 +186,25 @@ public class WebhookConnector {
 		return false;
 	}
 
+	/**
+	 * Is Used for Slack
+	 */
+	private boolean postKnowledgeElement(KnowledgeElement changedElement, String event) {
+		WebhookContentProviderForSlack provider = new WebhookContentProviderForSlack(projectKey, changedElement, type);
+		PostMethod postMethod = provider.createPostMethod(event);
+		return executePostMethod(postMethod);
+	}
+
 	private boolean checkIfDataIsValid(KnowledgeElement changedElement) {
-		if (url == null || url.equals("")) {
+		if (url == null || url.isBlank()) {
 			LOGGER.error("Could not trigger webhook data because the url is missing.");
 			return false;
 		}
-		if (secret == null || secret.equals("")) {
+		if ((secret == null || secret.isBlank()) && type != WebhookType.SLACK) {
 			LOGGER.error("Could not trigger webhook data because the secret is missing.");
 			return false;
 		}
-		if (projectKey == null || projectKey.equals("")) {
+		if (projectKey == null || projectKey.isBlank()) {
 			LOGGER.error("Could not trigger webhook data because the project key is missing.");
 			return false;
 		}
@@ -148,11 +215,21 @@ public class WebhookConnector {
 		return true;
 	}
 
+	/**
+	 * @return url of the receiver as a string, e.g.
+	 *         "https://hooks.slack.com/services/...".
+	 */
 	public String getUrl() {
-		return url;
+		return this.url;
 	}
 
+	/**
+	 * @param url
+	 *            of the receiver, e.g. "https://hooks.slack.com/services/...". This
+	 *            also determines the {@link WebhookType}.
+	 */
 	public void setUrl(String url) {
 		this.url = url;
+		this.type = WebhookType.getTypeFromUrl(url);
 	}
 }
