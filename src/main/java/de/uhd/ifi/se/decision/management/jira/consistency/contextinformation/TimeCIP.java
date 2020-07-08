2785b93a7178174d8563990e24fcd5a1aea1a3bf
desombre <philipp@desomb.re>
2020-07-08 12:04:21 +0200

    Added: Decorator Pattern for CIP calculations

--- src/main/java/de/uhd/ifi/se/decision/management/jira/consistency/contextinformation/TimeCIP.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/consistency/contextinformation/TimeCIP.java
@@ -1,11 +1,20 @@
 package de.uhd.ifi.se.decision.management.jira.consistency.contextinformation;
 
 import com.atlassian.jira.issue.Issue;
-import de.uhd.ifi.se.decision.management.jira.consistency.contextinformation.ContextInformationProvider;
+import de.uhd.ifi.se.decision.management.jira.consistency.suggestions.LinkSuggestion;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class TimeCIP implements ContextInformationProvider {
 	private String id = "TimeCIP_ms";
 	private String name = "TimeCIP";
+	private Collection<LinkSuggestion> linkSuggestions;
+
+	public TimeCIP() {
+		this.linkSuggestions = new ArrayList<>();
+	}
 
 	@Override
 	public String getId() {
@@ -18,9 +27,18 @@ public class TimeCIP implements ContextInformationProvider {
 	}
 
 	@Override
-	public double assessRelation(Issue i1, Issue i2) {
-		long differenceInHours = i1.getCreated().getTime() - i2.getCreated().getTime() / (1000 * 60 * 60);
+	public Collection<LinkSuggestion> getLinkSuggestions() {
+		return this.linkSuggestions;
+	}
 
-		return 1. / Math.abs(differenceInHours + 1);
+	@Override
+	public void assessRelation(Issue baseIssue, List<Issue> issuesToTest) {
+		for (Issue issueToTest : issuesToTest) {
+			LinkSuggestion linkSuggestion = new LinkSuggestion(baseIssue, issueToTest);
+			long differenceInHours = baseIssue.getCreated().getTime() - issueToTest.getCreated().getTime() / (1000 * 60 * 60);
+			linkSuggestion.addToScore((1. / Math.abs(differenceInHours + 1)), this.getName());
+			this.linkSuggestions.add(linkSuggestion);
+
+		}
 	}
 }
