130410fa1846f0e0eea252400fc2df9b09217d74
Colin Otchere <cotchere@icloud.com>
2020-07-17 22:50:53 +0200

    added test for testDecisionTableCriteriaQuery()

--- src/test/java/de/uhd/ifi/se/decision/management/jira/rest/configrest/TestGetDecisionTableCriteriaQuery.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/rest/configrest/TestGetDecisionTableCriteriaQuery.java
@@ -8,23 +8,37 @@ import org.junit.Test;
 
 public class TestGetDecisionTableCriteriaQuery extends TestConfigSuper {
 
-	private String testQuery = "?jql=project=Test";
-	
+	private String projectKey = "TEST";
+	private String testQuery = "project=Test";
+
 	@Test
-	public void getDecisionTableCriteriaQueryWithInvalidProjectKey() {
-		assertEquals(getBadRequestResponse(INVALID_PROJECTKEY).getEntity(), 
+	public void testGetDecisionTableCriteriaQueryWithInvalidProjectKey() {
+		assertEquals(getBadRequestResponse(INVALID_PROJECTKEY).getEntity(),
 				configRest.getDesionTabelCriteriaQuery(null).getEntity());
 	}
 
 	@Test
-	public void setDecisionTableCriteriaQueryWith() {
-		assertEquals(Response.status(Response.Status.OK).build().getStatus(), 
-				configRest.setDesionTabelCriteriaQuery(request, "TEST", testQuery).getStatus());
+	public void testSetDecisionTableCriteriaQuery() {
+		assertEquals(Response.status(Response.Status.OK).build().getStatus(),
+				configRest.setDesionTabelCriteriaQuery(request, projectKey, testQuery).getStatus());
 	}
-	
+
 	@Test
-	public void getDecisionTableCriteriaQueryWithIvalidProjectKey() {
-		assertEquals(Response.status(Response.Status.OK).build().getStatus(), 
-				configRest.getDesionTabelCriteriaQuery("TEST").getStatus());
-	}	
+	public void testGetDecisionTableCriteriaQuery() {
+		assertEquals(Response.status(Response.Status.OK).build().getStatus(),
+				configRest.getDesionTabelCriteriaQuery(projectKey).getStatus());
+	}
+
+	@Test
+	public void testTestDecisionTableCriteriaQueryWithInvalidProjectKey() {
+		assertEquals(getBadRequestResponse(INVALID_PROJECTKEY).getEntity(),
+				configRest.testDecisionTableCriteriaQuery(request, null, testQuery).getEntity());
+	}
+
+	@Test
+	public void testTestDecisionTableCriteriaQuery() {
+		assertEquals(
+				"{1=[WI: Implement feature], 2=[How can we implement the feature?], 3=[We could do it like this!], 4=[We will do it like this!], 5=[This is a great solution.], 6=[NFR: Usabililty], 12=[How can we implement the new get function?], 14=[WI: Yet another work item], 30=[WI: Do an interesting task]}",
+				configRest.testDecisionTableCriteriaQuery(request, projectKey, testQuery).getEntity().toString());
+	}
 }
