27d7bd4a9938637318229ad331662ae0d8cfbdac
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2020-09-12 21:46:53 +0200

    Refactor TreeViewer tests

--- src/test/java/de/uhd/ifi/se/decision/management/jira/view/treeviewer/TestTreeViewer.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/view/treeviewer/TestTreeViewer.java
@@ -1,40 +1,26 @@
 package de.uhd.ifi.se.decision.management.jira.view.treeviewer;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import com.atlassian.jira.component.ComponentAccessor;
-import com.atlassian.jira.issue.MutableIssue;
-import com.atlassian.jira.issue.comments.Comment;
-import com.atlassian.jira.issue.comments.CommentManager;
-import com.atlassian.jira.user.ApplicationUser;
 import com.google.common.collect.ImmutableMap;
 
 import de.uhd.ifi.se.decision.management.jira.TestSetUp;
 import de.uhd.ifi.se.decision.management.jira.filtering.FilterSettings;
 import de.uhd.ifi.se.decision.management.jira.model.KnowledgeElement;
-import de.uhd.ifi.se.decision.management.jira.model.KnowledgeType;
 import de.uhd.ifi.se.decision.management.jira.model.text.PartOfJiraIssueText;
-import de.uhd.ifi.se.decision.management.jira.persistence.KnowledgePersistenceManager;
-import de.uhd.ifi.se.decision.management.jira.persistence.singlelocations.AbstractPersistenceManagerForSingleLocation;
-import de.uhd.ifi.se.decision.management.jira.persistence.singlelocations.CodeClassPersistenceManager;
-import de.uhd.ifi.se.decision.management.jira.persistence.singlelocations.JiraIssueTextPersistenceManager;
 import de.uhd.ifi.se.decision.management.jira.testdata.JiraIssues;
-import de.uhd.ifi.se.decision.management.jira.testdata.JiraUsers;
 import de.uhd.ifi.se.decision.management.jira.testdata.KnowledgeElements;
 import net.java.ao.test.jdbc.NonTransactional;
 
 public class TestTreeViewer extends TestSetUp {
-	private AbstractPersistenceManagerForSingleLocation persistenceManager;
 
 	private TreeViewer treeViewer;
 	private FilterSettings filterSettings;
@@ -44,7 +30,6 @@ public class TestTreeViewer extends TestSetUp {
 		init();
 		filterSettings = new FilterSettings("TEST", "");
 		treeViewer = new TreeViewer(filterSettings);
-		persistenceManager = KnowledgePersistenceManager.getOrCreate("TEST").getJiraIssueManager();
 	}
 
 	@Test
@@ -111,8 +96,8 @@ public class TestTreeViewer extends TestSetUp {
 				.getSentencesForCommentText("{alternative} This would be a great solution option! {alternative}");
 		PartOfJiraIssueText sentence = comment.get(0);
 		filterSettings.setSelectedElement(sentence);
-		TreeViewer tree = new TreeViewer(filterSettings);
-		assertNotNull(tree.getTreeViewerNodeWithChildren(sentence));
+		treeViewer = new TreeViewer(filterSettings);
+		assertEquals(1, treeViewer.getNodes().size());
 	}
 
 	@Test
@@ -122,63 +107,14 @@ public class TestTreeViewer extends TestSetUp {
 		filterSettings.setSelectedElement(rootElement);
 		treeViewer = new TreeViewer(filterSettings);
 		assertEquals(5, treeViewer.getTreeViewerNodeWithChildren(rootElement).getChildren().size());
-
-		// 2) Add comment to issue
-		MutableIssue issue = ComponentAccessor.getIssueManager().getIssueByCurrentKey("TEST-14");
-		ComponentAccessor.getCommentManager().deleteCommentsForIssue(issue);
-		ApplicationUser currentUser = JiraUsers.SYS_ADMIN.getApplicationUser();
-		CommentManager commentManager = ComponentAccessor.getCommentManager();
-		Comment comment1 = commentManager.create(issue, currentUser, "This is a testsentence for test purposes", true);
-
-		// 3) Manipulate Sentence object so it will be shown in the tree viewer
-		List<PartOfJiraIssueText> sentences = JiraIssueTextPersistenceManager.insertPartsOfComment(comment1);
-		// JiraIssueComment comment = new JiraIssueCommentImpl(comment1);
-		sentences.get(0).setRelevant(true);
-		sentences.get(0).setType(KnowledgeType.ALTERNATIVE);
-		rootElement = persistenceManager.getKnowledgeElement(14);
-		filterSettings.setSelectedElement(rootElement);
-		treeViewer = new TreeViewer(filterSettings);
-
-		// 4) Check if TreeViewer has one element
-		assertNotNull(treeViewer);
-		assertEquals(1, treeViewer.getNodes().size());
-
-		ComponentAccessor.getCommentManager().deleteCommentsForIssue(issue);
 	}
 
 	@Test
 	@NonTransactional
-	public void testTreeViewerCalledFromTabpanelEmptyData() {
+	public void testSelectedElementInvalid() {
 		filterSettings.setSelectedElement("");
-		TreeViewer tv = new TreeViewer(filterSettings);
-		assertNotNull(tv);
-	}
-
-	@Test
-	@NonTransactional
-	public void testSecondConstructorWithProjectKeyValid() {
-		KnowledgeElement classElement;
-		CodeClassPersistenceManager ccManager = new CodeClassPersistenceManager("Test");
-		classElement = new KnowledgeElement();
-		classElement.setProject("TEST");
-		classElement.setType("Other");
-		classElement.setDescription("TEST-1;");
-		classElement.setSummary("TestClass.java");
-		ApplicationUser user = JiraUsers.SYS_ADMIN.getApplicationUser();
-		classElement = ccManager.insertKnowledgeElement(classElement, user);
-
-		filterSettings.setSelectedElement((KnowledgeElement) null);
-		assertNull(filterSettings.getSelectedElement());
-
-		Set<String> types = new HashSet<>();
-		types.add("codeClass");
-		filterSettings.setKnowledgeTypes(types);
-		assertEquals(1, filterSettings.getKnowledgeTypes().size());
-		assertEquals("codeClass", filterSettings.getKnowledgeTypes().iterator().next());
-		filterSettings.setLinkDistance(0);
-		TreeViewer newTreeViewer = new TreeViewer(filterSettings);
-		assertNotNull(newTreeViewer);
-		assertNotNull(newTreeViewer.getNodes());
+		treeViewer = new TreeViewer(filterSettings);
+		assertEquals(9, treeViewer.getNodes().size());
 	}
 
 }
