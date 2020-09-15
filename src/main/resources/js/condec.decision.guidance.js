2c93df6f85f9b46e09dfff0fdf3c2f2629f33b1d
Paul Zubrod <paul.zubrod@freenet.de>
2020-09-15 13:05:17 +0200

    CONDEC-763: The recommendation table is now sortable, but it's ugly and still buggy

--- src/main/resources/js/condec.decision.guidance.js
+++ src/main/resources/js/condec.decision.guidance.js
@@ -18,50 +18,57 @@
 		$("#recommendation-button").click(function(event) {
 			event.preventDefault();
 			$(this).prop("disabled",true);
-			const table = $("#recommendation-container tbody");
-			$("#recommendation-container tbody tr").remove()
+			$("#recommendation-container tbody tr").remove() //TODO the rows are kept in the cache, but they should be removed completly
 			const keyword = $("#recommendation-keyword");
 			const spinner = $("#loading-spinner-recommendation");
 
 			spinner.show();
-
 			conDecAPI.getRecommendation(conDecAPI.getProjectKey(), keyword.val(), function(results, error) {
-				let counter = 0;
+
 				if (error === null) {
-
-					var sortedByScore = results.slice(0);
-                    sortedByScore.sort(function(a,b) {
-                        return b.score - a.score;
-                    });
-
-					sortedByScore.forEach((recommendation) => {
-							const alternative = recommendation.recommendations;
-							let url = "";
-							let tableRow = "";
-
-							if (alternative.url) { url = alternative.url }
-							counter += 1;
-							tableRow += "<tr>";
-							tableRow += "<td><a class='alternative-summary' href='" + url + "'>" + alternative.summary + "</a></td>";
-							tableRow += "<td>" + recommendation.knowledgeSourceName + "</td>";
-							tableRow += "<td>"+ recommendation.score +"</td>";
-							tableRow += "<td><button id='row_" + counter + "' class='aui-button-primary aui-button accept-solution-button'>Accept</button></td>";
-							tableRow += "</tr>";
-							table.append(tableRow);
-
-							$(" #row_" + counter).click(function() {
-								const currentIssue = conDecDecisionTable.getCurrentIssue();
-                            	conDecDialog.showCreateDialog(currentIssue.id, currentIssue.documentationLocation, "Alternative",  alternative.summary, alternative.description);
-                            });
-					});
-					conDecAPI.showFlag("success", "Results: " +  counter);
+					buildRecommendationTable(results);
 				}
 				$("#recommendation-button").prop("disabled",false);
 				spinner.hide();
+
 			});
 		});
 
 	};
+
+	function buildRecommendationTable(results) {
+		const table = $("#recommendation-container tbody");
+
+		let counter = 0;
+		var sortedByScore = results.slice(0);
+		sortedByScore.sort(function(a,b) {
+			return b.score - a.score;
+		});
+
+		sortedByScore.forEach((recommendation) => {
+				const alternative = recommendation.recommendations;
+				let url = "";
+				let tableRow = "";
+
+				if (alternative.url) { url = alternative.url }
+				counter += 1;
+				tableRow += "<tr>";
+				tableRow += "<td><a class='alternative-summary' href='" + url + "'>" + alternative.summary + "</a></td>";
+				tableRow += "<td>" + recommendation.knowledgeSourceName + "</td>";
+				tableRow += "<td>"+ recommendation.score +"%</td>";
+				tableRow += "<td><button id='row_" + counter + "' class='aui-button-primary aui-button accept-solution-button'>Accept</button></td>";
+				tableRow += "</tr>";
+				table.append(tableRow);
+
+				$(" #row_" + counter).click(function() {
+					const currentIssue = conDecDecisionTable.getCurrentIssue();
+					conDecDialog.showCreateDialog(currentIssue.id, currentIssue.documentationLocation, "Alternative",  alternative.summary, alternative.description);
+				});
+		});
+		conDecAPI.showFlag("success", "Results: " +  counter);
+		//Since the data is added later, the table must be set to sortable afterwards
+		AJS.tablessortable.setTableSortable(AJS.$("#recommendation-container"));
+	}
 
 
 	
