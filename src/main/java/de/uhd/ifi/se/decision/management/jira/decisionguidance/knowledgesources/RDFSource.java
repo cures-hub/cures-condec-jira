bcaf10dc4e11986da7fb438f89e1e6c73bbafc88
Paul Zubrod <paul.zubrod@freenet.de>
2020-09-23 14:04:21 +0200

    CONDEC-777: Fix settings page. RDF Knowledge Sources can now be edited again.

--- src/main/java/de/uhd/ifi/se/decision/management/jira/decisionguidance/knowledgesources/RDFSource.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/decisionguidance/knowledgesources/RDFSource.java
@@ -197,6 +197,11 @@ public class RDFSource implements KnowledgeSource {
 	}
 
 	@Override
+	public String toString() {
+		return this.name.replace(' ', '-');
+	}
+
+	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
 		if (o == null || getClass() != o.getClass()) return false;
