40d78dcf3ba0d94d1552f9c85ceb573ff1138c2d
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2020-09-25 20:54:39 +0200

    CONDEC-782: Enable to select whether vis graph is shown hierarchical or not (#336)
    * Add new filter criteria "show graph with hierarchy" as velocity template (isHierarchicalGraph.vm)
    
    * Read isHierachical filter criteria in conDecFiltering
    
    * Show random layout without hierarchy in knowledge graph as default, also in comparison view!
    [pro] The random layout loads faster than the hierarchical layout, i.e. time efficiency is better. [/pro]

--- src/main/resources/js/condec.comparison.js
+++ src/main/resources/js/condec.comparison.js
@@ -8,81 +8,67 @@
     };
 
     ConDecComparison.prototype.initView = function () {
-        console.log("ConDecComparison initView");    
-            
+        console.log("ConDecComparison initView");
+
         // Fill HTML elements for filter criteria            
-        conDecFiltering.fillFilterElements("comparison"); 
+        conDecFiltering.fillFilterElements("comparison");
         conDecFiltering.fillDatePickers("comparison", 30); // left side
         conDecFiltering.fillDatePickers("comparison-right", 7);
-        
+        addOnClickEventToFilterButton();
+
         // Register/subscribe this view as an observer
-        conDecObservable.subscribe(this);
-        
+        conDecObservable.subscribe(this);       
+
         // Fill view
-        this.buildCompare();
+        this.updateView();
     };
-    
+
     ConDecComparison.prototype.updateView = function () {
-    	document.getElementById("filter-button-comparison").click();
-	};
-
-    ConDecComparison.prototype.buildCompare = function buildCompare() {        
-        var containerLeft = document.getElementById('left-network');
-        networkLeft = new vis.Network(containerLeft, new vis.DataSet(), getOptions());
-        networkLeft.setSize("100%", "500px");
-        networkLeft.on("oncontext", function (params) {
-            conDecVis.addContextMenu(params, networkLeft);
-        });
-        networkLeft.on("selectNode", function (params) {
-            networkRight.focus(params.nodes[0]);
-            networkLeft.focus(params.nodes[0]);
-        });
-        var containerRight = document.getElementById('right-network');
-        networkRight = new vis.Network(containerRight, new vis.DataSet(), getOptions());
-        networkRight.setSize("100%", "500px");
-        networkRight.on("oncontext", function (params) {
-            conDecVis.addContextMenu(params, networkRight);
-        });
-
-        networkRight.on("selectNode", function (params) {
-            networkRight.focus(params.nodes[0]);
-            networkLeft.focus(params.nodes[0]);
-        });
-        
-        conDecFiltering.addOnClickEventToFilterButton("comparison", function(filterSettings) {
-        	// left side
-            var filterSettingsLeft = conDecFiltering.getFilterSettings("comparison");            
-            filterSettingsLeft ["isHierarchical"] = true;
-            conDecAPI.getVis(filterSettingsLeft, function (visDataLeft) {
-            	visDataLeft.nodes.sort(sortVis);
-                var dateLeft = {
-                    nodes: visDataLeft.nodes,
-                    edges: visDataLeft.edges
-                };
-                networkLeft.setData(dateLeft);
-                networkLeft.setOptions(getOptions(visDataLeft));
-            });
-            
-            // right side
-            var filterSettingsRight = filterSettingsLeft;
-            var startDatePicker = document.getElementById("start-date-picker-comparison-right");    		
-    		filterSettingsRight["startDate"] = new Date(startDatePicker.value).getTime();  			
-    		var endDatePicker = document.getElementById("end-date-picker-comparison-right");
-    		filterSettingsRight["endDate"] = new Date(endDatePicker.value).getTime();
-            conDecAPI.getVis(filterSettingsRight, function (visDataRight) {
-                visDataRight.nodes.sort(sortVis);
-                var dateRight = {
-                    nodes: visDataRight.nodes,
-                    edges: visDataRight.edges
-                };
-                networkRight.setData(dateRight);
-                networkRight.setOptions(getOptions(visDataRight));
-            });
-        });
-        
         document.getElementById("filter-button-comparison").click();
     };
-    
+
+    function addOnClickEventToFilterButton () {
+        conDecFiltering.addOnClickEventToFilterButton("comparison", function (filterSettings) {
+            // left side
+            conDecAPI.getVis(filterSettings, function (visDataLeft) {
+                visDataLeft.nodes.sort(sortVis);
+                var containerLeft = document.getElementById('left-network');
+                var optionsLeft = conDecVis.getOptions(visDataLeft, filterSettings["isHierarchical"]);
+                networkLeft = new vis.Network(containerLeft, visDataLeft, optionsLeft);
+                networkLeft.setSize("100%", "500px");
+                networkLeft.on("oncontext", function (params) {
+                    conDecVis.addContextMenu(params, networkLeft);
+                });
+                networkLeft.on("selectNode", function (params) {
+                    networkRight.focus(params.nodes[0]);
+                    networkLeft.focus(params.nodes[0]);
+                });
+            });
+
+            // right side
+            var filterSettingsRight = filterSettings;
+            var startDatePicker = document.getElementById("start-date-picker-comparison-right");
+            filterSettingsRight["startDate"] = new Date(startDatePicker.value).getTime();
+            var endDatePicker = document.getElementById("end-date-picker-comparison-right");
+            filterSettingsRight["endDate"] = new Date(endDatePicker.value).getTime();
+            conDecAPI.getVis(filterSettingsRight, function (visDataRight) {
+                visDataRight.nodes.sort(sortVis);
+                var containerRight = document.getElementById('right-network');
+                var optionsRight = conDecVis.getOptions(visDataRight, filterSettings["isHierarchical"]);
+                networkRight = new vis.Network(containerRight, visDataRight, optionsRight);
+                networkRight.setSize("100%", "500px");
+                networkRight.on("oncontext", function (params) {
+                    conDecVis.addContextMenu(params, networkRight);
+                });
+
+                networkRight.on("selectNode", function (params) {
+                    networkRight.focus(params.nodes[0]);
+                    networkLeft.focus(params.nodes[0]);
+                });
+            });
+        });
+    }
+
     function sortVis(a, b) {
         if (a.id > b.id) {
             return 1;
@@ -91,17 +77,6 @@
             return -1;
         }
         return 0;
-    }
-
-    function getOptions(visData) {
-    	var options = conDecVis.getOptions(visData);
-    	options["layout"] = {
-                randomSeed: 1,
-                hierarchical: {
-                    direction: "UD"
-                }
-    		};
-        return options;
     }
 
     global.conDecComparison = new ConDecComparison();
--- src/main/resources/js/condec.vis.js
+++ src/main/resources/js/condec.vis.js
@@ -1,31 +1,31 @@
-(function(global) {
+(function (global) {
 
-	var ConDecVis = function() {
+	var ConDecVis = function () {
 	};
-	
+
 	ConDecVis.prototype.initView = function (isJiraIssueView = false) {
-		console.log("ConDecVis initView");       
-		
+		console.log("ConDecVis initView");
+
 		// Fill HTML elements for filter criteria and add on click listener
 		if (isJiraIssueView) {
 			conDecFiltering.fillFilterElements("graph");
-			conDecFiltering.addOnClickEventToFilterButton("graph", function(filterSettings) {
+			conDecFiltering.addOnClickEventToFilterButton("graph", function (filterSettings) {
 				var issueKey = conDecAPI.getIssueKey();
 				filterSettings["selectedElement"] = issueKey;
 				conDecVis.buildVis(filterSettings, "vis-graph-container");
 			});
 		} else {
 			conDecFiltering.fillFilterElements("graph", ["Decision"]);
-			conDecFiltering.addOnClickEventToFilterButton("graph", function(filterSettings) {
+			conDecFiltering.addOnClickEventToFilterButton("graph", function (filterSettings) {
 				conDecVis.buildVis(filterSettings, "vis-graph-container");
 			});
 			document.getElementById("link-distance-input-label-graph").remove();
 			document.getElementById("link-distance-input-graph").remove();
-		}	
+		}
 
 		// Register/subscribe this view as an observer
 		conDecObservable.subscribe(this);
-	        
+
 		// Fill view
 		this.updateView();
 	};
@@ -37,74 +37,87 @@
 	/*
 	 * external references: condec.knowledge.page.js and condec.rationale.backlog.js
 	 */
-	ConDecVis.prototype.buildVis = function(filterSettings, container) {
+	ConDecVis.prototype.buildVis = function (filterSettings, container) {
 		console.log("conDecVis buildVis");
-		conDecAPI.getVis(filterSettings, function(visData) {
-			conDecVis.buildGraphNetwork(visData, container);
+		conDecAPI.getVis(filterSettings, function (visData) {
+			options = conDecVis.getOptions(visData, filterSettings["isHierarchical"]);
+
+			var graphContainer = document.getElementById(container);
+			var graphNetwork = new vis.Network(graphContainer, visData, options);
+			graphNetwork.setSize("100%", "500px");
+
+			graphNetwork.on("oncontext", function (params) {
+				conDecVis.addContextMenu(params, graphNetwork);
+			});
+			if (visData.selectedVisNodeId !== undefined && visData.selectedVisNodeId !== "") {
+				graphNetwork.selectNodes([visData.selectedVisNodeId]);
+			}
 		});
 	};
 
-	ConDecVis.prototype.buildGraphNetwork = function(data, container) {
-		var options = conDecVis.getOptions(data);
-
-		var graphContainer = document.getElementById(container);
-		var graphNetwork = new vis.Network(graphContainer, data, options);
-		graphNetwork.setSize("100%", "500px");
-
-		graphNetwork.on("oncontext", function(params) {
-			conDecVis.addContextMenu(params, graphNetwork);
-		});
-		if (data.selectedVisNodeId !== undefined && data.selectedVisNodeId !== "") {
-			graphNetwork.selectNodes([ data.selectedVisNodeId ]);
-		}
-	};
-	
-	ConDecVis.prototype.getOptions = function (visData) {		
+	ConDecVis.prototype.getOptions = function (visData, isHierarchical) {
 		var options = {
-		    edges : {
-		        arrows : "to",
-		        length : 200
-		    },
-		    layout : {
-			    randomSeed : 1,
-			    improvedLayout : false
-		    },
-		    manipulation : {
-		        enabled : true,
-		        addNode : function(newNode, callback) {
-		        	conDecVis.addNode(newNode);
-		        },
-		        deleteNode : function(selectedNode, callback) {
-			        conDecVis.deleteNode(selectedNode, callback);
-		        },
-		        addEdge : function(selectedNodes, callback) {
-			        conDecVis.addEdge(selectedNodes);
-		        },
-		        deleteEdge : function(selectedEdge, callback) {
-			        conDecVis.deleteEdge(selectedEdge, visData, callback);
-		        },
-		        editNode : function(selectedNode, callback) {
-			        conDecVis.editNode(selectedNode, callback);
-		        },
-		        editEdge : function(selectedEdge, callback) {
-			        conDecVis.editEdge(selectedEdge, visData);
-		        }
-		    },
-		    physics : {
-		        enabled : true,
-		        stabilization : true // if false, there is a lot of movement
-										// at the beginning
-		    },
-		    nodes : {
-		    	  shapeProperties: {
-		    	    interpolation: false    // 'true' for intensive zooming
-		    	}
-		    }
+			edges: {
+				arrows: "to",
+				length: 200
+			},
+			manipulation: {
+				enabled: true,
+				addNode: function (newNode, callback) {
+					conDecVis.addNode(newNode);
+				},
+				deleteNode: function (selectedNode, callback) {
+					conDecVis.deleteNode(selectedNode, callback);
+				},
+				addEdge: function (selectedNodes, callback) {
+					conDecVis.addEdge(selectedNodes);
+				},
+				deleteEdge: function (selectedEdge, callback) {
+					conDecVis.deleteEdge(selectedEdge, visData, callback);
+				},
+				editNode: function (selectedNode, callback) {
+					conDecVis.editNode(selectedNode, callback);
+				},
+				editEdge: function (selectedEdge, callback) {
+					conDecVis.editEdge(selectedEdge, visData);
+				}
+			},
+			physics: {
+				enabled: true,
+				stabilization: true // if false, there is a lot of movement
+				// at the beginning
+			},
+			nodes: {
+				shapeProperties: {
+					interpolation: false    // 'true' for intensive zooming
+				}
+			}
 		};
+		if (isHierarchical) {
+			options["layout"] = getHierarchicalLayout();
+		} else {
+			options["layout"] = getRandomLayout();
+		}
 		return options;
 	};
 
-	ConDecVis.prototype.addContextMenu = function(params, network) {
+	function getHierarchicalLayout() {
+		return {
+			randomSeed: 1,
+			hierarchical: {
+				direction: "UD"
+			}
+		}
+	}
+
+	function getRandomLayout() {
+		return {
+			randomSeed: 1,
+			improvedLayout: false
+		}
+	}
+
+	ConDecVis.prototype.addContextMenu = function (params, network) {
 		params.event.preventDefault();
 		var nodeIndices = network.body.nodeIndices;
 		var clickedNodeId;
@@ -112,7 +125,7 @@
 			var nodeId = nodeIndices[i];
 			var boundingBox = network.getBoundingBox(nodeId);
 			if (boundingBox.left <= params.pointer.canvas.x && params.pointer.canvas.x <= boundingBox.right
-			        && boundingBox.top <= params.pointer.canvas.y && params.pointer.canvas.y <= boundingBox.bottom) {
+				&& boundingBox.top <= params.pointer.canvas.y && params.pointer.canvas.y <= boundingBox.bottom) {
 				clickedNodeId = nodeId;
 			}
 		}
@@ -123,7 +136,7 @@
 		const documentationLocation = getDocumentationLocation(clickedNodeId);
 		conDecContextMenu.createContextMenu(elementId, documentationLocation, params.event, "vis-container");
 	};
-	
+
 	ConDecVis.prototype.addNode = function (newNode) {
 		conDecDialog.showCreateDialog(-1, null, "Decision");
 	};
@@ -131,7 +144,7 @@
 	ConDecVis.prototype.deleteNode = function (selectedNodes, callback) {
 		conDecDialog.showDeleteDialog(getElementId(selectedNodes.nodes[0]), getDocumentationLocation(selectedNodes.nodes[0]), callback);
 	};
-	
+
 	ConDecVis.prototype.editNode = function (selectedNode, callback) {
 		conDecDialog.showEditDialog(selectedNode.elementId, selectedNode.documentationLocation, callback);
 	};
@@ -140,8 +153,8 @@
 		if (newEdge.from === newEdge.to) {
 			return;
 		}
-		conDecDialog.showLinkDialog(getElementId(newEdge.from), getDocumentationLocation(newEdge.from), 
-				getElementId(newEdge.to), getDocumentationLocation(newEdge.to));
+		conDecDialog.showLinkDialog(getElementId(newEdge.from), getDocumentationLocation(newEdge.from),
+			getElementId(newEdge.to), getDocumentationLocation(newEdge.to));
 	};
 
 	ConDecVis.prototype.deleteEdge = function (selectedEdges, visData, callback) {
@@ -149,33 +162,33 @@
 		var edgeToBeDeleted = allEdges.get(selectedEdges.edges[0]);
 		showDeleteLinkDialogForVisEdge(edgeToBeDeleted, callback);
 	};
-	
-	ConDecVis.prototype.editEdge = function (newEdge, visData) {		
+
+	ConDecVis.prototype.editEdge = function (newEdge, visData) {
 		var allEdges = new vis.DataSet(visData.edges);
 		var edgeToBeDeleted = allEdges.get(newEdge.id);
 		if (edgeToBeDeleted.from !== newEdge.from || edgeToBeDeleted.to !== newEdge.to) {
 			showDeleteLinkDialogForVisEdge(edgeToBeDeleted);
-		}		
-		conDecDialog.showLinkDialog(getElementId(newEdge.from), getDocumentationLocation(newEdge.from), 
-				getElementId(newEdge.to), getDocumentationLocation(newEdge.to), newEdge.label);
+		}
+		conDecDialog.showLinkDialog(getElementId(newEdge.from), getDocumentationLocation(newEdge.from),
+			getElementId(newEdge.to), getDocumentationLocation(newEdge.to), newEdge.label);
 	};
-	
+
 	function showDeleteLinkDialogForVisEdge(edge, callback) {
 		var idOfChild = getElementId(edge.to);
 		var idOfParent = getElementId(edge.from);
 		var documentationLocationOfChild = getDocumentationLocation(edge.to);
 		var documentationLocationOfParent = getDocumentationLocation(edge.from);
 		conDecDialog.showDeleteLinkDialog(idOfChild, documentationLocationOfChild, idOfParent,
-		        documentationLocationOfParent, callback);	
+			documentationLocationOfParent, callback);
 	}
 
 	function getDocumentationLocation(node) {
 		return node.substr(-1);
 	}
-	
+
 	function getElementId(node) {
 		return node.slice(0, -2);
 	}
-	
+
 	global.conDecVis = new ConDecVis();
 })(window);
