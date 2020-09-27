8eaf278934cba7c9c0f59d99cb452401e9e64f91
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2020-09-27 14:31:08 +0200

    Enable to update the status of Jira issues

--- src/main/java/de/uhd/ifi/se/decision/management/jira/config/workflows/WorkflowXMLDescriptorProvider.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/config/workflows/WorkflowXMLDescriptorProvider.java
@@ -3,7 +3,8 @@ package de.uhd.ifi.se.decision.management.jira.config.workflows;
 import com.atlassian.jira.issue.issuetype.IssueType;
 
 /**
- * @issue How can we create workflows programmatically?
+ * @issue How can we create workflows (i.e. status of knowledge elements and
+ *        their transitions) programmatically?
  * @issue How can we read XML files from the resources folder?
  */
 public class WorkflowXMLDescriptorProvider {
