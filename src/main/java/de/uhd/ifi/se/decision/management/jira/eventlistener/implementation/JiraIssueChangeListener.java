53d17411706d3db1368b3ef87ab3f92ca27ae065
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2020-09-27 16:12:52 +0200

    Update knowledge graph singleton object about changes on Jira issues and Jira issue links

---
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/eventlistener/implementation/JiraIssueChangeListener.java
@@ -0,0 +1,20 @@
+package de.uhd.ifi.se.decision.management.jira.eventlistener.implementation;
+
+import com.atlassian.jira.event.issue.IssueEvent;
+import com.atlassian.jira.issue.Issue;
+
+import de.uhd.ifi.se.decision.management.jira.eventlistener.IssueEventListener;
+import de.uhd.ifi.se.decision.management.jira.model.KnowledgeElement;
+import de.uhd.ifi.se.decision.management.jira.model.KnowledgeGraph;
+
+public class JiraIssueChangeListener implements IssueEventListener {
+
+	@Override
+	public void onIssueEvent(IssueEvent issueEvent) {
+		String projectKey = issueEvent.getProject().getKey();
+		Issue jiraIssue = issueEvent.getIssue();
+		KnowledgeElement element = new KnowledgeElement(jiraIssue);
+		KnowledgeGraph.getOrCreate(projectKey).updateElement(element);
+	}
+
+}
