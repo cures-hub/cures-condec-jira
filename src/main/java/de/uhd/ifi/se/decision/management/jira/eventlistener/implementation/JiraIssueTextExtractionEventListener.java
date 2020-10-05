53954f3655cf617e02eb419a642ca3ba9c36c821
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2020-10-05 20:56:54 +0200

    Remove getChangedString method in JiraIssueTextEventListener to determine the diff in the description after a change because it is currently unused

--- src/main/java/de/uhd/ifi/se/decision/management/jira/eventlistener/implementation/JiraIssueTextExtractionEventListener.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/eventlistener/implementation/JiraIssueTextExtractionEventListener.java
@@ -1,11 +1,5 @@
 package de.uhd.ifi.se.decision.management.jira.eventlistener.implementation;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.ofbiz.core.entity.GenericEntityException;
-import org.ofbiz.core.entity.GenericValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,7 +8,6 @@ import com.atlassian.jira.event.issue.IssueEvent;
 import com.atlassian.jira.event.type.EventType;
 import com.atlassian.jira.issue.MutableIssue;
 import com.atlassian.jira.issue.comments.MutableComment;
-import com.atlassian.jira.util.collect.MapBuilder;
 
 import de.uhd.ifi.se.decision.management.jira.classification.implementation.ClassificationManagerForJiraIssueComments;
 import de.uhd.ifi.se.decision.management.jira.eventlistener.IssueEventListener;
@@ -56,8 +49,9 @@ public class JiraIssueTextExtractionEventListener implements IssueEventListener 
 	 * @issue How to implement the handling of the concrete event?
 	 * @decision Use if-statements to handle the concrete event!
 	 * @alternative Use switch-case-statement to handle the concrete event!
-	 * @con Switch-case-statement is not trivial to implement, because the switch
-	 *      cannot be a long variable. Casting from long to int is dangerous.
+	 * @con Switch-case-statement is not trivial to implement. The event type is a
+	 *      long variable but the switch cannot be a long variable. Casting from
+	 *      long to int is dangerous.
 	 */
 	@Override
 	public void onIssueEvent(IssueEvent issueEvent) {
@@ -69,7 +63,6 @@ public class JiraIssueTextExtractionEventListener implements IssueEventListener 
 		}
 
 		long eventTypeId = issueEvent.getEventTypeId();
-
 		if (eventTypeId == EventType.ISSUE_COMMENT_DELETED_ID) {
 			handleDeleteComment();
 		}
@@ -80,7 +73,7 @@ public class JiraIssueTextExtractionEventListener implements IssueEventListener 
 			handleDeleteIssue();
 		}
 		if (eventTypeId == EventType.ISSUE_UPDATED_ID || eventTypeId == EventType.ISSUE_CREATED_ID) {
-			handleUpdateDescription();
+			handleNewJiraIssueOrUpdatedDescription();
 		}
 	}
 
@@ -141,7 +134,7 @@ public class JiraIssueTextExtractionEventListener implements IssueEventListener 
 		persistenceManager.createLinksForNonLinkedElements(issueEvent.getIssue());
 	}
 
-	private void handleUpdateDescription() {
+	private void handleNewJiraIssueOrUpdatedDescription() {
 		if (JiraIssueTextExtractionEventListener.editCommentLock) {
 			// If locked, a REST service is currently manipulating a comment or the
 			// description and this event should not be handled by this event listener.
@@ -162,29 +155,5 @@ public class JiraIssueTextExtractionEventListener implements IssueEventListener 
 			persistenceManager.updateDescription(issueEvent.getIssue());
 		}
 		persistenceManager.createLinksForNonLinkedElements(issueEvent.getIssue());
-	}
-
-	public Map<String, String> getChangedString(IssueEvent issueEvent) {
-		Map<String, String> changedString = new HashMap<String, String>();
-
-		GenericValue changeLog = issueEvent.getChangeLog();
-		Long changeId = changeLog.getLong("id");
-		if (changeId == null) {
-			return changedString;
-		}
-
-		try {
-			List<GenericValue> changes = changeLog.internalDelegator.findByAnd("ChangeItem",
-					MapBuilder.build("group", changeId));
-			for (GenericValue changeItem : changes) {
-				String oldValue = changeItem.getString("oldstring");
-				String newValue = changeItem.getString("newstring");
-				changedString.put(oldValue, newValue);
-			}
-		} catch (NullPointerException | GenericEntityException e) {
-			LOGGER.error("Get changed string during a Jira issue event failed. Message: " + e.getMessage());
-		}
-
-		return changedString;
 	}
 }
