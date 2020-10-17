5bcb2aaf6998ac4919e7d6581968ffbcdec50de1
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2020-10-17 12:59:30 +0200

    Only update decision problem if status changed

--- src/main/java/de/uhd/ifi/se/decision/management/jira/persistence/KnowledgePersistenceManager.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/persistence/KnowledgePersistenceManager.java
@@ -271,13 +271,22 @@ public class KnowledgePersistenceManager {
 	 * @return true if the status of the decision problem (=issue) was updated.
 	 */
 	public boolean updateIssueStatus(KnowledgeElement element, ApplicationUser user) {
+		boolean isIssueResolved = false;
+		boolean isUpdateNeeded = false;
 		if (element.getType().getSuperType() == KnowledgeType.PROBLEM) {
 			if (IssueCompletenessCheck.isDecisionProblemResolved(element)) {
-				element.setStatus(KnowledgeStatus.RESOLVED);
-			} else {
-				element.setStatus(KnowledgeStatus.UNRESOLVED);
+				isIssueResolved = true;
 			}
-			return updateKnowledgeElement(element, user);
+
+			if (isIssueResolved && element.getStatus() == KnowledgeStatus.UNRESOLVED) {
+				element.setStatus(KnowledgeStatus.RESOLVED);
+				return updateKnowledgeElement(element, user);
+			}
+
+			if (!isIssueResolved && element.getStatus() == KnowledgeStatus.RESOLVED) {
+				element.setStatus(KnowledgeStatus.UNRESOLVED);
+				return updateKnowledgeElement(element, user);
+			}
 		}
 		return false;
 	}
