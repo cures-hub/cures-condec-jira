7cc4bbce97a4c83b6c3c8e00207ab53787c74915
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2020-11-21 12:15:01 +0100

    CONDEC-822: Add CodeFileExtractorAndMaintainer class (#367)
    * Add CodeFileExtractorAndMaintainer class responsible for extraction and linking code files when cloning the repo and for maintaining links on git fetch
    
    * Update changed file after modifying or renaming, enable to keep old links when renaming file
    
    * Extracting means: 1) Adding code files to the {@link CodeClassInDatabase}, 2) adding links to the {@link LinkInDatabase}, 3) adding code files and links to the {@link KnowledgeGraph}.
    
    * Trace link creation and maintenance between code files and Jira issues works as follows: 1) Initial trace link creation during git clone. 2) Manual link improvement and maintenance by developers. Developers can manually change links. 3) Automatic trace link maintenance during git fetch based on recent changes.

---
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/versioncontrol/TestCodeFileExtractorAndMaintainer.java
@@ -0,0 +1,68 @@
+package de.uhd.ifi.se.decision.management.jira.extraction.versioncontrol;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+import de.uhd.ifi.se.decision.management.jira.extraction.gitclient.TestSetUpGit;
+import de.uhd.ifi.se.decision.management.jira.model.KnowledgeElement;
+import de.uhd.ifi.se.decision.management.jira.model.git.ChangedFile;
+import de.uhd.ifi.se.decision.management.jira.model.git.Diff;
+import de.uhd.ifi.se.decision.management.jira.persistence.codeclasspersistencemanager.TestInsertKnowledgeElement;
+import de.uhd.ifi.se.decision.management.jira.persistence.singlelocations.CodeClassPersistenceManager;
+import de.uhd.ifi.se.decision.management.jira.testdata.JiraUsers;
+import net.java.ao.test.jdbc.NonTransactional;
+
+public class TestCodeFileExtractorAndMaintainer extends TestSetUpGit {
+
+	@Test
+	@NonTransactional
+	public void testMaintainChangedFilesDiffNull() {
+		new CodeFileExtractorAndMaintainer("TEST").maintainChangedFilesInDatabase(null);
+		assertEquals(0, new CodeClassPersistenceManager("TEST").getKnowledgeElements().size());
+	}
+
+	@Test
+	@NonTransactional
+	public void testMaintainChangedFilesDiffEmpty() {
+		new CodeFileExtractorAndMaintainer("TEST").maintainChangedFilesInDatabase(new Diff());
+		assertEquals(0, new CodeClassPersistenceManager("TEST").getKnowledgeElements().size());
+	}
+
+	@Test
+	@NonTransactional
+	public void testMaintainChangedFilesDiffValidNoFilesInDatabase() {
+		Diff diff = gitClient.getDiffOfEntireDefaultBranch();
+		new CodeFileExtractorAndMaintainer("TEST").maintainChangedFilesInDatabase(diff);
+		assertEquals(6, new CodeClassPersistenceManager("TEST").getKnowledgeElements().size());
+	}
+
+	@Test
+	@NonTransactional
+	public void testMaintainChangedFilesDiffValidWithFilesInDatabase() {
+		KnowledgeElement classElement = TestInsertKnowledgeElement.createTestCodeClass();
+		CodeClassPersistenceManager persistenceManager = new CodeClassPersistenceManager("TEST");
+		persistenceManager.insertKnowledgeElement(classElement, JiraUsers.SYS_ADMIN.getApplicationUser());
+		Diff diff = gitClient.getDiffOfEntireDefaultBranch();
+		new CodeFileExtractorAndMaintainer("TEST").maintainChangedFilesInDatabase(diff);
+		assertEquals(7, persistenceManager.getKnowledgeElements().size());
+	}
+
+	@Test
+	@NonTransactional
+	public void testUpdateNonJavaFile() {
+		CodeFileExtractorAndMaintainer codeFileExtractorAndMaintainer = new CodeFileExtractorAndMaintainer("TEST");
+		codeFileExtractorAndMaintainer.addUpdateOrDeleteChangedFileInDatabase(new ChangedFile());
+		assertEquals(0, new CodeClassPersistenceManager("TEST").getKnowledgeElements().size());
+	}
+
+	@Test
+	@NonTransactional
+	public void testExtractAllChangedFilesTwice() {
+		CodeFileExtractorAndMaintainer codeFileExtractorAndMaintainer = new CodeFileExtractorAndMaintainer("TEST");
+		codeFileExtractorAndMaintainer.extractAllChangedFiles();
+		assertEquals(6, new CodeClassPersistenceManager("TEST").getKnowledgeElements().size());
+		codeFileExtractorAndMaintainer.extractAllChangedFiles();
+		assertEquals(6, new CodeClassPersistenceManager("TEST").getKnowledgeElements().size());
+	}
+}
