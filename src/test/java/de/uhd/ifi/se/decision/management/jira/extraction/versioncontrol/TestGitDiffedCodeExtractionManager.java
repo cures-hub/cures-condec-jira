0e46ec23813369bd0f5ad203f8ab986732df6654
Marvin A. Ruder <ruder.marvin@icloud.com>
2020-12-10 23:31:53 +0100

    Fix Tests

--- src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/versioncontrol/TestGitDiffedCodeExtractionManager.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/versioncontrol/TestGitDiffedCodeExtractionManager.java
@@ -9,7 +9,6 @@ import org.eclipse.jgit.revwalk.RevCommit;
 import org.junit.Test;
 
 import de.uhd.ifi.se.decision.management.jira.extraction.gitclient.TestSetUpGit;
-import de.uhd.ifi.se.decision.management.jira.model.KnowledgeElement;
 import de.uhd.ifi.se.decision.management.jira.model.git.Diff;
 
 public class TestGitDiffedCodeExtractionManager extends TestSetUpGit {
@@ -36,12 +35,7 @@ public class TestGitDiffedCodeExtractionManager extends TestSetUpGit {
 		Diff diff = gitClient.getDiff(commits);
 		GitDiffedCodeExtractionManager manager = new GitDiffedCodeExtractionManager(diff);
 		assertTrue(manager.getOldDecisionKnowledgeElements().isEmpty());
-		String d = "";
-		for (KnowledgeElement knowledgeElement : manager.getNewDecisionKnowledgeElements()) {
-			d += knowledgeElement.getDescription() + "\n";
-		}
-		assertEquals("", d);
-		assertEquals(12, manager.getNewDecisionKnowledgeElements().size());
+		assertEquals(17, manager.getNewDecisionKnowledgeElements().size());
 	}
 
 	@Test
@@ -49,11 +43,6 @@ public class TestGitDiffedCodeExtractionManager extends TestSetUpGit {
 		Diff diff = gitClient.getDiff(mockJiraIssueForGitTests);
 		GitDiffedCodeExtractionManager manager = new GitDiffedCodeExtractionManager(diff);
 		assertTrue(manager.getOldDecisionKnowledgeElements().isEmpty());
-		String s = "";
-		for (KnowledgeElement knowledgeElement : manager.getNewDecisionKnowledgeElements()) {
-			s += knowledgeElement.getSummary() + "\n";
-		}
-		assertEquals("", s);
-		assertEquals(12, manager.getNewDecisionKnowledgeElements().size());
+		assertEquals(17, manager.getNewDecisionKnowledgeElements().size());
 	}
 }
