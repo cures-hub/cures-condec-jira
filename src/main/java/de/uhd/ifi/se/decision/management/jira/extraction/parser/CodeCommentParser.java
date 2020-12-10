ba270074e415450330dd6b65d42e382a7f800291
Marvin A. Ruder <ruder.marvin@icloud.com>
2020-12-10 17:21:56 +0100

    Implement new code comment parser, starting with Java comment syntax only

--- src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/parser/CodeCommentParser.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/parser/CodeCommentParser.java
@@ -1,56 +1,85 @@
 package de.uhd.ifi.se.decision.management.jira.extraction.parser;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.github.javaparser.JavaParser;
-import com.github.javaparser.ParseProblemException;
-import com.github.javaparser.ParseResult;
-import com.github.javaparser.Position;
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.comments.Comment;
 
 import de.uhd.ifi.se.decision.management.jira.model.git.ChangedFile;
 import de.uhd.ifi.se.decision.management.jira.model.git.CodeComment;
 
 /**
- * Extracts decision knowledge elements from code comments of Java classes.
+ * Extracts decision knowledge elements from code comments.
  */
 public class CodeCommentParser {
-	private static final Logger LOGGER = LoggerFactory.getLogger(CodeCommentParser.class);
+	private List<CodeComment> commentList = new ArrayList<CodeComment>();
+
+	private CodeComment createComment(ChangedFile inspectedFile, String comment) {
+		return new CodeComment(comment, 0, 0, 0, 0); // TODO implement
+	}
 
 	public List<CodeComment> getComments(ChangedFile inspectedFile) {
-		ParseResult<CompilationUnit> parseResult = parseJavaFile(inspectedFile.getFileContent());
-		CompilationUnit compilationUnit = parseResult.getResult().get();
-		if (compilationUnit == null) {
-			return null;
-		} else {
-			return getComments(compilationUnit);
-		}
-	}
+		if (this.commentList == new ArrayList<CodeComment>()) {
+			String fileContent = inspectedFile.getFileContent() + "\n";
+			boolean inMultilineComment = false;
+			String comment = "";
+			int beginColumn = -1;
+			int beginLine = -1;
+			int lineNumber = 1;
 
-	public static ParseResult<CompilationUnit> parseJavaFile(String inspectedFileContent) {
-		ParseResult<CompilationUnit> parseResult = null;
-		try {
-			JavaParser javaParser = new JavaParser();
-			parseResult = javaParser.parse(inspectedFileContent);
-		} catch (ParseProblemException | NullPointerException e) {
-			LOGGER.error(e.getMessage());
-		}
-		return parseResult;
-	}
+			String singleLineCommentChar = "//"; // TODO change by reading from inspectedFile
+			String multiLineCommentCharStart = "/*"; // TODO change by reading from inspectedFile
+			String multiLineCommentCharEnd = "*/"; // TODO change by reading from inspectedFile
 
-	private List<CodeComment> getComments(CompilationUnit compilationUnit) {
-		List<Comment> comments = compilationUnit.getAllComments();
-		List<CodeComment> positionedComments = comments.stream()
-				.filter(comment -> (comment.getBegin().isPresent() && comment.getEnd().isPresent())).map(comment -> {
-					Position begin = comment.getBegin().get();
-					Position end = comment.getEnd().get();
-					return new CodeComment(comment.getContent(), begin.column, begin.line, end.column, end.line);
-				}).collect(Collectors.toList());
-		return positionedComments;
+			while (fileContent.indexOf("\n") != -1) {
+				String line = fileContent.substring(0, fileContent.indexOf("\n"));
+				if (inMultilineComment) { // we are in a multi-line comment
+					int multiLineCommentCharEndPos = line.indexOf(multiLineCommentCharEnd);
+					if (multiLineCommentCharEndPos != -1) { // the multi-line comment ends in this line
+						comment += line.substring(0, multiLineCommentCharEndPos + multiLineCommentCharEnd.length());
+						commentList.add(new CodeComment(comment, beginColumn, beginLine, lineNumber, multiLineCommentCharEndPos + multiLineCommentCharEnd.length()));
+						comment = "";
+						inMultilineComment = false;
+					} else { // the multi-line comment does not end in this line
+						comment += line;
+					}
+				} else { // we are not in a multi-line comment
+					int singleLineCommentCharPos = line.indexOf(singleLineCommentChar);
+					int multiLineCommentCharStartPos = line.indexOf(multiLineCommentCharStart);
+					if (multiLineCommentCharStartPos != -1) { // a multi-line comment starts in this line
+						inMultilineComment = true;
+						beginLine = lineNumber;
+						beginColumn = multiLineCommentCharStartPos;
+						if (comment.length() > 0) { // there is a single-line comment to end
+							commentList.add(new CodeComment(comment + "\n", beginColumn, beginLine, lineNumber, 0));
+						}
+						int multiLineCommentCharEndPos = line.indexOf(multiLineCommentCharEnd);
+						if (multiLineCommentCharEndPos == -1) { // the multi-line comment ends in this line
+							comment = line.substring(multiLineCommentCharStartPos);
+						} else { // the multi-line comment does not end in this line
+							comment = line.substring(multiLineCommentCharStartPos, multiLineCommentCharEndPos + multiLineCommentCharEnd.length());
+							commentList.add(new CodeComment(comment, beginColumn, beginLine, lineNumber, multiLineCommentCharEndPos + multiLineCommentCharEnd.length()));
+							comment = "";
+							inMultilineComment = false;
+						}
+					} else if (singleLineCommentCharPos != -1) { // a single-line comment starts in this line
+						comment += line.substring(singleLineCommentCharPos);
+						if (comment.length() == 0) { // there is no single-line comment present
+							beginLine = lineNumber;
+							beginColumn = singleLineCommentCharPos;
+						}
+					} else { // there is no comment in this line
+						if (comment.length() > 0) { // there is a single-line comment to end
+							commentList.add(new CodeComment(comment + "\n", beginColumn, beginLine, lineNumber, 0));
+							comment = "";
+						}
+					}
+				}
+				fileContent = fileContent.substring(fileContent.indexOf("\n") + 1);
+				lineNumber++;
+			}
+			if (comment.length() > 0) {
+				commentList.add(createComment(inspectedFile, comment));
+			}
+		}
+		return this.commentList;
 	}
 }
