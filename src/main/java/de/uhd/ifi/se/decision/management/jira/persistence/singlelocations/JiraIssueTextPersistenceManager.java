cd8909e1ad6c2cd456b5d9603bc2c25a95f0324d
Marvin A. Ruder <ruder.marvin@icloud.com>
2020-12-12 14:22:24 +0100

    Attempt to debug test on macOS

--- src/main/java/de/uhd/ifi/se/decision/management/jira/persistence/singlelocations/JiraIssueTextPersistenceManager.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/persistence/singlelocations/JiraIssueTextPersistenceManager.java
@@ -205,7 +205,7 @@ public class JiraIssueTextPersistenceManager extends AbstractPersistenceManagerF
 				Query.select().where("PROJECT_KEY = ? AND COMMENT_ID = ?", projectKey, commentId))) {
 			elements.add(new PartOfJiraIssueText(databaseEntry));
 		}
-		assertEquals(new ArrayList<>(), elements);
+		assertEquals(0, elements.size());
 		return elements;
 	}
 
