ab8a0b1dda8a0a16c6b2de7d5f945e5499425f95
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2020-12-21 16:37:33 +0100

    Test project deleted event listener

--- src/test/java/de/uhd/ifi/se/decision/management/jira/eventlistener/jiraissuetextextractioneventlistener/TestEventProjectDeleted.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/eventlistener/jiraissuetextextractioneventlistener/TestEventProjectDeleted.java
@@ -4,51 +4,27 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.event.ProjectDeletedEvent;
-import com.atlassian.jira.issue.comments.Comment;
 
-import de.uhd.ifi.se.decision.management.jira.model.KnowledgeElement;
+import de.uhd.ifi.se.decision.management.jira.persistence.KnowledgePersistenceManager;
+import de.uhd.ifi.se.decision.management.jira.persistence.singlelocations.JiraIssueTextPersistenceManager;
+import de.uhd.ifi.se.decision.management.jira.testdata.JiraIssues;
 import de.uhd.ifi.se.decision.management.jira.testdata.JiraProjects;
 import net.java.ao.test.jdbc.NonTransactional;
 
 public class TestEventProjectDeleted extends TestSetUpEventListener {
 
-	private boolean testProjectDeletedEvent(String commentBody) {
-		Comment comment = createComment(commentBody);
-		ComponentAccessor.getCommentManager().delete(comment);
+	@Test
+	@NonTransactional
+	public void testDeleteProject() {
+		JiraIssues.addElementToDataBase();
+		JiraIssueTextPersistenceManager persistenceManager = KnowledgePersistenceManager.getOrCreate("TEST")
+				.getJiraIssueTextManager();
+		assertTrue(persistenceManager.getKnowledgeElements().size() > 0);
+
 		ProjectDeletedEvent projectDeletedEvent = new ProjectDeletedEvent(user, JiraProjects.getTestProject());
 		listener.onProjectDeletedEvent(projectDeletedEvent);
 
-		boolean isCommentDeleted = !isCommentExistent(commentBody);
-
-		KnowledgeElement element = getFirstElementInComment(comment);
-		boolean isElementDeletedInDatabase = (element == null);
-
-		return isCommentDeleted && isElementDeletedInDatabase;
-	}
-
-	@Test
-	@NonTransactional
-	public void testNoCommentContained() {
-		assertTrue(testProjectDeletedEvent(""));
-	}
-
-	@Test
-	@NonTransactional
-	public void testRationaleTag() {
-		assertTrue(testProjectDeletedEvent("{issue}This is a very severe issue.{issue}"));
-	}
-
-	@Test
-	@NonTransactional
-	public void testExcludedTag() {
-		assertTrue(testProjectDeletedEvent("{code}public static class{code}"));
-	}
-
-	@Test
-	@NonTransactional
-	public void testRationaleIcon() {
-		assertTrue(testProjectDeletedEvent("(!)This is a very severe issue."));
+		assertTrue(persistenceManager.getKnowledgeElements().size() == 0);
 	}
 }
