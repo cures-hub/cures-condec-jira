ac054ccc76a13ccbcd0b75168be3af60d0dcf70b
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2021-02-28 19:28:33 +0100

    Test setTextClassifierEnabled REST API

---
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/rest/textclassificationrest/TestSetTextClassifierEnabled.java
@@ -0,0 +1,41 @@
+package de.uhd.ifi.se.decision.management.jira.rest.textclassificationrest;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.Response.Status;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.atlassian.jira.mock.servlet.MockHttpServletRequest;
+
+import de.uhd.ifi.se.decision.management.jira.TestSetUp;
+import de.uhd.ifi.se.decision.management.jira.rest.TextClassificationRest;
+import de.uhd.ifi.se.decision.management.jira.testdata.JiraUsers;
+
+public class TestSetTextClassifierEnabled extends TestSetUp {
+
+	private HttpServletRequest request;
+	private TextClassificationRest classificationRest;
+
+	@Before
+	public void setUp() {
+		init();
+		classificationRest = new TextClassificationRest();
+		request = new MockHttpServletRequest();
+		request.setAttribute("user", JiraUsers.SYS_ADMIN.getApplicationUser());
+	}
+
+	@Test
+	public void testRequestNullProjectKeyNullActivationFalse() {
+		assertEquals(Status.BAD_REQUEST.getStatusCode(),
+				classificationRest.setTextClassifierEnabled(request, null, false).getStatus());
+	}
+
+	@Test
+	public void testRequestNullProjectKeyValidActivationTrue() {
+		assertEquals(Status.OK.getStatusCode(),
+				classificationRest.setTextClassifierEnabled(request, "TEST", true).getStatus());
+	}
+}
