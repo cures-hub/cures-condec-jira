58d32e41052d028d38b87f365ac6553e275c9c2c
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2021-03-04 11:39:53 +0100

    Document decision on which classifier algorithms to use

--- src/main/java/de/uhd/ifi/se/decision/management/jira/classification/ClassifierType.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/classification/ClassifierType.java
@@ -7,6 +7,19 @@ import org.slf4j.LoggerFactory;
  * Models the type of machine learning algorithm used to train the binary and
  * fine-grained classifiers. For example, types can be support vector machine or
  * logistic regression.
+ * 
+ * @issue Which machine learning algorithm should we implement for automatic
+ *        text classification?
+ * @decision Implement support vector machines for automatic text
+ *           classification!
+ * @pro Performs best in related work, e.g. by Li et al. (2020): Automatic
+ *      Identification of Decisions from the Hibernate Developer Mailing List
+ * @decision Implement logistic regression for automatic text classification!
+ * @pro Easy to implement.
+ * @pro Training is fast.
+ * @alternative In the future, more algorithms need to be added, in particular
+ *              Naive Bayes and Decision Tree, because they performed well in
+ *              related work.
  */
 public enum ClassifierType {
 
