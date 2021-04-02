924b58850add60109782d01000b74d13595ac260
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2021-04-02 11:25:07 +0200

    Add DecisionGuidanceConfiguration to DecisionKnowledgeProject

--- src/test/java/de/uhd/ifi/se/decision/management/jira/config/TestSettingsOfSingleProject.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/config/TestSettingsOfSingleProject.java
@@ -97,6 +97,6 @@ public class TestSettingsOfSingleProject {
 	@NonTransactional
 	public void testGetVelocityParametersFilled() {
 		request.setAttribute("projectKey", "TEST");
-		assertEquals(19, servlet.getVelocityParameters(request).size());
+		assertEquals(15, servlet.getVelocityParameters(request).size());
 	}
 }
