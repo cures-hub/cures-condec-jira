5bdb89fa00af4d96ae1d586defd286d331a39332
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2021-04-06 10:44:49 +0200

    Make test data more realistic

--- src/test/java/de/uhd/ifi/se/decision/management/jira/decisionguidance/evaluation/TestEvaluationMetrics.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/decisionguidance/evaluation/TestEvaluationMetrics.java
@@ -26,10 +26,7 @@ import de.uhd.ifi.se.decision.management.jira.model.KnowledgeType;
 public class TestEvaluationMetrics extends TestSetUp {
 
 	protected FScore fScore;
-	protected Precision precision;
-	protected Recall recall;
 	protected AveragePrecision averagePrecision;
-	protected ReciprocalRank reciprocalRank;
 	protected NumberOfTruePositives numberOfTruePositives;
 	protected List<Recommendation> recommendations;
 	protected List<KnowledgeElement> solutionOptions;
@@ -43,8 +40,8 @@ public class TestEvaluationMetrics extends TestSetUp {
 
 		KnowledgeSource knowledgeSource = new ProjectSource("TEST", "TEST", true);
 
-		Recommendation recommendation = new Recommendation(knowledgeSource, "Test Recommendation", "Test Url");
-		Recommendation recommendation2 = new Recommendation(knowledgeSource, "Recommendation Test", "Test Url");
+		Recommendation recommendation = new Recommendation(knowledgeSource, "MySQL", "Test Url");
+		Recommendation recommendation2 = new Recommendation(knowledgeSource, "PostgreSQL", "Test Url");
 
 		recommendations.add(recommendation);
 		recommendations.add(recommendation2);
@@ -52,16 +49,16 @@ public class TestEvaluationMetrics extends TestSetUp {
 		KnowledgeElement alternativeIdea = new KnowledgeElement();
 		alternativeIdea.setType(KnowledgeType.ALTERNATIVE);
 		alternativeIdea.setStatus(KnowledgeStatus.IDEA);
-		alternativeIdea.setSummary("Test Recommendation"); // true positive
+		alternativeIdea.setSummary("We could use MySQL"); // true positive
 
 		KnowledgeElement alternativeDiscarded = new KnowledgeElement();
 		alternativeDiscarded.setType(KnowledgeType.ALTERNATIVE);
 		alternativeDiscarded.setStatus(KnowledgeStatus.DISCARDED);
-		alternativeDiscarded.setSummary("Test Discarded"); // false positive
+		alternativeDiscarded.setSummary("We could use a NoSQL database such as Cassandra"); // false positive
 
 		KnowledgeElement decision = new KnowledgeElement();
 		decision.setType(KnowledgeType.ALTERNATIVE);
-		decision.setSummary("Test DECISION"); // false Negative
+		decision.setSummary("We will use simple text files to store the data!"); // false Negative
 		decision.setStatus(KnowledgeStatus.DECIDED);
 
 		solutionOptions.add(alternativeIdea);
@@ -72,45 +69,38 @@ public class TestEvaluationMetrics extends TestSetUp {
 
 		fScore = new FScore(recommendations, solutionOptions);
 		numberOfTruePositives = new NumberOfTruePositives(recommendations, solutionOptions);
-		precision = new Precision(recommendations, solutionOptions);
-		recall = new Recall(recommendations, solutionOptions);
 		averagePrecision = new AveragePrecision(recommendations, solutionOptions);
-		reciprocalRank = new ReciprocalRank(recommendations, solutionOptions);
 	}
 
 	@Test
 	public void testAllMetricsCalculations() {
 		assertEquals(0.5, fScore.calculateMetric(), 0.0);
-		assertEquals(0.5, precision.calculateMetric(), 0.0);
-		assertEquals(0.33, recall.calculateMetric(), 0.1);
 		assertEquals(0.33, averagePrecision.calculateMetric(), 0.1);
-		assertEquals(1.0, reciprocalRank.calculateMetric(), 0.0);
 		assertEquals(1.0, numberOfTruePositives.calculateMetric(), 0.0);
 	}
 
 	@Test
 	public void testLabels() {
 		assertEquals("F-Score", fScore.getName());
-		assertEquals("Precision(@k)", precision.getName());
-		assertEquals("Recall(@k)", recall.getName());
 		assertEquals("Average Precision", averagePrecision.getName());
-		assertEquals("Reciprocal Rank", reciprocalRank.getName());
 		assertEquals("#True Positives", numberOfTruePositives.getName());
 	}
 
 	@Test
 	public void testDescriptions() {
 		assertEquals(false, fScore.getDescription().isBlank());
-		assertEquals(false, precision.getDescription().isBlank());
-		assertEquals(false, recall.getDescription().isBlank());
 		assertEquals(false, averagePrecision.getDescription().isBlank());
-		assertEquals(false, reciprocalRank.getDescription().isBlank());
 		assertEquals(false, numberOfTruePositives.getDescription().isBlank());
 	}
 
 	@Test
 	public void testPrecision() {
-		Precision precision = new Precision(recommendations, 2);
+		Precision precision = new Precision(recommendations, solutionOptions);
+		assertEquals(0.5, precision.calculateMetric(), 0.0);
+		assertEquals("Precision(@k)", precision.getName());
+		assertEquals(false, precision.getDescription().isBlank());
+
+		precision = new Precision(recommendations, 2);
 		assertEquals(2, recommendations.size());
 		assertEquals(1.0, precision.calculateMetric(), 0.0);
 
@@ -120,7 +110,12 @@ public class TestEvaluationMetrics extends TestSetUp {
 
 	@Test
 	public void testRecall() {
-		Recall recall = new Recall(1, 1);
+		Recall recall = new Recall(recommendations, solutionOptions);
+		assertEquals("Recall(@k)", recall.getName());
+		assertEquals(0.33, recall.calculateMetric(), 0.1);
+		assertEquals(false, recall.getDescription().isBlank());
+
+		recall = new Recall(1, 1);
 		assertEquals(0.5, recall.calculateMetric(), 0.0);
 
 		recall = new Recall(0, 0);
@@ -132,7 +127,15 @@ public class TestEvaluationMetrics extends TestSetUp {
 
 	@Test
 	public void testReciprocalRank() {
-		ReciprocalRank reciprocalRank = new ReciprocalRank(new ArrayList<>(), new ArrayList<>());
+		ReciprocalRank reciprocalRank = new ReciprocalRank(recommendations, solutionOptions);
+		assertEquals(1.0, reciprocalRank.calculateMetric(), 0.0);
+		assertEquals("Reciprocal Rank", reciprocalRank.getName());
+		assertEquals(false, reciprocalRank.getDescription().isBlank());
+
+		reciprocalRank = new ReciprocalRank(new ArrayList<>(), new ArrayList<>());
+		assertEquals(0.0, reciprocalRank.calculateMetric(), 0.0);
+
+		reciprocalRank = new ReciprocalRank(recommendations, new ArrayList<>());
 		assertEquals(0.0, reciprocalRank.calculateMetric(), 0.0);
 	}
 }
