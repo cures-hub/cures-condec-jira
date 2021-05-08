3c85fbe40793bd997c6d5992753efb4d1b5a443c
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2021-05-08 18:23:37 +0200

    Update dependencies

--- src/main/resources/js/condec.api.js
+++ src/main/resources/js/condec.api.js
@@ -903,20 +903,22 @@
 	};
 
 	function getIssueKey() {
-		var issueKey = null;
-		if (JIRA && JIRA.Issue && JIRA.Issue.getIssueKey) {
+		var issueKey;
+		try {
 			issueKey = JIRA.Issue.getIssueKey();
+		} catch (error) {
+			// console.log(error);
 		}
-		if (issueKey === undefined || !issueKey) {
-			// console.log("conDecAPI could not getIssueKey using object
-			// JIRA!");
-			if (AJS && AJS.Meta && AJS.Meta.get) {
+		if (issueKey === undefined) {
+			// console.log("conDecAPI could not getIssueKey using object JIRA!");
+			try {
 				issueKey = AJS.Meta.get("issue-key");
+			} catch (error) {
+				// console.log(error);
 			}
 		}
-		if (issueKey === undefined || !issueKey) {
-			// console.log("conDecAPI could not getIssueKey using object
-			// AJS!");
+		if (issueKey === undefined) {
+			// console.log("conDecAPI could not getIssueKey using object AJS!");
 			var chunks = document.location.pathname.split("/");
 			if (chunks.length > 0) {
 				var lastChunk = chunks[chunks.length - 1];
