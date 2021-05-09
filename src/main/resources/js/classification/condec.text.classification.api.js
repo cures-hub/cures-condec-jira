ab3cb6bfa77d8e7c7cd93e8185a865397314cd34
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2021-05-09 12:25:27 +0200

    Move text classification REST client methods to separate Javascript file

---
+++ src/main/resources/js/classification/condec.text.classification.api.js
@@ -0,0 +1,100 @@
+/**
+ * This module implements the communication with the ConDec REST API for 
+ * automatic decision knowledge identification in Jira issue descriptions and comments.
+ *
+ * Is referenced in HTML by settings/classification/*.vm
+ */
+(function(global) {
+
+	var ConDecTextClassificationAPI = function() {
+		this.restPrefix = AJS.contextPath() + "/rest/condec/latest/classification";
+	};
+
+	ConDecTextClassificationAPI.prototype.setTextClassifierEnabled = function(isTextClassifierEnabled, projectKey) {
+		generalApi.postJSON(this.restPrefix + "/setTextClassifierEnabled.json?projectKey="
+			+ projectKey + "&isTextClassifierEnabled=" + isTextClassifierEnabled, null, function(error,
+				response) {
+			if (error === null) {
+				conDecAPI.showFlag("success",
+					"Activation of text classification for decision knowledge in Jira issue description and comments was set to "
+					+ isTextClassifierEnabled + ".");
+			}
+		});
+	};
+
+	ConDecTextClassificationAPI.prototype.classifyText = function(text, projectKey, callback) {
+		generalApi.postJSON(this.restPrefix + "/classifyText?projectKey="
+			+ projectKey + "&text=" + text, null, function(error, response) {
+				if (error === null) {
+					callback(response.classificationResult);
+				} else {
+					callback("Error! Please check if the classifier is trained.");
+				}
+			});
+	};
+
+	ConDecTextClassificationAPI.prototype.classifyWholeProject = function(projectKey, animatedElement) {
+		animatedElement.classList.add("aui-progress-indicator-value");
+		generalApi.postJSON(this.restPrefix + "/classifyWholeProject.json?projectKey=" + projectKey,
+			null,
+			function(error, response) {
+				animatedElement.classList.remove("aui-progress-indicator-value");
+				if (error === null) {
+					conDecAPI.showFlag("success", "The whole project has been classified.");
+				}
+			});
+	};
+
+	ConDecTextClassificationAPI.prototype.trainClassifier = function(projectKey, trainingFileName, binaryClassifierType, fineGrainedClassifierType, animatedElement) {
+		animatedElement.classList.add("aui-progress-indicator-value");
+		generalApi.postJSON(this.restPrefix + "/trainClassifier.json?projectKey=" + projectKey + "&trainingFileName="
+			+ trainingFileName + "&binaryClassifierType=" + binaryClassifierType + "&fineGrainedClassifierType=" + fineGrainedClassifierType,
+			null,
+			function(error, response) {
+				animatedElement.classList.remove("aui-progress-indicator-value");
+				if (error === null) {
+					conDecAPI.showFlag("success", "The classifier was successfully retrained.");
+				}
+			});
+	};
+
+	ConDecTextClassificationAPI.prototype.useTrainedClassifier = function(projectKey, trainedClassifier, isOnlineLearningActivated) {
+		generalApi.postJSON(this.restPrefix + "/useTrainedClassifier.json?projectKey=" + projectKey +
+			"&trainedClassifier=" + trainedClassifier + "&isOnlineLearningActivated=" + isOnlineLearningActivated,
+			null,
+			function(error, response) {
+				if (error === null) {
+					conDecAPI.showFlag("success", "The trained classifier was successfully set for the project.");
+				}
+			});
+	};
+
+	ConDecTextClassificationAPI.prototype.evaluateTextClassifier = function(projectKey, trainingFileName, numberOfFolds,
+		binaryClassifierType, fineGrainedClassifierType, animatedElement, callback) {
+		animatedElement.classList.add("aui-progress-indicator-value");
+		generalApi.postJSON(this.restPrefix + "/evaluateTextClassifier.json?projectKey=" + projectKey + "&trainingFileName="
+			+ trainingFileName + "&numberOfFolds=" + numberOfFolds
+			+ "&binaryClassifierType=" + binaryClassifierType + "&fineGrainedClassifierType=" + fineGrainedClassifierType,
+			null,
+			function(error, response) {
+				animatedElement.classList.remove("aui-progress-indicator-value");
+				if (error === null) {
+					conDecAPI.showFlag("success", "The evaluation results file was successfully created.");
+					callback(response["content"]);
+				}
+			});
+	};
+
+	ConDecTextClassificationAPI.prototype.saveTrainingFile = function(projectKey, callback) {
+		generalApi.postJSON(this.restPrefix + "/saveTrainingFile.json?projectKey=" + projectKey, null,
+			function(error, response) {
+				if (error === null) {
+					conDecAPI.showFlag("success", "The training file was successfully created and saved in "
+						+ response["trainingFile"] + ".");
+					callback(response["content"]);
+				}
+			});
+	};
+
+	global.conDecTextClassificationAPI = new ConDecTextClassificationAPI();
+})(window);
