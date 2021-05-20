4eae07155ae38b87378d06f818a1d044f6b73395
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2021-05-20 20:43:37 +0200

    Remove simplifiedGraph method of CIA Service because it is currently not used and can be replaced by FilteringManager::getFilteredGraph

--- src/main/java/de/uhd/ifi/se/decision/management/jira/service/CiaService.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/service/CiaService.java
@@ -1,28 +1,28 @@
 package de.uhd.ifi.se.decision.management.jira.service;
 
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.collect.ImmutableMap;
+
 import de.uhd.ifi.se.decision.management.jira.filtering.FilterSettings;
+import de.uhd.ifi.se.decision.management.jira.filtering.FilteringManager;
 import de.uhd.ifi.se.decision.management.jira.model.KnowledgeElement;
 import de.uhd.ifi.se.decision.management.jira.model.KnowledgeGraph;
 import de.uhd.ifi.se.decision.management.jira.model.KnowledgeType;
-import de.uhd.ifi.se.decision.management.jira.model.Link;
-import de.uhd.ifi.se.decision.management.jira.model.LinkType;
 import de.uhd.ifi.se.decision.management.jira.view.matrix.Matrix;
 import de.uhd.ifi.se.decision.management.jira.view.treeviewer.TreeViewer;
 import de.uhd.ifi.se.decision.management.jira.view.treeviewer.TreeViewerNode;
 import de.uhd.ifi.se.decision.management.jira.view.vis.VisEdge;
 import de.uhd.ifi.se.decision.management.jira.view.vis.VisGraph;
 import de.uhd.ifi.se.decision.management.jira.view.vis.VisNode;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.awt.Color;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
 
 public class CiaService {
 
@@ -39,18 +39,22 @@ public class CiaService {
 
 	public static VisGraph calculateGraphImpact(FilterSettings filterSettings) {
 		HashMap<KnowledgeElement, Double> results = calculateImpactedKnowledgeElements(filterSettings);
-		//KnowledgeGraph graph = simplifiedGraph(asGraph(results,filterSettings),filterSettings, new AtomicLong(-1));
+		// KnowledgeGraph graph =
+		// simplifiedGraph(asGraph(results,filterSettings),filterSettings, new
+		// AtomicLong(-1));
 		KnowledgeGraph graph = KnowledgeGraph.getInstance(filterSettings.getProjectKey());
 		return asVisGraph(results, filterSettings, graph);
-		//return asVisGraph(results, filterSettings, simplifiedGraph(graph, filterSettings, new AtomicLong(-1)));
+		// return asVisGraph(results, filterSettings, simplifiedGraph(graph,
+		// filterSettings, new AtomicLong(-1)));
 	}
 
 	public static Matrix calculateMatrixImpact(FilterSettings filterSettings) {
 		HashMap<KnowledgeElement, Double> impactGraph = calculateImpactedKnowledgeElements(filterSettings);
+		FilteringManager filteringManager = new FilteringManager(filterSettings);
 		Set<KnowledgeElement> elementSet = impactGraph.keySet().stream()
-			.filter(knowledgeElement -> filterSettings.getKnowledgeTypes().contains(knowledgeElement.getTypeAsString()))
-			.filter(knowledgeElement -> filterSettings.getStatus().contains(knowledgeElement.getStatus()))
-			.collect(Collectors.toSet());
+				.filter(knowledgeElement -> filteringManager.isElementMatchingKnowledgeTypeFilter(knowledgeElement))
+				.filter(knowledgeElement -> filteringManager.isElementMatchingStatusFilter(knowledgeElement))
+				.collect(Collectors.toSet());
 		Map<Long, String> colorMap = new HashMap<>();
 		elementSet.forEach(entry -> {
 			if (impactGraph.get(entry) != 0.0) {
@@ -66,24 +70,22 @@ public class CiaService {
 		HashMap<KnowledgeElement, Double> results = new HashMap<>();
 		KnowledgeElement root = filterSettings.getSelectedElement();
 		results.put(root, 1.0);
-		calculateImpactedKnowledgeElementsHelper(root, 1.0, filterSettings, results, (long) filterSettings.getLinkDistance());
+		calculateImpactedKnowledgeElementsHelper(root, 1.0, filterSettings, results,
+				(long) filterSettings.getLinkDistance());
 		LOGGER.info("CONDEC: {} Elements found", results.size());
 		return results;
 	}
 
-	private static void calculateImpactedKnowledgeElementsHelper(
-		KnowledgeElement root, Double parentImpact,
-		FilterSettings filterSettings,
-		HashMap<KnowledgeElement, Double> results,
-		final Long context
-	) {
+	private static void calculateImpactedKnowledgeElementsHelper(KnowledgeElement root, Double parentImpact,
+			FilterSettings filterSettings, HashMap<KnowledgeElement, Double> results, final Long context) {
 		Set<String> result = new HashSet<>();
 		root.getLinks().forEach(entry -> {
 			// TODO Link specific weights
 			boolean isOutwardLink = entry.getSource().equals(root);
 			String linkType = (isOutwardLink) ? entry.getType().getOutwardName() : entry.getType().getInwardName();
 			if (!filterSettings.getLinkImpact().containsKey(linkType)) {
-				result.add("link -> " + linkType + ", source -> " + entry.getSource().getId() + ", target -> " + entry.getTarget().getId());
+				result.add("link -> " + linkType + ", source -> " + entry.getSource().getId() + ", target -> "
+						+ entry.getTarget().getId());
 			}
 			double typeWeight = filterSettings.getLinkImpact().getOrDefault(linkType, 1.0f);
 			double decayValue = filterSettings.getDecayValue();
@@ -91,8 +93,9 @@ public class CiaService {
 
 			KnowledgeElement next = (isOutwardLink) ? entry.getTarget() : entry.getSource();
 
-			final boolean[] propagate = {true};
-			filterSettings.getPropagationRule().forEach(rule -> propagate[0] = propagate[0] && rule.getPredicate().pass(root, parentImpact, next, impact, entry));
+			final boolean[] propagate = { true };
+			filterSettings.getPropagationRule().forEach(rule -> propagate[0] = propagate[0]
+					&& rule.getPredicate().pass(root, parentImpact, next, impact, entry));
 
 			if (impact >= filterSettings.getThreshold() && propagate[0]) {
 				if (!results.containsKey(next) || (results.containsKey(next) && results.get(next) < impact)) {
@@ -109,93 +112,49 @@ public class CiaService {
 		}
 	}
 
-	private static VisGraph asVisGraph( HashMap<KnowledgeElement, Double> results, FilterSettings filterSettings, KnowledgeGraph graph ) {
+	private static VisGraph asVisGraph(HashMap<KnowledgeElement, Double> results, FilterSettings filterSettings,
+			KnowledgeGraph graph) {
 		Set<KnowledgeElement> nodes = results.keySet();
 		VisGraph graphVis = new VisGraph();
-		graphVis.setNodes(results.keySet().stream().map(
-			(entry) -> {
-				boolean collapse = collapse(entry, filterSettings);
-				VisNode node = new VisNode(entry,collapse, 0);
-				if ( results.get(entry) != null &&  results.get(entry) != 0.0) {
-					colorizeNode(node, results.get(entry));
-				}
-				return node;
+		graphVis.setNodes(results.keySet().stream().map((entry) -> {
+			boolean collapse = collapse(entry, filterSettings);
+			VisNode node = new VisNode(entry, collapse, 0);
+			if (results.get(entry) != null && results.get(entry) != 0.0) {
+				colorizeNode(node, results.get(entry));
 			}
-		).collect(Collectors.toSet()));
-		graph.edgeSet().stream()
-			.filter(link -> nodes.contains(link.getSource()) && nodes.contains(link.getTarget()))
-			.forEach(link ->{
-				VisEdge edge = new VisEdge(link);
-				graphVis.getEdges().add(edge);
-			});
+			return node;
+		}).collect(Collectors.toSet()));
+		graph.edgeSet().stream().filter(link -> nodes.contains(link.getSource()) && nodes.contains(link.getTarget()))
+				.forEach(link -> {
+					VisEdge edge = new VisEdge(link);
+					graphVis.getEdges().add(edge);
+				});
 		return graphVis;
 	}
 
 	private static boolean collapse(KnowledgeElement element, FilterSettings settings) {
-		KnowledgeType type =
-			(element.getType().equals(KnowledgeType.CON) || element.getType().equals(KnowledgeType.PRO)) ? KnowledgeType.ARGUMENT : element.getType();
-		if(element.equals(settings.getSelectedElement())){
+		KnowledgeType type = (element.getType().equals(KnowledgeType.CON)
+				|| element.getType().equals(KnowledgeType.PRO)) ? KnowledgeType.ARGUMENT : element.getType();
+		if (element.equals(settings.getSelectedElement())) {
 			return false;
-		} else return !(settings.getKnowledgeTypes().contains(type.toString()))
-			|| !(settings.getStatus().contains(element.getStatus()));
-	}
-
-	static KnowledgeGraph simplifiedGraph( KnowledgeGraph graph,  FilterSettings filterSettings, AtomicLong counter ) {
-		Set<KnowledgeElement> elements = new HashSet<>(graph.vertexSet());
-		elements.stream()
-			.filter( element -> collapse(element, filterSettings))
-			.forEach( element -> {
-				Set<KnowledgeElement> targetElements = new HashSet<>();
-				Set<KnowledgeElement> srcElements = new HashSet<>();
-				element.getLinks().forEach( link -> {
-					if(link.getSource().equals(element)) {
-						// Outgoing Link
-						targetElements.add(link.getTarget());
-					} else {
-						// Incoming Link
-						srcElements.add(link.getSource());
-					}
-				});
-				// Remove Links
-				graph.removeVertex(element);
-				// Link all Nodes
-				generateTransitveLinks(srcElements, targetElements, counter).forEach(graph::addEdge);
-				generateTransitveLinks(srcElements, srcElements,counter).forEach(graph::addEdge);
-				generateTransitveLinks(targetElements, targetElements, counter).forEach(graph::addEdge);
-			});
-		return graph;
-	}
-
-	private static Set<Link> generateTransitveLinks(Set<KnowledgeElement> sources, Set<KnowledgeElement> targets, AtomicLong counter) {
-		Set <Link> links = new HashSet<>();
-		sources.forEach( src -> {
-			targets.forEach( dest -> {
-				if(src != dest) {
-					Link newLink = new Link(src, dest, LinkType.TRANSITIVE);
-					newLink.setId(counter.getAndDecrement());
-					links.add(newLink);
-				}
-			});
-		});
-		return links;
+		} else
+			return !(settings.getKnowledgeTypes().contains(type.toString()))
+					|| !(settings.getStatus().contains(element.getStatus()));
 	}
 
 	private static void colorizeNode(TreeViewerNode node, HashMap<KnowledgeElement, Double> results) {
 		String style = "";
-		if(results.containsKey(node.getElement())){
-			style = "background-color:"+colorForImpact(results.get(node.getElement()));
+		if (results.containsKey(node.getElement())) {
+			style = "background-color:" + colorForImpact(results.get(node.getElement()));
 		} else {
 			style = "background-color:white";
 		}
 		String clzz = node.getLiAttr().get("class");
-		node.setLiAttr(ImmutableMap.<String, String>builder()
-			.put("style", style)
-			.put("class", clzz)
-			.build());
+		node.setLiAttr(ImmutableMap.<String, String>builder().put("style", style).put("class", clzz).build());
 		String aStyle = "color:black";
 		node.setAttr(ImmutableMap.of("style", aStyle));
-		node.getChildren().forEach( child -> {
-			colorizeNode( child,results);
+		node.getChildren().forEach(child -> {
+			colorizeNode(child, results);
 		});
 	}
 
@@ -215,8 +174,10 @@ public class CiaService {
 
 	private static Color blend(Color color1, Color color2, float pRatio) {
 		float ratio = pRatio;
-		if (ratio > 1f) ratio = 1f;
-		else if (ratio < 0f) ratio = 0f;
+		if (ratio > 1f)
+			ratio = 1f;
+		else if (ratio < 0f)
+			ratio = 0f;
 		float iRatio = 1.0f - ratio;
 
 		int i1 = color1.getRGB();
