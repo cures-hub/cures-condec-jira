0701c7df1746f4b3d1647e93f3e1ae3dd2ccc070
Julia Baum <j.baum@stud.uni-heidelberg.de>
2021-05-23 12:54:56 +0200

    Add test case with no non-validated elements

--- src/test/java/de/uhd/ifi/se/decision/management/jira/rest/textclassificationrest/TestGetNonValidatedElements.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/rest/textclassificationrest/TestGetNonValidatedElements.java
@@ -1,14 +1,20 @@
 package de.uhd.ifi.se.decision.management.jira.rest.textclassificationrest;
 
+import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.mock.servlet.MockHttpServletRequest;
 import de.uhd.ifi.se.decision.management.jira.TestSetUp;
+import de.uhd.ifi.se.decision.management.jira.model.KnowledgeElement;
 import de.uhd.ifi.se.decision.management.jira.rest.TextClassificationRest;
+import de.uhd.ifi.se.decision.management.jira.testdata.JiraIssues;
 import de.uhd.ifi.se.decision.management.jira.testdata.JiraUsers;
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,6 +22,7 @@ public class TestGetNonValidatedElements extends TestSetUp {
 
 	private HttpServletRequest request;
 	private TextClassificationRest classificationRest;
+	private List<Issue> jiraIssues;
 
 	@Before
 	public void setUp() {
@@ -23,6 +30,8 @@ public class TestGetNonValidatedElements extends TestSetUp {
 		classificationRest = new TextClassificationRest();
 		request = new MockHttpServletRequest();
 		request.setAttribute("user", JiraUsers.SYS_ADMIN.getApplicationUser());
+		jiraIssues = JiraIssues.getTestJiraIssues();
+
 	}
 
 	@Test
@@ -32,5 +41,13 @@ public class TestGetNonValidatedElements extends TestSetUp {
 
 	}
 
+	@Test
+	public void testValidWithNoNonValidatedElements() {
+		Response response = classificationRest.getNonValidatedElements(request, jiraIssues.get(0).getProjectObject().getKey(), jiraIssues.get(0).getKey());
+
+		Map<String, List<KnowledgeElement>> expected = Map.of("nonValidatedElements", new ArrayList<>());
+
+		assertEquals(expected, response.getEntity());
+	}
 
 }
