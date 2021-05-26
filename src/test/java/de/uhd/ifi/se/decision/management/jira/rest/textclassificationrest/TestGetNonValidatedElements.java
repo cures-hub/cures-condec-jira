ef261dbfdbf3196700e23e82de71b326490d0fca
Julia Baum <j.baum@stud.uni-heidelberg.de>
2021-05-26 18:26:15 +0200

    Fix broken test

--- src/test/java/de/uhd/ifi/se/decision/management/jira/rest/textclassificationrest/TestGetNonValidatedElements.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/rest/textclassificationrest/TestGetNonValidatedElements.java
@@ -11,14 +11,17 @@ import de.uhd.ifi.se.decision.management.jira.persistence.singlelocations.JiraIs
 import de.uhd.ifi.se.decision.management.jira.rest.TextClassificationRest;
 import de.uhd.ifi.se.decision.management.jira.testdata.JiraIssues;
 import de.uhd.ifi.se.decision.management.jira.testdata.JiraUsers;
+import net.java.ao.test.jdbc.NonTransactional;
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static java.util.List.of;
 import static org.junit.Assert.assertEquals;
 
 public class TestGetNonValidatedElements extends TestSetUp {
@@ -53,29 +56,31 @@ public class TestGetNonValidatedElements extends TestSetUp {
 		assertEquals(expected, response.getEntity());
 	}
 
-//	@Test
-//	public void testValidWithNonValidatedElements() {
-//		Issue issue = jiraIssues.get(4);
-//
-//		PartOfJiraIssueText validatedElement = JiraIssues.addElementToDataBase(12234, KnowledgeType.ARGUMENT);
-//		validatedElement.setJiraIssue(issue);
-//		validatedElement.setValidated(true);
-//		JiraIssues.updateElementInDatabase(validatedElement);
-//
-//		PartOfJiraIssueText nonValidatedElement = JiraIssues.addElementToDataBase(12235, KnowledgeType.ARGUMENT);
-//		nonValidatedElement.setJiraIssue(issue);
-//		nonValidatedElement.setValidated(false);
-//		nonValidatedElement.setDescription("Dummy argument!");
-//
-//
-//		List<KnowledgeElement> expectedElement = new ArrayList<>();
-//		expectedElement.add(nonValidatedElement);
-//
-//		Response response = classificationRest.getNonValidatedElements(request, jiraIssues.get(4).getProjectObject().getKey(), jiraIssues.get(4).getKey());
-//
-//		ImmutableMap<String, List<KnowledgeElement>> expected = ImmutableMap.of("nonValidatedElements", expectedElement);
-//		assertEquals(expected, response.getEntity());
-//
-//	}
+	@Test
+	@NonTransactional
+	public void testValidWithNonValidatedElements() {
+		Issue issue = jiraIssues.get(0);
+
+
+		PartOfJiraIssueText nonValidatedElement1 = JiraIssues.addElementToDataBase(12234, KnowledgeType.ARGUMENT);
+		nonValidatedElement1.setJiraIssue(issue);
+		nonValidatedElement1.setValidated(false);
+
+		PartOfJiraIssueText nonValidatedElement2 = JiraIssues.addElementToDataBase(12234, KnowledgeType.ARGUMENT);
+		nonValidatedElement2.setJiraIssue(issue);
+		nonValidatedElement2.setValidated(false);
+
+		JiraIssueTextPersistenceManager manager = new JiraIssueTextPersistenceManager(issue.getProjectObject().getKey());
+		manager.updateInDatabase(nonValidatedElement1);
+		manager.updateInDatabase(nonValidatedElement2);
+
+		List<KnowledgeElement> expectedElements = Arrays.asList(nonValidatedElement1, nonValidatedElement2);
+
+		Response response = classificationRest.getNonValidatedElements(request, issue.getProjectObject().getKey(), issue.getKey());
+
+		ImmutableMap<String, List<KnowledgeElement>> expected = ImmutableMap.of("nonValidatedElements", expectedElements);
+		assertEquals(expected, response.getEntity());
+
+	}
 
 }
