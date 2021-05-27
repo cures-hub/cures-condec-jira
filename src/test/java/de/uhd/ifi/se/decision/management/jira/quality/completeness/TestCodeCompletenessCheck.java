6c3600bddd8571387dc5619482100d12aa4f9fe8
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2021-05-27 21:20:48 +0200

    Try to fix broken test

--- src/test/java/de/uhd/ifi/se/decision/management/jira/quality/completeness/TestCodeCompletenessCheck.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/quality/completeness/TestCodeCompletenessCheck.java
@@ -8,9 +8,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 import de.uhd.ifi.se.decision.management.jira.TestSetUp;
+import de.uhd.ifi.se.decision.management.jira.model.KnowledgeElement;
+import de.uhd.ifi.se.decision.management.jira.model.KnowledgeType;
 import de.uhd.ifi.se.decision.management.jira.model.git.ChangedFile;
 import de.uhd.ifi.se.decision.management.jira.persistence.ConfigPersistenceManager;
+import de.uhd.ifi.se.decision.management.jira.persistence.KnowledgePersistenceManager;
 import de.uhd.ifi.se.decision.management.jira.testdata.CodeFiles;
+import de.uhd.ifi.se.decision.management.jira.testdata.JiraIssues;
+import de.uhd.ifi.se.decision.management.jira.testdata.JiraUsers;
 import net.java.ao.test.jdbc.NonTransactional;
 
 public class TestCodeCompletenessCheck extends TestSetUp {
@@ -61,6 +66,9 @@ public class TestCodeCompletenessCheck extends TestSetUp {
 		DefinitionOfDone definitionOfDone = ConfigPersistenceManager.getDefinitionOfDone("TEST");
 		definitionOfDone.setMinimumDecisionsWithinLinkDistance(1);
 		ConfigPersistenceManager.saveDefinitionOfDone("TEST", definitionOfDone);
+		KnowledgeElement decision = JiraIssues.addElementToDataBase(322, KnowledgeType.DECISION);
+		KnowledgePersistenceManager.getOrCreate("TEST").insertLink(linkedFileThatIsDone, decision,
+				JiraUsers.SYS_ADMIN.getApplicationUser());
 		assertTrue(codeCompletenessCheck.execute(linkedFileThatIsDone));
 		definitionOfDone.setMaximumLinkDistanceToDecisions(0);
 		ConfigPersistenceManager.saveDefinitionOfDone("TEST", definitionOfDone);
