518f889a6549f83c423323230fe9ce2ab92443d7
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2021-05-28 19:32:35 +0200

    Start to refactor CodeCommentParser

--- src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/parser/CodeCommentParser.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/extraction/parser/CodeCommentParser.java
@@ -5,92 +5,101 @@ import java.util.List;
 
 import de.uhd.ifi.se.decision.management.jira.model.git.ChangedFile;
 import de.uhd.ifi.se.decision.management.jira.model.git.CodeComment;
+import de.uhd.ifi.se.decision.management.jira.model.git.CommentStyleType;
 
 /**
  * Extracts decision knowledge elements from code comments.
  */
 public class CodeCommentParser {
-	private List<CodeComment> commentList = new ArrayList<CodeComment>();
 
 	public List<CodeComment> getComments(ChangedFile inspectedFile) {
-		if (this.commentList.size() == 0) {
-			String fileContent = inspectedFile.getFileContent() + "\n";
-			boolean inMultilineComment = false;
-			String comment = "";
-			int beginColumn = -1;
-			int beginLine = -1;
-			int lineNumber = 1;
-			int lastLineCol = -1;
+		return getComments(inspectedFile.getFileContent() + "\n", inspectedFile.getCommentStyleType());
+	}
 
-			String singleLineCommentChar = inspectedFile.getCommentStyleType().getSingleLineCommentChar();
-			String multiLineCommentCharStart = inspectedFile.getCommentStyleType().getMultiLineCommentCharStart();
-			String multiLineCommentCharEnd = inspectedFile.getCommentStyleType().getMultiLineCommentCharEnd();
+	public List<CodeComment> getComments(String fileContent, CommentStyleType commentStyleType) {
+		List<CodeComment> commentList = new ArrayList<CodeComment>();
+		boolean inMultilineComment = false;
+		String comment = "";
+		int beginColumn = -1;
+		int beginLine = -1;
+		int lineNumber = 1;
+		int lastLineCol = -1;
 
-			while (fileContent.indexOf("\n") != -1) {
-				String line = fileContent.substring(0, fileContent.indexOf("\n") + 1);
-				if (inMultilineComment) { // we are in a multi-line comment
+		String singleLineCommentChar = commentStyleType.getSingleLineCommentChar();
+		String multiLineCommentCharStart = commentStyleType.getMultiLineCommentCharStart();
+		String multiLineCommentCharEnd = commentStyleType.getMultiLineCommentCharEnd();
+
+		while (fileContent.indexOf("\n") != -1) {
+			String line = fileContent.substring(0, fileContent.indexOf("\n") + 1);
+			if (inMultilineComment) { // we are in a multi-line comment
+				int multiLineCommentCharEndPos = -1;
+				if (multiLineCommentCharEnd != null) {
+					multiLineCommentCharEndPos = line.indexOf(multiLineCommentCharEnd);
+				}
+				if (multiLineCommentCharEndPos != -1) { // the multi-line comment ends in this line
+					comment += line.substring(0, multiLineCommentCharEndPos + multiLineCommentCharEnd.length());
+					commentList.add(new CodeComment(comment, beginColumn, beginLine,
+							multiLineCommentCharEndPos + multiLineCommentCharEnd.length() + 1, lineNumber));
+					comment = "";
+					inMultilineComment = false;
+				} else { // the multi-line comment does not end in this line
+					comment += line;
+				}
+			} else { // we are not in a multi-line comment
+				int singleLineCommentCharPos = -1;
+				if (singleLineCommentChar != null) { // the file type does not support single-line comments
+					singleLineCommentCharPos = line.indexOf(singleLineCommentChar);
+				}
+				int multiLineCommentCharStartPos = -1;
+				if (multiLineCommentCharStart != null) { // the file type does not support multi-line comments
+					multiLineCommentCharStartPos = line.indexOf(multiLineCommentCharStart);
+				}
+				if (multiLineCommentCharStartPos != -1) { // a multi-line comment starts in this line
+					inMultilineComment = true;
+					beginLine = lineNumber;
+					beginColumn = multiLineCommentCharStartPos + 1;
+					if (comment.length() > 0) { // there is a single-line comment to end
+						commentList.add(new CodeComment(comment.substring(0, comment.length() - 1), beginColumn,
+								beginLine, lastLineCol, lineNumber - 1));
+						comment = "";
+					}
 					int multiLineCommentCharEndPos = -1;
-					if (multiLineCommentCharEnd != null) {
+					if (multiLineCommentCharEnd != null) { // the file type does not support multi-line comments
 						multiLineCommentCharEndPos = line.indexOf(multiLineCommentCharEnd);
 					}
-					if (multiLineCommentCharEndPos != -1) { // the multi-line comment ends in this line
-						comment += line.substring(0, multiLineCommentCharEndPos + multiLineCommentCharEnd.length());
-						commentList.add(new CodeComment(comment, beginColumn, beginLine, multiLineCommentCharEndPos + multiLineCommentCharEnd.length() + 1, lineNumber));
+					if (multiLineCommentCharEndPos == -1) { // the multi-line comment does not end in this line
+						comment = line.substring(multiLineCommentCharStartPos);
+					} else { // the multi-line comment ends in this line
+						comment = line.substring(multiLineCommentCharStartPos,
+								multiLineCommentCharEndPos + multiLineCommentCharEnd.length());
+						commentList.add(new CodeComment(comment, beginColumn, beginLine,
+								multiLineCommentCharEndPos + multiLineCommentCharEnd.length() + 1, lineNumber));
 						comment = "";
 						inMultilineComment = false;
-					} else { // the multi-line comment does not end in this line
-						comment += line;
 					}
-				} else { // we are not in a multi-line comment
-					int singleLineCommentCharPos = -1;
-					if (singleLineCommentChar != null) { // the file type does not support single-line comments
-						singleLineCommentCharPos = line.indexOf(singleLineCommentChar);
-					}
-					int multiLineCommentCharStartPos = -1;
-					if (multiLineCommentCharStart != null) { // the file type does not support multi-line comments
-						multiLineCommentCharStartPos = line.indexOf(multiLineCommentCharStart);
-					}
-					if (multiLineCommentCharStartPos != -1) { // a multi-line comment starts in this line
-						inMultilineComment = true;
+				} else if (singleLineCommentCharPos != -1) { // a single-line comment starts in this line
+					if (comment.length() == 0) { // there is no single-line comment present
 						beginLine = lineNumber;
-						beginColumn = multiLineCommentCharStartPos + 1;
-						if (comment.length() > 0) { // there is a single-line comment to end
-							commentList.add(new CodeComment(comment.substring(0, comment.length() - 1), beginColumn, beginLine, lastLineCol, lineNumber - 1));
-							comment = "";
-						}
-						int multiLineCommentCharEndPos = -1;
-						if (multiLineCommentCharEnd != null) { // the file type does not support multi-line comments
-							multiLineCommentCharEndPos = line.indexOf(multiLineCommentCharEnd);
-						}
-						if (multiLineCommentCharEndPos == -1) { // the multi-line comment does not end in this line
-							comment = line.substring(multiLineCommentCharStartPos);
-						} else { // the multi-line comment ends in this line
-							comment = line.substring(multiLineCommentCharStartPos, multiLineCommentCharEndPos + multiLineCommentCharEnd.length());
-							commentList.add(new CodeComment(comment, beginColumn, beginLine, multiLineCommentCharEndPos + multiLineCommentCharEnd.length() + 1, lineNumber));
-							comment = "";
-							inMultilineComment = false;
-						}
-					} else if (singleLineCommentCharPos != -1) { // a single-line comment starts in this line
-						if (comment.length() == 0) { // there is no single-line comment present
-							beginLine = lineNumber;
-							beginColumn = singleLineCommentCharPos + 1;
-						}
-						comment += line.substring(singleLineCommentCharPos);
-					} else { // there is no comment in this line
-						if (comment.length() > 0) { // there is a single-line comment to end
-							commentList.add(new CodeComment(comment.substring(0, comment.length() - 1), beginColumn, beginLine, lastLineCol, lineNumber - 1));
-							comment = "";
-						}
+						beginColumn = singleLineCommentCharPos + 1;
+					}
+					comment += line.substring(singleLineCommentCharPos);
+				} else { // there is no comment in this line
+					if (comment.length() > 0) { // there is a single-line comment to end
+						commentList.add(new CodeComment(comment.substring(0, comment.length() - 1), beginColumn,
+								beginLine, lastLineCol, lineNumber - 1));
+						comment = "";
 					}
 				}
-				fileContent = fileContent.substring(fileContent.indexOf("\n") + 1);
-				lineNumber++;
-				lastLineCol = line.length();
 			}
-			if (comment.length() > 0) {
-				commentList.add(new CodeComment(comment.substring(0, comment.length() - 1), beginColumn, beginLine, lastLineCol, lineNumber - 1));
-			}
+			fileContent = fileContent.substring(fileContent.indexOf("\n") + 1);
+			lineNumber++;
+			lastLineCol = line.length();
 		}
-		return this.commentList;
+		if (comment.length() > 0) {
+			commentList.add(new CodeComment(comment.substring(0, comment.length() - 1), beginColumn, beginLine,
+					lastLineCol, lineNumber - 1));
+		}
+		return commentList;
 	}
+
 }
--- src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/parser/TestCodeCommentParser.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/extraction/parser/TestCodeCommentParser.java
@@ -22,18 +22,26 @@ public class TestCodeCommentParser extends TestSetUp {
 	@Test
 	public void testCodeCommentParser() {
 		ChangedFile file = new ChangedFile(
-				"// @" + "con This is a structure violation, but it should not kill knowledge extraction\n" + "\n"
-						+ "// @" + "goal This is a goal outside an issue, let's see where it lands.\n" + "// @"
-						+ "assumption This is an assumption outside an issue, let's see where it lands.\n" + "\n"
-						+ "// @" + "alternative Here is another structure violation!\n" + "\n" + "/**\n" + " * @"
-						+ "issue Is this yet another structure violation?\n" + " * @" + "con It would appear so.\n"
-						+ " * @" + "assumption Here is an assumption inside an issue, let's see where this one lands.\n"
-						+ " * @" + "goal Here is a goal inside an issue, let's see where this one lands.\n" + " */"
-						+ "\n" + "public class GodClass {\n" + "//@"
-						+ "issue Small code issue in GodClass, it does nothing. \t \n \t \n" + "/** \n" + " * @"
-						+ "issue Will this issue be parsed correctly? \n" + " * @" + "alternative We will see!\n"
-						+ " * @" + "pro This is a very long argument, so we put it into more than one\n" + " * line. \n"
-						+ " * \n" + " * not rationale text anymore\n" + " */ \t \n}");
+				"// @con This is a structure violation, but it should not kill knowledge extraction\n\n"//
+						+ "// @goal This is a goal outside an issue, let's see where it lands.\n"//
+						+ "// @assumption This is an assumption outside an issue, let's see where it lands.\n\n" //
+						+ "// @alternative Here is another structure violation!\n\n"//
+						+ "/**\n" //
+						+ " * @issue Is this yet another structure violation?\n"//
+						+ " * @con It would appear so.\n" //
+						+ " * @assumption Here is an assumption inside an issue, let's see where this one lands.\n" //
+						+ " * @goal Here is a goal inside an issue, let's see where this one lands.\n"//
+						+ " */\n" //
+						+ "public class GodClass {\n"//
+						+ "//@issue Small code issue in GodClass, it does nothing. \t \n \t \n" //
+						+ "/** \n" //
+						+ " * @issue Will this issue be parsed correctly? \n" //
+						+ " * @alternative We will see!\n" //
+						+ " * @pro This is a very long argument, so we put it into more than one\n" //
+						+ " * line. \n" //
+						+ " * \n" //
+						+ " * not rationale text anymore\n" //
+						+ " */ \t \n}");
 		file.setSummary("example.java");
 		file.setProject("TEST");
 		assertEquals(CommentStyleType.JAVA_C, file.getCommentStyleType());
