a366a340aa821f0b917f5b8b52e19c32d241ac43
Markus Boerner <pielmeier@stud.uni-heidelberg.de>
2021-06-08 23:20:41 +0200

    Fix knowledge element completeness check

--- src/test/java/de/uhd/ifi/se/decision/management/jira/model/knowledgeelement/TestIsIncomplete.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/model/knowledgeelement/TestIsIncomplete.java
@@ -1,7 +1,7 @@
 package de.uhd.ifi.se.decision.management.jira.model.knowledgeelement;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Set;
 
