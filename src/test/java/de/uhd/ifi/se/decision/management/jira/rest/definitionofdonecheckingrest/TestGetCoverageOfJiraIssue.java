174561b36d09fcfd5db398b93aa666b7b5428d6c
Markus Boerner <pielmeier@stud.uni-heidelberg.de>
2021-06-22 13:34:21 +0200

    Improve test coverage

--- src/test/java/de/uhd/ifi/se/decision/management/jira/rest/definitionofdonecheckingrest/TestGetCoverageOfJiraIssue.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/rest/definitionofdonecheckingrest/TestGetCoverageOfJiraIssue.java
@@ -49,11 +49,4 @@ public class TestGetCoverageOfJiraIssue extends TestSetUp {
 		assertEquals(Status.BAD_REQUEST.getStatusCode(),
 			dodCheckingRest.getCoverageOfJiraIssue(request, null, JiraIssues.getTestJiraIssues().get(0).getKey()).getStatus());
 	}
-
-	@Test
-	@NonTransactional
-	public void testRequestValidProjectKeyValidIssueKeyInvalid() {
-		assertEquals(Status.OK.getStatusCode(),
-			dodCheckingRest.getCoverageOfJiraIssue(request, "TEST", null));
-	}
 }
