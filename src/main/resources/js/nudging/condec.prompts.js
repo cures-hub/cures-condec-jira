5d94fd9214353022aafad4481f79fcb2f7a4752c
aaaailuj <18719332+aaaailuj@users.noreply.github.com>
2021-07-12 19:33:57 +0200

    Change the unified prompt to only have one button
    Make the text classification table show if there are no elements and hide the validate button then

--- src/main/resources/js/nudging/condec.prompts.js
+++ src/main/resources/js/nudging/condec.prompts.js
@@ -1,19 +1,16 @@
-(function(global) {
+(function (global) {
 
-	const ConDecPrompt = function() {
+	const ConDecPrompt = function () {
 		this.restPrefix = AJS.contextPath() + "/rest/condec/latest/nudging";
-		jQuery(document).ajaxComplete(function(event, request, settings) {
+		jQuery(document).ajaxComplete(function (event, request, settings) {
 			if (settings.url.includes("WorkflowUIDispatcher.jspa")) {
 				const issueKey = conDecAPI.getIssueKey();
 				// Create unified prompt
 				document.getElementById("unified-prompt-header").innerHTML = "Recommendations for " + issueKey + "...";
 
 				const unifiedPromptElement = document.getElementById("unified-prompt");
-				document.getElementById("warning-dialog-fix-elements").onclick = function() {
-					AJS.dialog2(unifiedPromptElement).hide();
-				}// TODO change this so it actually stops the event from continuing
 
-				document.getElementById("warning-dialog-continue").onclick = function() {
+				document.getElementById("warning-dialog-continue").onclick = function () {
 					AJS.dialog2(unifiedPromptElement).hide();
 				}
 
@@ -30,7 +27,7 @@
 				])
 					.then(([isDoDCheckActivated, isLinkRecommendationActivated, isTextClassificationActivated, isDecisionGuidanceActivated]) => {
 						/**
-						 * @issue The page is reloaded and the ambient feedback is removed again on the 
+						 * @issue The page is reloaded and the ambient feedback is removed again on the
 						 * link recommendation menu item. How can we prevent this?
 						 * @alternative Use jQuery(document).ready to wait for the page to be loaded.
 						 * @con Does not work, the link recommendation menu item coloring is removed.
@@ -62,7 +59,7 @@
 		});
 	};
 
-	ConDecPrompt.prototype.promptLinkSuggestion = function() {
+	ConDecPrompt.prototype.promptLinkSuggestion = function () {
 		const issueId = JIRA.Issue.getIssueId();
 		const projectKey = conDecAPI.projectKey;
 		if (issueId === null || issueId === undefined) {
@@ -70,7 +67,7 @@
 		}
 
 		Promise.all([conDecLinkRecommendationAPI.getDuplicateKnowledgeElement(projectKey, issueId, "i"),
-		conDecLinkRecommendationAPI.getRelatedKnowledgeElements(projectKey, issueId, "i")]) // TODO: could add list of the elements here
+			conDecLinkRecommendationAPI.getRelatedKnowledgeElements(projectKey, issueId, "i")]) // TODO: could add list of the elements here
 			.then((recommendations) => {
 				let numDuplicates = conDecRecommendation.getNumberOfNonDiscardedRecommendations(recommendations[0]);
 				let numLinkRecommendations = conDecRecommendation.getNumberOfNonDiscardedRecommendations(recommendations[1]);
@@ -82,7 +79,7 @@
 			});
 	}
 
-	ConDecPrompt.prototype.promptDefinitionOfDoneChecking = function() {
+	ConDecPrompt.prototype.promptDefinitionOfDoneChecking = function () {
 		const projectKey = conDecAPI.getProjectKey();
 		if (projectKey === null || projectKey === undefined) {
 			return;
@@ -108,7 +105,7 @@
 		document.getElementById("definition-of-done-checking-prompt-jira-issue-key").innerHTML = conDecAPI.getIssueKey();
 	}
 
-	ConDecPrompt.prototype.promptNonValidatedElements = function() {
+	ConDecPrompt.prototype.promptNonValidatedElements = function () {
 		const issueKey = conDecAPI.getIssueKey();
 		if (issueKey === null || issueKey === undefined) {
 			return;
@@ -116,31 +113,31 @@
 
 		conDecTextClassificationAPI.getNonValidatedElements(conDecAPI.projectKey, issueKey)
 			.then(response => {
-				if (response["nonValidatedElements"].length === 0) {
-					return;
-				}
 				const nonValidatedElements = response["nonValidatedElements"]
+				document.getElementById("num-non-validated-elements").innerHTML = nonValidatedElements.length;
 
-				document.getElementById("num-non-validated-elements").innerHTML = response["nonValidatedElements"].length
+				if (nonValidatedElements.length === 0) {
+					document.getElementById("non-validated-table-body").innerHTML = "<i>All elements have been validated!</i>";
+					document.getElementById("non-validated-elements-validate-button").style.display = "none";
+				} else {
+					let tableContents = "";
+					nonValidatedElements.forEach(recommendation => {
+						let tableRow = "<tr>";
+						tableRow += "<td> " + recommendation.summary + "</td>";
+						tableRow += "<td>" + recommendation.type + "</td>";
+						tableRow += "</tr>";
+						tableContents += tableRow;
 
-
-				let tableContents = "";
-				nonValidatedElements.forEach(recommendation => {
-					let tableRow = "<tr>";
-					tableRow += "<td> " + recommendation.summary + "</td>";
-					tableRow += "<td>" + recommendation.type + "</td>";
-					tableRow += "</tr>";
-					tableContents += tableRow;
-
-				});
-				document.getElementById("non-validated-table-body").innerHTML = tableContents;
-				document.getElementById("non-validated-elements-validate-button").onclick = function() {
-					conDecTextClassificationAPI.validateAllElements(conDecAPI.projectKey, issueKey);
-				};
+					});
+					document.getElementById("non-validated-table-body").innerHTML = tableContents;
+					document.getElementById("non-validated-elements-validate-button").onclick = function () {
+						conDecTextClassificationAPI.validateAllElements(conDecAPI.projectKey, issueKey);
+					};
+				}
 			})
 	};
 
-	ConDecPrompt.prototype.promptDecisionGuidance = function() {
+	ConDecPrompt.prototype.promptDecisionGuidance = function () {
 		const issueKey = conDecAPI.getIssueKey();
 		if (issueKey === null || issueKey === undefined) {
 			return;
@@ -172,6 +169,5 @@
 				}
 			});
 	};
-
 	global.conDecPrompt = new ConDecPrompt();
 })(window);
