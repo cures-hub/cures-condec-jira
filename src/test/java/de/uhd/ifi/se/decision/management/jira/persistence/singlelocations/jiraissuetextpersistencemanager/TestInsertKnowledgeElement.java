0d6c15b763c26f85fb9f9b36f6f309f63326710e
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2021-08-24 12:29:55 +0200

    Test adding sentence to comment

--- src/test/java/de/uhd/ifi/se/decision/management/jira/persistence/singlelocations/jiraissuetextpersistencemanager/TestInsertKnowledgeElement.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/persistence/singlelocations/jiraissuetextpersistencemanager/TestInsertKnowledgeElement.java
@@ -9,29 +9,27 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.atlassian.jira.component.ComponentAccessor;
-
 import de.uhd.ifi.se.decision.management.jira.TestSetUp;
 import de.uhd.ifi.se.decision.management.jira.model.KnowledgeElement;
 import de.uhd.ifi.se.decision.management.jira.model.PartOfJiraIssueText;
 import de.uhd.ifi.se.decision.management.jira.persistence.singlelocations.JiraIssueTextPersistenceManager;
 import de.uhd.ifi.se.decision.management.jira.testdata.JiraIssues;
 import de.uhd.ifi.se.decision.management.jira.testdata.JiraUsers;
+import de.uhd.ifi.se.decision.management.jira.testdata.KnowledgeElements;
 import net.java.ao.test.jdbc.NonTransactional;
 
 public class TestInsertKnowledgeElement extends TestSetUp {
 
 	protected JiraIssueTextPersistenceManager manager;
 	protected PartOfJiraIssueText element;
-	protected KnowledgeElement decisionKnowledgeElement;
+	protected KnowledgeElement unsolvedIssue;
 
 	@Before
 	public void setUp() {
 		init();
 		manager = new JiraIssueTextPersistenceManager("TEST");
-		decisionKnowledgeElement = new KnowledgeElement(
-				ComponentAccessor.getIssueManager().getIssueByCurrentKey("TEST-3"));
-		element = JiraIssues.addElementToDataBase();
+		unsolvedIssue = KnowledgeElements.getUnsolvedDecisionProblem();
+		element = JiraIssues.getSentencesForCommentText("{alternative}We could...{alternative}").get(0);
 	}
 
 	@Test
@@ -53,38 +51,53 @@ public class TestInsertKnowledgeElement extends TestSetUp {
 	@Test
 	@NonTransactional
 	public void testElementFilledUserNullParentNull() {
-		assertNull(manager.insertKnowledgeElement(decisionKnowledgeElement, null, null));
+		assertNull(manager.insertKnowledgeElement(unsolvedIssue, null, null));
 	}
 
 	@Test
 	@NonTransactional
 	public void testElementNullUserFilledParentNull() {
-		assertNull(manager.insertKnowledgeElement(null, null, null));
+		assertNull(manager.insertKnowledgeElement(null, JiraUsers.SYS_ADMIN.getApplicationUser(), null));
 	}
 
 	@Test
 	@NonTransactional
 	public void testElementFilledUserFilledParentNull() {
-		assertNull(manager.insertKnowledgeElement(decisionKnowledgeElement, null, null));
+		assertNotNull(manager.insertKnowledgeElement(element, JiraUsers.SYS_ADMIN.getApplicationUser(), null));
 	}
 
 	@Test
 	@NonTransactional
-	public void testElementNullUserNullParentFilled() {
-		assertNull(manager.insertKnowledgeElement(null, null, null));
+	public void testElementNullUserNullParentJiraIssue() {
+		assertNull(manager.insertKnowledgeElement(null, null, unsolvedIssue));
 	}
 
 	@Test
 	@NonTransactional
-	public void testElementFilledUserNullParentFilled() {
-		assertNull(manager.insertKnowledgeElement(decisionKnowledgeElement, null, null));
+	public void testElementFilledUserNullParentJiraIssue() {
+		assertNull(manager.insertKnowledgeElement(element, null, unsolvedIssue));
 	}
 
 	@Test
 	@NonTransactional
-	public void testElementFilledUserFilledParentFilled() {
-		assertNotNull(manager.insertKnowledgeElement(element, JiraUsers.SYS_ADMIN.getApplicationUser(),
-				decisionKnowledgeElement));
+	public void testElementFilledUserFilledParentJiraIssue() {
+		assertNotNull(manager.insertKnowledgeElement(element, JiraUsers.SYS_ADMIN.getApplicationUser(), unsolvedIssue));
+	}
+
+	@Test
+	@NonTransactional
+	public void testElementFilledUserFilledParentDocumentedInComment() {
+		KnowledgeElement parent = JiraIssues.getSentencesForCommentText("{issue}How to?{issue}").get(0);
+		assertNotNull(manager.insertKnowledgeElement(element, JiraUsers.SYS_ADMIN.getApplicationUser(), parent));
+	}
+
+	@Test
+	@NonTransactional
+	public void testElementFilledUserFilledParentDocumentedInDescription() {
+		PartOfJiraIssueText parent = JiraIssues.getSentencesForCommentText("{issue}How to?{issue}").get(0);
+		parent.setCommentId(0);
+		manager.updateInDatabase(parent);
+		assertNotNull(manager.insertKnowledgeElement(element, JiraUsers.SYS_ADMIN.getApplicationUser(), parent));
 	}
 
 }
