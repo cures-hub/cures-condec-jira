a3bd04e3969b479ec80b90f09b3df6c49e03ef5c
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2021-10-25 14:20:47 +0200

    Move class for testing

--- src/test/java/de/uhd/ifi/se/decision/management/jira/changeimpactanalysis/TestPostCommitsIntoJiraIssueComments.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/changeimpactanalysis/TestPostCommitsIntoJiraIssueComments.java
@@ -1,4 +1,4 @@
-package de.uhd.ifi.se.decision.management.jira.git.commitmessagetocommenttranscriber;
+package de.uhd.ifi.se.decision.management.jira.changeimpactanalysis;
 
 import static org.junit.Assert.assertEquals;
 
