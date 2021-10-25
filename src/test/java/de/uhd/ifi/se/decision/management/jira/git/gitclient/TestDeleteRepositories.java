1ace7703d0039091f64cb01077ae6c8aed94137b
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2021-10-25 23:01:40 +0200

    Do not ignore tests anymore

--- src/test/java/de/uhd/ifi/se/decision/management/jira/git/gitclient/TestDeleteRepositories.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/git/gitclient/TestDeleteRepositories.java
@@ -3,7 +3,6 @@ package de.uhd.ifi.se.decision.management.jira.git.gitclient;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import net.java.ao.test.jdbc.NonTransactional;
@@ -11,7 +10,6 @@ import net.java.ao.test.jdbc.NonTransactional;
 public class TestDeleteRepositories extends TestSetUpGit {
 
 	@Test
-	@Ignore
 	@NonTransactional
 	public void testDeleteRepositoryNotNull() {
 		assertTrue(gitClient.deleteRepositories());
