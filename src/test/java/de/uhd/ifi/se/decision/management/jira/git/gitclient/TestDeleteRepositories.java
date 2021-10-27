535a0ef899a27c7b77f879c8319cacb40b9b66e5
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2021-10-27 14:52:24 +0200

    Change git repo to delete

--- src/test/java/de/uhd/ifi/se/decision/management/jira/git/gitclient/TestDeleteRepositories.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/git/gitclient/TestDeleteRepositories.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import de.uhd.ifi.se.decision.management.jira.git.GitClient;
 import net.java.ao.test.jdbc.NonTransactional;
 
 public class TestDeleteRepositories extends TestSetUpGit {
@@ -12,8 +13,8 @@ public class TestDeleteRepositories extends TestSetUpGit {
 	@Test
 	@NonTransactional
 	public void testDeleteRepositoryNotNull() {
-		assertTrue(gitClient.deleteRepositories());
-		assertNotNull(gitClient);
+		assertTrue(GitClient.getInstance("TEST").deleteRepositories());
+		assertNotNull(GitClient.getInstance("TEST"));
 	}
 
 }
