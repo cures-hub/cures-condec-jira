e8fb01e2f80162d0303147ace98e39399f8223e8
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2021-11-14 18:16:51 +0100

    Simplify Javascript

--- src/main/resources/js/git/condec.gitdiffviewer.js
+++ src/main/resources/js/git/condec.gitdiffviewer.js
@@ -11,48 +11,77 @@ var NO_QUALITY_PROBLEMS_FOR_NO_RATIONALE_IN_BRANCH = "No rationale found in mess
 
 var BRANCHES_XHR_ERROR_MSG = "An unspecified error occurred while fetching REST data, please try again.";
 
-var branches = [];
-
 function getBranchesDiff() {
 	contentHtml = document.getElementById("featureBranches-container");
 	contentHtml.innerText = "Loading ...";
 
 	conDecGitAPI.getElementsFromBranchesOfJiraIssue(conDecAPI.getIssueKey())
 		.then((branches) => {
+			if (branches === null || branches === undefined) {
+				contentHtml.innerText = "Git extraction is disabled.";
+				return;
+			}
+			if (branches.length === 0) {
+				contentHtml.innerText = "No feature branches found for this Jira issue.";
+			}
 			showBranchesDiff(branches);
+			contentHtml.appendChild(createForceRestFetch());
 		})
 		.catch((error) => showError(error));
+}
+
+function showBranchesDiff(branches) {
+	console.debug("showBranchesDiff");
+	contentHtml.innerText = "";
+
+	for (branchIdx = 0; branchIdx < branches.length; branchIdx++) {
+		lastBranch = conDecLinkBranchCandidates.extractPositions(branches[branchIdx]);
+		lastBranchIdx = branchIdx;
+		lastBranchBlocks = new Map();
+
+		lastBranchElementsFromMessages = lastBranch.commitElements;
+		lastBranchElementsFromFiles = lastBranch.codeElements;
+
+		showBranchDiff(lastBranch, branchIdx);
+
+		/* assess relations between rationale and their problems */
+		conDecLinkBranchCandidates.init(lastBranchElementsFromMessages, lastBranch.branchName, branchIdx,
+			"messages");
+		/* render results in HTML */
+		conDecLinkBranchCandidates.attachProblemsToElementsInHTML();
+
+		conDecLinkBranchCandidates.init(lastBranchElementsFromFiles, lastBranch.branchName, branchIdx, "files");
+		/* render results in HTML */
+		conDecLinkBranchCandidates.attachProblemsToElementsInHTML();
+	}
+}
+
+function createForceRestFetch() {
+	forceRestNode = document.createElement("div");
+	forceRestNode.innerText = "Suspecting branch list is not up-to date? Click here to try again.";
+	forceRestNode.addEventListener("click", () => getBranchesDiff());
+	return forceRestNode;
 }
 
 function showError(error) {
 	console.debug("showError");
 	contentHtml.innerText = BRANCHES_XHR_ERROR_MSG;
 	console.log(error);
-	appendForceRestFetch(contentHtml);
+	contentHtml.appendChild(createForceRestFetch());
 }
 
-function getElementAsHTML(element, isFromMessage) {
+function getElementAsHTML(element) {
 	console.debug("getElementAsHTML");
 	root = document.createElement("p");
 	root.className = "messageBox " + element.type.toLowerCase();
-
-	var locationText = "";
-	if (isFromMessage) {		
-		locationText = "Commit message " + element.source;
-	} else {
-		locationText = "Line in file: " + element.startLine;
-	}
-	
 	root.style = "padding:5px;";
-
-	root.title = locationText;
 	root.dataset.ratType = element.type.toLowerCase();
 
-    img = document.createElement("img");
-    img.src = element.image;
-    img.align ='absmiddle';
-    img.className = "emoticon";
-    
+	img = document.createElement("img");
+	img.src = element.image;
+	img.align = "absmiddle";
+	img.className = "emoticon";
+
 	root.appendChild(img);
 	root.insertAdjacentText("beforeend", element.summary);
 	return root;
@@ -94,9 +123,10 @@ function getCodeElementsFromSide(blockData) {
 	return codeElements;
 }
 
-function appendCodeElements(brNode) {
+function appendCodeElements() {
 	console.debug("appendCodeElements");
 	blockLinesIterator = lastBranchBlocks.entries();
+	var allCodeElementsHTML = document.createElement("div");
 	while (blockEntry = blockLinesIterator.next()) {
 		if (blockEntry.done) {
 			break;
@@ -107,7 +137,7 @@ function appendCodeElements(brNode) {
 
 		var fileRatBlockLabel = document.createElement("h4");
 		fileRatBlockLabel.innerText = blockData.filename;
-		
+
 		fileRatBlockLabel.dataset.blockSequence = blockData.sequence;
 
 		var codeElements = getCodeElementsFromSide(blockData);
@@ -116,9 +146,10 @@ function appendCodeElements(brNode) {
 		codeElements = getCodeElementsFromSide(blockData);
 		fileRatElement.appendChild(codeElements);
 
-		brNode.appendChild(fileRatBlockLabel);
-		brNode.appendChild(fileRatElement);
+		allCodeElementsHTML.appendChild(fileRatBlockLabel);
+		allCodeElementsHTML.appendChild(fileRatElement);
 	}
+	return allCodeElementsHTML;
 }
 
 function getBlock(element, counter) {
@@ -132,53 +163,45 @@ function getBlock(element, counter) {
 	return block;
 }
 
-function appendBranchMessageElementsHtml(elementsFromMessage, parentNode) {
-	if (elementsFromMessage !== null && elementsFromMessage.length > 0) {
-		/* group rationale in messages by commit hash */
-		var msgCommitIsh = "";
-		var messageBlockHtml = null;
-		for (m = 0; m < elementsFromMessage.length; m++) {
-			if (msgCommitIsh !== elementsFromMessage[m].source) {
-				msgCommitIsh = elementsFromMessage[m].source;
-				if (messageBlockHtml) {
-					/* add previous message */
-					parentNode.appendChild(messageBlockHtml);
-				}
-				messageBlockHtml = document.createElement("p");
-				messageBlockHtml.id = "branchGroup-" + lastBranchIdx + "-message-" + msgCommitIsh;
-				messageBlockHtml.className = "messageBox";
-
-				messageBlockLabelHtml = document.createElement("div");
-				messageBlockLabelHtml.innerText = "Commit message " + msgCommitIsh;
-				messageBlockLabelHtml.className = "commitMessageLabel";
-				messageBlockHtml.appendChild(messageBlockLabelHtml);
-			}
+function createBranchMessageElementsHtml(elementsFromMessage) {
+	/* group rationale in messages by commit hash */
+	var msgCommitIsh = "";
+	var messageBlockHtml = null;
+	var allMessageBlockHtml = document.createElement("div");
+	for (m = 0; m < elementsFromMessage.length; m++) {
+		if (msgCommitIsh !== elementsFromMessage[m].source) {
+			msgCommitIsh = elementsFromMessage[m].source;
 			if (messageBlockHtml) {
-				var messageElementHtml = getElementAsHTML(elementsFromMessage[m], true);
-				messageBlockHtml.appendChild(messageElementHtml);
+				/* add previous message */
+				allMessageBlockHtml.appendChild(messageBlockHtml);
 			}
+			messageBlockHtml = document.createElement("p");
+			messageBlockHtml.id = "branchGroup-" + lastBranchIdx + "-message-" + msgCommitIsh;
+			messageBlockHtml.className = "messageBox";
+
+			messageBlockLabelHtml = document.createElement("div");
+			messageBlockLabelHtml.innerText = "Commit message " + msgCommitIsh;
+			messageBlockLabelHtml.className = "commitMessageLabel";
+			messageBlockHtml.appendChild(messageBlockLabelHtml);
 		}
-		parentNode.appendChild(messageBlockHtml);
+		if (messageBlockHtml) {
+			var messageElementHtml = getElementAsHTML(elementsFromMessage[m]);
+			messageElementHtml.title = "Commit " + elementsFromMessage[m].source;
+			messageBlockHtml.appendChild(messageElementHtml);
+		}
 	}
+	allMessageBlockHtml.appendChild(messageBlockHtml);
+	return allMessageBlockHtml;
 }
 
-function appendBranchCodeElementsHtml(elementsFromCode, parentNode) {
+function createBranchCodeElementsHtml(elementsFromCode) {
 	var blockCounter = 0;
-	var previousBlock = null;
 
 	for (c = 0; c < elementsFromCode.length; c++) {
-		codeElementHtml = getElementAsHTML(elementsFromCode[c], false);
+		codeElementHtml = getElementAsHTML(elementsFromCode[c]);
+		codeElementHtml.title = "Line in file: " + elementsFromCode[c].startLine;
 
 		var block = getBlock(elementsFromCode[c], blockCounter);
-
-		if (previousBlock === null) {
-			previousBlock = block;
-		}
-		if (block.diffType !== previousBlock.diffType || block.entry !== previousBlock.entry) {
-			blockCounter++;
-			block.sequence = blockCounter;
-		}
-		previousBlock = block;
 
 		var blockKey = block.toString();
 
@@ -196,10 +219,10 @@ function appendBranchCodeElementsHtml(elementsFromCode, parentNode) {
 		blockData.codeElements.push(codeElementHtml);
 		lastBranchBlocks.set(blockKey, blockData);
 	}
-	appendCodeElements(parentNode);
+	return appendCodeElements();
 }
 
-function appendBranchQualityAssessment(parentNode) {
+function createBranchQualityAssessment() {
 	qualitySummary = document.createElement("p");
 	qualitySummary.className = "qualitySummary";
 	if ((lastBranchElementsFromMessages && lastBranchElementsFromMessages.length > 0)
@@ -210,86 +233,23 @@ function appendBranchQualityAssessment(parentNode) {
 		qualitySummary.innerText = NO_QUALITY_PROBLEMS_FOR_NO_RATIONALE_IN_BRANCH;
 		qualitySummary.classList.add("noRationale");
 	}
-	parentNode.appendChild(qualitySummary);
+	return qualitySummary;
 }
 
-/*
- * render feature branch in HTML
+/**
+ * Renders one git branch in HTML.
  */
 function showBranchDiff(branch) {
 	console.debug("showBranchDiff");
 
 	branchContainer = document.createElement("div");
-	branchContainer.className = "branchGroup";
-
-	/* branch name */
 	branchLabel = document.createElement("h3");
 	branchLabel.innerText = branch.branchName;
 	branchContainer.appendChild(branchLabel);
-	
-	/* show user the quality assessment for rationale observed in modified files */
-	appendBranchQualityAssessment(branchContainer);
-	/* show user the rationale observed in modified files */	
-	appendBranchMessageElementsHtml(branch.commitElements, branchContainer);
-	appendBranchCodeElementsHtml(branch.codeElements, branchContainer);
 
-	/* append branch HTMl to parent HTML container */
+	branchContainer.appendChild(createBranchQualityAssessment());
+	branchContainer.appendChild(createBranchMessageElementsHtml(branch.commitElements));
+	branchContainer.appendChild(createBranchCodeElementsHtml(branch.codeElements));
+
 	contentHtml.appendChild(branchContainer);
-}
-
-function appendForceRestFetch(parentNode) {
-	forceRestNode = document.createElement("p");
-	forceRestNode.innerText = "Suspecting branch list is not up-to date? Click here to try again.";
-	forceRestNode.addEventListener("click", function(e) {
-		getBranchesDiff(true);
-	});
-	parentNode.appendChild(forceRestNode);
-}
-
-function showBranchesDiff(branches) {
-	console.debug("showBranchesDiff");
-	if (branches === null || branches === undefined) {
-		contentHtml.innerText = "Git extraction is disabled.";
-		return;
-	}
-	branches.timestamp = Date.now();
-	contentHtml = document.getElementById("featureBranches-container");
-	contentHtml.innerText = "";
-
-	if (branches.length > 0) {
-		/* branches come not sorted from rest */
-		branches = branches.sort(function(a, b) {
-			if (a.branchName < b.branchName) {
-				return -1;
-			} else {
-				return 1;
-			}
-		});
-
-		for (branchIdx = 0; branchIdx < branches.length; branchIdx++) {
-			lastBranch = conDecLinkBranchCandidates.extractPositions(branches[branchIdx]);
-			lastBranchIdx = branchIdx;
-			lastBranchBlocks = new Map();
-
-			/* these elements are sorted by commit age and occurrence in message */
-			lastBranchElementsFromMessages = lastBranch.commitElements;
-			/* these elements are not sorted */
-			lastBranchElementsFromFiles = lastBranch.codeElements;
-
-			showBranchDiff(lastBranch, branchIdx);
-
-			/* assess relations between rationale and their problems */
-			conDecLinkBranchCandidates.init(lastBranchElementsFromMessages, lastBranch.branchName, branchIdx,
-				"messages");
-			/* render results in HTML */
-			conDecLinkBranchCandidates.attachProblemsToElementsInHTML();
-
-			conDecLinkBranchCandidates.init(lastBranchElementsFromFiles, lastBranch.branchName, branchIdx, "files");
-			/* render results in HTML */
-			conDecLinkBranchCandidates.attachProblemsToElementsInHTML();
-		}
-	} else {
-		contentHtml.innerText = "No feature branches found for this issue.";
-	}
-	appendForceRestFetch(contentHtml);
 }
