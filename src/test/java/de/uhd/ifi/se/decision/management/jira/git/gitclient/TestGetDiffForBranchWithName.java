a869261ae90607bd588fdf38b8b1ad653eb7acfb
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2021-11-20 12:15:45 +0100

    Refactor tests

--- src/test/java/de/uhd/ifi/se/decision/management/jira/git/gitclient/TestGetDiffForBranchWithName.java
+++ src/test/java/de/uhd/ifi/se/decision/management/jira/git/gitclient/TestGetDiffForBranchWithName.java
@@ -3,8 +3,11 @@ package de.uhd.ifi.se.decision.management.jira.git.gitclient;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.util.List;
 
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.junit.Test;
 
@@ -17,7 +20,9 @@ public class TestGetDiffForBranchWithName extends TestSetUpGit {
 		Diff diff = gitClient.getDiffForBranchWithName("TEST-4");
 
 		assertEquals(1, diff.getRefs().size());
-		assertEquals("refs/remotes/origin/TEST-4.feature.branch", diff.getRefs().get(0).getName());
+		Ref featureBranch = diff.getRefs().get(0);
+		assertEquals("refs/remotes/origin/TEST-4.feature.branch", featureBranch.getName());
+		assertEquals(URLEncoder.encode(GIT_URI, Charset.defaultCharset()), diff.get(0).getRepoUri());
 
 		List<RevCommit> commits = diff.getCommits();
 		assertEquals(5, commits.size());
