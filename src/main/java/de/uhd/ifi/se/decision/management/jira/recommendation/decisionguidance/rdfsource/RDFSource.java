805d9aed693f6e2c2595b20b56037e15c1df7cc9
mhartmann <max_hartmann@stud.uni-heidelberg.de>
2022-03-10 13:35:59 +0100

    CONDEC-990: Resolve issues detected by static code analyses

--- src/main/java/de/uhd/ifi/se/decision/management/jira/recommendation/decisionguidance/rdfsource/RDFSource.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/recommendation/decisionguidance/rdfsource/RDFSource.java
@@ -16,22 +16,52 @@ import de.uhd.ifi.se.decision.management.jira.recommendation.decisionguidance.Kn
  */
 public class RDFSource extends KnowledgeSource {
 
+	/**
+	 * Uniform Resource Identifier (URI) to RDF graph, e.g. http://dbpedia.org/sparql.
+	 */
 	protected String service;
+
+	/**
+	 * Query in SPARQL language (SPARQL Protocol And RDF Query Language).
+	 */
 	protected String query;
+
+	/**
+	 * Timeout in milliseconds for the requests.
+	 */
 	protected int timeout;
+
+	/**
+	 * Constraints added to the query, e.g. License=dbo:license. Multiple values are separated with "&".
+	 */
 	protected String constraints;
 
+	/**
+	 * Prefix of the query.
+	 */
 	public static final String PREFIX = "PREFIX dbo: <http://dbpedia.org/ontology/>\n" //
 			+ "PREFIX dct: <http://purl.org/dc/terms/>\n" //
 			+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" //
 			+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" //
 			+ "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n\n";
+
+	/**
+	 * Default query, searching for other subjects of the same genre as the one added for the placeholder "%variable%".
+	 */
 	public static final String DEFAULT_QUERY = PREFIX + "select distinct ?subject ?url count(?link) where { \n" //
 			+ "%variable% dbo:genre ?genre.\n" //
 			+ "?subject dbo:genre ?genre.\n"//
 			+ "?subject foaf:isPrimaryTopicOf ?url.\n" //
 			+ "?subject dbo:wikiPageExternalLink ?link.} GROUP BY ?subject ?url";
+
+	/**
+	 * Default value for {@link KnowledgeSource#name}.
+	 */
 	public static final String DEFAULT_NAME = "Frameworks";
+
+	/**
+	 * DBpedia as default value for {@link RDFSource#service}.
+	 */
 	public static final String DEFAULT_SERVICE = "http://dbpedia.org/sparql";
 
 	/**
@@ -41,20 +71,16 @@ public class RDFSource extends KnowledgeSource {
 		super(DEFAULT_NAME, true);
 		this.service = DEFAULT_SERVICE;
 		this.query = DEFAULT_QUERY;
-		this.timeout = 30000;
+		this.timeout = 30_000;
 		this.constraints = "";
 	}
 
 	/**
-	 * @param service,
-	 *            Uniform Resource Identifier (URI) to RDF graph, e.g.,
-	 *            http://dbpedia.org/sparql
-	 * @param query
-	 *            in SPARQL language (SPARQL Protocol And RDF Query Language).
-	 * @param name
-	 *            of the knowledge source.
-	 * @param timeout
-	 *            in milliseconds.
+	 * @param service {@link RDFSource#service}
+	 * @param query {@link RDFSource#query}
+	 * @param name {@link KnowledgeSource#name}
+	 * @param timeout {@link RDFSource#timeout}
+	 *
 	 */
 	public RDFSource(String name, String service, String query, int timeout, String constraint) {
 		super(name, true);
@@ -118,17 +144,21 @@ public class RDFSource extends KnowledgeSource {
 		return constraints;
 	}
 
+	/**
+	 * @return Map based on {@link RDFSource#constraints}
+	 */
 	public Map<String, String> getConstraintMap() {
+		Map<String, String> constraintMap;
 		try {
-			return Splitter.on("&").withKeyValueSeparator("=").split(getConstraints());
+			constraintMap = Splitter.on("&").withKeyValueSeparator("=").split(getConstraints());
 		} catch (IllegalArgumentException e) {
-			return new HashMap<>();
+			constraintMap = new HashMap<>();
 		}
+		return constraintMap;
 	}
 
 	/**
-	 * @param constraints
-	 *            e.g. License=dbo:license. Multiple values are separated with "&".
+	 * @param constraints {@link RDFSource#constraints}
 	 */
 	public void setConstraints(String constraints) {
 		this.constraints = constraints;
@@ -139,6 +169,8 @@ public class RDFSource extends KnowledgeSource {
 		return name.replace(" ", "-");
 	}
 
+	@SuppressWarnings("PMD.OnlyOneReturn")  // For static code analysis: Allow multiple returns as it improves
+	// ´                                       readability.
 	@Override
 	public boolean equals(Object object) {
 		if (this == object) {
@@ -161,12 +193,18 @@ public class RDFSource extends KnowledgeSource {
 		return "aui-iconfont-download";
 	}
 
+	/**
+	 * @return List containing as only element the default {@link RDFSource}, i.e. {@link RDFSource#dbPediaFrameworks()}.
+	 */
 	public static List<RDFSource> getDefaultDBPediaQueries() {
 		List<RDFSource> defaultSources = new ArrayList<>();
 		defaultSources.add(dbPediaFrameworks());
 		return defaultSources;
 	}
 
+	/**
+	 * @return New {@link RDFSource} with the default service (DBpedia).
+	 */
 	public static RDFSource dbPediaFrameworks() {
 		return new RDFSource();
 	}
