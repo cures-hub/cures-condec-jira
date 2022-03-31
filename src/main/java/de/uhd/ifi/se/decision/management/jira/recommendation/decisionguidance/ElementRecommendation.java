d5dfc5f06e778e9eade2df1d6b49e49c32d1fc5f
mhhd2020 <max_hartmann@stud.uni-heidelberg.de>
2022-03-31 12:48:16 +0200

    Update ElementRecommendation.java: Add JsonIgnore annotation to arguments

--- src/main/java/de/uhd/ifi/se/decision/management/jira/recommendation/decisionguidance/ElementRecommendation.java
+++ src/main/java/de/uhd/ifi/se/decision/management/jira/recommendation/decisionguidance/ElementRecommendation.java
@@ -46,6 +46,7 @@ public class ElementRecommendation extends KnowledgeElement implements Recommend
 	/**
 	 * Pro and Con {@link Argument}s for the recommendation.
 	 */
+	@JsonIgnore
 	private List<Argument> arguments;
 
 	/**
